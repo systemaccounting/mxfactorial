6ea2871a922b1190488299aa84bf99cad0629e2c
Tay Nguyen <taynh73@gmail.com>
2016-06-20 07:13:51 -0700

    fix typo

--- app/selectors/transaction.js
+++ app/selectors/transaction.js
@@ -5,4 +5,4 @@ const getTransactionItem = (state) => (state.transaction_item);
 
 export const transactionAmountSelector = (state) => (
   reduce(getTransactionItem(items), (memo, item) =>(memo + item.value * item.quantity), 0)
-));
+);
