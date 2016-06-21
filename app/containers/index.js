d5af27c4757eb0de190dab7da169548339cc8bb0
Arinde Eniola <eniola.arinde@andela.co>
2016-06-21 22:18:59 +0100

    Add history & detail screens (#33)

--- app/containers/index.js
+++ app/containers/index.js
@@ -3,3 +3,5 @@ export HomePage from './Home/Home';
 export CreateAccountInfo from './CreateAccountInfo/CreateAccountInfo';
 export CreateAccount from './CreateAccount/CreateAccount';
 export BaseLayout from './Layout/BaseLayout';
+export TransactionHistory from './TransactionHistory/TransactionHistory';
+export TransactionDetails from './TransactionDetails/TransactionDetails';
