a1932c2a1c7f8f13bc64cccba0ce6de77e593a59
Tay Nguyen <taynh73@gmail.com>
2016-07-23 06:36:56 -0700

    add black list transaction which is already emit an notification

--- transact/observer.js
+++ transact/observer.js
@@ -2,20 +2,26 @@ var _ = require('lodash');
 
 var ref = require('./ref');
 var notifyRef = require('notification/ref');
+var blacklistRef = require('./blacklist-transaction');
 
 ref.on('child_added', function (snapshot, prevChildKey) {
   var transaction = snapshot.val();
-
-  notifyRef.push({
-    key: snapshot.key,
-    sender_account: transaction.db_author || null,
-    receiver_account: transaction.cr_author || null,
-    type: 'bid200',
-    method: 'webclient',
-    payload: _.reduce(transaction.transaction_item, function (memo, item) {
-      return memo + item.quantity * item.value;
-    }, 0),
-    sent_time: new Date().toISOString(),
-    received_time: null
+  var transactionKey = snapshot.key;
+  blacklistRef.child(transactionKey).once('value', function (snapshot) {
+    if (!snapshot.val()) {
+      notifyRef.push({
+        key: transactionKey,
+        sender_account: transaction.db_author || null,
+        receiver_account: transaction.cr_author || null,
+        type: 'bid200',
+        method: 'webclient',
+        payload: _.reduce(transaction.transaction_item, function (memo, item) {
+          return memo + item.quantity * item.value;
+        }, 0),
+        sent_time: new Date().toISOString(),
+        received_time: null
+      });
+      blacklistRef.child(transactionKey).set(true);
+    }
   });
 });
