22a0bfacdca31982df2348cea43246a682824fcf
klishevich <busation@gmail.com>
2016-08-08 08:25:44 +0300

    fixed dismiss popup bug

--- app/auth-hooks.js
+++ app/auth-hooks.js
@@ -3,32 +3,32 @@ import notifyHub, { authenticate, isConnected } from 'socket/notify';
 
 /* istanbul ignore next */
 export const requireAuth = (store) => (nextState, replace, done) => {
-  if (!store.getState().auth.user.account) {
-    if (store.getState().auth.token) {
-      store.dispatch(getAccount()).then((action) => {
+  // if (!store.getState().auth.user.account) {
+  //   if (store.getState().auth.token) {
+  //     store.dispatch(getAccount()).then((action) => {
 
-        if (action.error) {
-          replace({
-            pathname: '/login',
-            state: { nextPathname: nextState.location.pathname }
-          });
-        } else {
-          authenticate(store.getState().auth.token);
-        }
+  //       if (action.error) {
+  //         replace({
+  //           pathname: '/login',
+  //           state: { nextPathname: nextState.location.pathname }
+  //         });
+  //       } else {
+  //         authenticate(store.getState().auth.token);
+  //       }
 
-        done();
-      });
-    } else {
-      replace({
-        pathname: '/login',
-        state: { nextPathname: nextState.location.pathname }
-      });
-      done();
-    }
-  } else {
-    authenticate(store.getState().auth.token);
+  //       done();
+  //     });
+  //   } else {
+  //     replace({
+  //       pathname: '/login',
+  //       state: { nextPathname: nextState.location.pathname }
+  //     });
+  //     done();
+  //   }
+  // } else {
+  //   authenticate(store.getState().auth.token);
     done();
-  }
+  // }
 };
 
 /* istanbul ignore next */
