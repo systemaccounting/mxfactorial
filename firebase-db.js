aaf575a6527a1730e73f43de07d1e5c29e7e386d
max funk <max@systemaccounting.org>
2017-02-08 09:15:58 +0000

    Update firebase client

--- firebase-db.js
+++ firebase-db.js
@@ -1,9 +1,9 @@
-var firebase = require('firebase');
+var firebase = require('firebase-admin');
 var config = require('config.js');
 
 firebase.initializeApp({
   databaseURL: config.get('FIREBASE_URL'),
-  serviceAccount: config.get('FIREBASE_KEY_PATH')
+  credential: firebase.credential.cert(config.get('FIREBASE_KEY_PATH'))
 });
 
 module.exports = firebase.database();
