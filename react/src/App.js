bd1da3e12e1d328fb6496ce6b6f6f309a90adbeb
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-09-30 21:35:13 +0200

    modify routes

--- react/src/App.js
+++ react/src/App.js
@@ -5,15 +5,30 @@ import LandingScreen from 'screens/LandingScreen'
 import HomeScreen from 'screens/HomeScreen'
 import CreateAccount from 'screens/CreateAccount'
 import RequestScreen from 'screens/RequestScreen'
+
+import Providers from 'providers'
+
+import PrivateRoutes from 'screens/private'
+import PublicRoutes from 'screens/public'
+import NotFound from 'screens/notFound'
+
 import './App.css'
 
 const App = () => (
-  <Router>
-    <LandingScreen exact path="/" />
-    <CreateAccount path="account/create" />
-    <HomeScreen path="account" />
-    <RequestScreen path="requests" />
-  </Router>
+  <Providers>
+    <Router>
+      <NotFound default />
+      <PrivateRoutes path="/">
+        {props => (
+          <React.Fragment>
+            <HomeScreen path="/account" />
+            <RequestScreen path="/requests" />
+          </React.Fragment>
+        )}
+      </PrivateRoutes>
+      <PublicRoutes path="/auth" />
+    </Router>
+  </Providers>
 )
 
 export default App
