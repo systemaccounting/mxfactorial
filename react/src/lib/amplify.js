d8f974cc2119074ecd53cb8fe236b69d7d15243a
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-10-01 22:38:11 +0200

    use subject to signal UserContext when auth performed

--- react/src/lib/amplify.js
+++ react/src/lib/amplify.js
@@ -1,10 +1,23 @@
 import { Auth } from 'aws-amplify'
+import { BehaviorSubject } from 'rxjs'
+
+export const singInPerform = new BehaviorSubject(null)
 
 export const currentUserInfo = () => Auth.currentUserInfo()
 
-export const signOut = () => Auth.signOut()
+export const signOut = () =>
+  Auth.signOut().then(() => {
+    singInPerform.next(null)
+  })
 
-export const signIn = (username, password) => Auth.signIn(username, password)
+export const signIn = ({ account, password }) => {
+  return Auth.signIn(account, password).then(async token => {
+    if (token) {
+      const user = await Auth.currentUserInfo()
+      singInPerform.next(user)
+    }
+  })
+}
 
 export const signUp = (credentials, attributes) =>
   Auth.signUp(credentials, attributes)
