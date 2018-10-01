97247930bcdc8eb741de2140dfb6cf1247e4b7f0
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-10-01 22:39:54 +0200

    switch react-router

--- react/src/components/MobileNav/MovileNav.test.js
+++ react/src/components/MobileNav/MovileNav.test.js
@@ -2,18 +2,24 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import MobileNav, { ListItem } from './index'
 
+const pushMock = jest.fn()
+
+const history = {
+  push: pushMock
+}
+
 describe('<MobileNav />', () => {
   it('renders', () => {
     const wrapper = shallow(<MobileNav />)
     expect(wrapper.exists()).toBeTruthy()
   })
   it('should call navigate on list item click', () => {
-    const wrapper = shallow(<MobileNav />)
+    const wrapper = shallow(<MobileNav history={history} />)
     const listItem1 = wrapper.find(ListItem).first()
     listItem1.simulate('click')
-    expect(navigate).toHaveBeenCalled()
+    expect(pushMock).toHaveBeenCalled()
     const listItem2 = wrapper.find(ListItem).at(1)
     listItem2.simulate('click')
-    // expect(navigate).toHaveBeenCalled()
+    expect(pushMock).toHaveBeenCalled()
   })
 })
