df6bab7b16eb4c52bf959292f179d4a434b8bd5d
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-10-01 22:38:32 +0200

    pass subject and amplify method as prop to provider

--- react/src/providers/index.js
+++ react/src/providers/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+import { currentUserInfo, singInPerform } from 'lib/amplify'
 import { UserProvider } from 'context/User/UserContext'
 
-export default ({ children }) => <UserProvider>{children}</UserProvider>
+export default ({ children }) => (
+  <UserProvider currentUserInfo={currentUserInfo} singInPerform={singInPerform}>
+    {children}
+  </UserProvider>
+)
