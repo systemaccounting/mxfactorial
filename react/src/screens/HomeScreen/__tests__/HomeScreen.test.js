6c746df84aa77cfbcd86191c09dd3e8734b73c9d
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-10-01 22:39:23 +0200

    fix tests and remove load user tests

--- react/src/screens/HomeScreen/__tests__/HomeScreen.test.js
+++ react/src/screens/HomeScreen/__tests__/HomeScreen.test.js
@@ -12,22 +12,14 @@ const fetchBalance = promiseToResolve(1000)
 const props = {
   signOut,
   currentUserInfo,
-  fetchBalance
+  fetchBalance,
+  user
 }
 
 describe('<HomeScreen />', () => {
   it('renders', () => {
     const wrapper = shallow(<HomeScreen {...props} />)
     expect(wrapper.exists()).toBeTruthy()
-  })
-
-  it('loads user', () => {
-    const wrapper = shallow(<HomeScreen {...props} />)
-    const instance = wrapper.instance()
-
-    instance.getUserData().then(user => {
-      expect(wrapper.state('user')).toEqual({ username: 'john doe' })
-    })
   })
 
   it('loads balance', () => {
@@ -38,6 +30,7 @@ describe('<HomeScreen />', () => {
       expect(wrapper.state('balance')).toEqual(1000)
     })
   })
+
   it('doesnt load balance', () => {
     const fetchBalance = promiseToReject('no balance')
     const wrapper = shallow(
@@ -47,35 +40,6 @@ describe('<HomeScreen />', () => {
 
     instance.getBalance().then(user => {
       expect(wrapper.state('error')).toEqual('no balance')
-    })
-  })
-
-  it('doesnt load user', () => {
-    props.currentUserInfo = promiseToReject()
-    const wrapper = shallow(<HomeScreen {...props} />)
-    const instance = wrapper.instance()
-
-    instance.getUserData().then(user => {
-      expect(wrapper.state('user')).toEqual(null)
-    })
-  })
-
-  it('signs out and navigates', () => {
-    const navigateMock = jest.fn()
-    const wrapper = shallow(<HomeScreen navigate={navigateMock} {...props} />)
-    const instance = wrapper.instance()
-
-    instance.handleSignOut().then(() => {
-      expect(navigateMock).toHaveBeenCalledWith('/')
-    })
-  })
-
-  it('updates state on unmount', () => {
-    const wrapper = shallow(<HomeScreen {...props} />)
-    const instance = wrapper.instance()
-    instance.getUserData().then(() => {
-      instance.componentWillUnmount()
-      expect(wrapper.state('user')).toEqual(null)
     })
   })
 })
