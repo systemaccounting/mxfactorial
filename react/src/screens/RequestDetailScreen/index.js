f2ba8fde06c8659cc5f5b1ad2cb670da09600a6b
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-10-17 23:04:42 +0200

    decorate withUser

--- react/src/screens/RequestDetailScreen/index.js
+++ react/src/screens/RequestDetailScreen/index.js
@@ -1,6 +1,10 @@
 import { compose } from 'ramda'
 
 import withApi from 'decorators/withApi'
+import withUser from 'decorators/withUser'
 import RequestDetailScreen from './RequestDetailScreen'
 
-export default compose(withApi)(RequestDetailScreen)
+export default compose(
+  withApi,
+  withUser
+)(RequestDetailScreen)
