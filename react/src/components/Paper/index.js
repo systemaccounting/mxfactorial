202ec434a33198ee63d251ca05b9e0d5f5686d04
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-10-20 20:26:19 +0200

    Paper styling

--- react/src/components/Paper/index.js
+++ react/src/components/Paper/index.js
@@ -4,7 +4,12 @@ const Paper = styled.div`
   border-radius: 4px;
   background-color: #efefef;
   padding: 10px;
+  min-height: 48px;
   margin: 5px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-shadow: 9px 9px 9px 1px rgba(92, 92, 95, 0.2);
 `
 
 export default Paper
