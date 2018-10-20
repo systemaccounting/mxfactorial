9bc2a45350838cc9d8e86433c980c89443658334
max funk <mxfactorial@users.noreply.github.com>
2018-10-20 14:43:37 -0700

    Add environment variable to s3 policy

--- terraform/workspaces/s3.tf
+++ terraform/workspaces/s3.tf
@@ -1,6 +1,20 @@
 resource "aws_s3_bucket" "mxfactorial-react" {
-  bucket = "mxfactorial-react"
-  policy = "${file("s3-policy.json")}"
+  bucket = "mxfactorial-react-${lookup(var.environment, "${terraform.workspace}")}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadForGetBucketObjects",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::mxfactorial-react-${lookup(var.environment, "${terraform.workspace}")}/*"
+    }
+  ]
+}
+POLICY
 
   website {
     index_document = "index.html"
@@ -9,10 +23,10 @@ resource "aws_s3_bucket" "mxfactorial-react" {
 }
 
 resource "aws_s3_bucket" "www-mxfactorial-react" {
-  bucket = "www-mxfactorial-react"
+  bucket = "www-mxfactorial-react-${lookup(var.environment, "${terraform.workspace}")}"
 
   website {
     # aws parses https prefix and bucket name when creating redirect rule
-    redirect_all_requests_to = "https://${aws_s3_bucket.mxfactorial-react.id}"
+    redirect_all_requests_to = "https://mxfactorial-react-${lookup(var.environment, "${terraform.workspace}")}"
   }
 }
