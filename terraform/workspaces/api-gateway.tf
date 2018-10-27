ff378339ab6a7908f1f24a1f7408691a716aa76d
max funk <mxfactorial@users.noreply.github.com>
2018-10-27 15:20:28 -0700

    Add logging to api gateway

--- terraform/workspaces/api-gateway.tf
+++ terraform/workspaces/api-gateway.tf
@@ -56,10 +56,60 @@ resource "aws_api_gateway_deployment" "environment" {
 resource "aws_lambda_permission" "mxfactorial_api_to_lambda" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.mxfactorial_graphql_server.arn}"
+  function_name = "${aws_lambda_function.mxfactorial_graphql_server.function_name}"
   principal     = "apigateway.amazonaws.com"
 
   # The /*/* portion grants access from any method on any resource
   # within the API Gateway "REST API".
   source_arn = "${aws_api_gateway_deployment.environment.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_account" "mxfactorial_api_account" {
+  cloudwatch_role_arn = "${aws_iam_role.cloudwatch.arn}"
+}
+
+resource "aws_iam_role" "cloudwatch" {
+  name = "mxfactorial-cloudwatch-role-${lookup(var.environment, "${terraform.workspace}")}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "cloudwatch" {
+  name = "mxfactorial-cloudwatch-policy-${lookup(var.environment, "${terraform.workspace}")}"
+  role = "${aws_iam_role.cloudwatch.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:FilterLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 }
