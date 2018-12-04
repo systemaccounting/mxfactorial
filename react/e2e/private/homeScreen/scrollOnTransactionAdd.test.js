700d7290a39f930c9efac72f7008e8867cec89e5
oozywaters <oozy.waters@gmail.com>
2018-12-05 00:12:46 +0300

    Update cache keys

--- react/e2e/private/homeScreen/scrollOnTransactionAdd.test.js
+++ react/e2e/private/homeScreen/scrollOnTransactionAdd.test.js
@@ -8,25 +8,27 @@ beforeAll(async () => {
   await page.waitForSelector(HOME_SELECTOR)
 })
 
-it('selecting add-item button scrolls window down', async () => {
-  let yScrollDiffs = []
-  const position0 = await page.evaluate(() => window.scrollY)
-  await addTransaction(page)
-  const position1 = await page.evaluate(() => window.scrollY)
-  yScrollDiffs.push(position1 - position0)
-  await addTransaction(page)
-  const position2 = await page.evaluate(() => window.scrollY)
-  yScrollDiffs.push(position2 - position1)
-  await addTransaction(page)
-  const position3 = await page.evaluate(() => window.scrollY)
-  yScrollDiffs.push(position3 - position2)
-  await addTransaction(page)
-  const position4 = await page.evaluate(() => window.scrollY)
-  yScrollDiffs.push(position4 - position3)
-  const inconsistencyTest = yScrollDiffs.filter(diff => {
-    return diff !== yScrollDiffs[0]
+describe('scroll home screen on add item', () => {
+  it('selecting add-item button scrolls window down', async () => {
+    let yScrollDiffs = []
+    const position0 = await page.evaluate(() => window.scrollY)
+    await addTransaction(page)
+    const position1 = await page.evaluate(() => window.scrollY)
+    yScrollDiffs.push(position1 - position0)
+    await addTransaction(page)
+    const position2 = await page.evaluate(() => window.scrollY)
+    yScrollDiffs.push(position2 - position1)
+    await addTransaction(page)
+    const position3 = await page.evaluate(() => window.scrollY)
+    yScrollDiffs.push(position3 - position2)
+    await addTransaction(page)
+    const position4 = await page.evaluate(() => window.scrollY)
+    yScrollDiffs.push(position4 - position3)
+    const inconsistencyTest = yScrollDiffs.filter(diff => {
+      return diff !== yScrollDiffs[0]
+    })
+    expect(
+      yScrollDiffs[yScrollDiffs.length - 1] - yScrollDiffs[0]
+    ).toBeGreaterThanOrEqual(90)
   })
-  expect(
-    yScrollDiffs[yScrollDiffs.length - 1] - yScrollDiffs[0]
-  ).toBeGreaterThanOrEqual(90)
 })
