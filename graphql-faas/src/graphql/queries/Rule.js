ee03701bdd28a2009048b33a16c16a799531de00
oozywaters <oozy.waters@gmail.com>
2018-12-18 19:08:22 +0300

    Change rules query arguments type

--- graphql-faas/src/graphql/queries/Rule.js
+++ graphql-faas/src/graphql/queries/Rule.js
@@ -6,12 +6,35 @@ const {
 } = require('../types/Transaction')
 const { GetRuleTransactionsResolver } = require('../resolvers/Rule')
 
+const TransactionItemType = new GraphQLObjectType({
+  name: 'TransactionItem',
+  description: 'Transaction Item',
+  fields: {
+    id: {
+      type: GraphQLID,
+      description: 'Transaction ID'
+    },
+    name: {
+      type: GraphQLString,
+      description: 'Name of good or service'
+    },
+    price: {
+      type: GraphQLString,
+      description: 'Value of good or service'
+    },
+    quantity: {
+      type: GraphQLString,
+      description: 'Quantity of good or service'
+    },
+  }
+})
+
 const RuleQueryType = () => ({
   type: new GraphQLList(TransactionType),
   description: 'Returns a list of modified transactions',
   args: {
     transactions: {
-      type: new GraphQLList(TransactionCreateType),
+      type: new GraphQLList(TransactionItemType),
       description: 'Please, specify transactions'
     }
   },
--- react/src/queries/rules.js
+++ react/src/queries/rules.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export default gql`
-  {
+  query fetchRules($transactions: [TransactionCreateType]) {
     rules(transactions: $transactions) {
       id
     }
