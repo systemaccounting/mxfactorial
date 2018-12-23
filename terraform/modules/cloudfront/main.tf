4c5ca4a1e0df3c51c63c8a96505d44531bbdd0ba
Enes Tüfekçi <enesxtufekci@gmail.com>
2018-12-23 22:11:02 +0100

    cloudfront module #1

---
+++ terraform/modules/cloudfront/main.tf
@@ -0,0 +1,52 @@
+resource "aws_cloudfront_distribution" "s3_react_distribution" {
+  comment = "${var.comment}"
+
+  origin {
+    domain_name = "${var.domain_name}"
+    origin_id   = "${var.origin_id}"
+  }
+
+  enabled         = true
+  aliases         = "${var.aliases}"
+  is_ipv6_enabled = true
+
+  default_cache_behavior {
+    allowed_methods        = ["HEAD", "GET"]
+    cached_methods         = ["HEAD", "GET"]
+    target_origin_id       = "${var.cloudfront_target_origin_id}"
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    min_ttl                = 0
+
+    default_ttl = 3600
+    max_ttl     = 86400
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  default_root_object = "index.html"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  viewer_certificate {
+    //https://github.com/terraform-providers/terraform-provider-aws/issues/2418#issuecomment-371192507
+    acm_certificate_arn = "${var.acm_certificate_arn}"
+    ssl_support_method  = "sni-only"
+  }
+}
---
+++ terraform/modules/cloudfront/variables.tf
@@ -0,0 +1,23 @@
+variable "comment" {
+  type = "string"
+}
+
+variable "aliases" {
+  type = "string"
+}
+
+variable "cloudfront_target_origin_id" {
+  type = "string"
+}
+
+variable "acm_certificate_arn" {
+  type = "string"
+}
+
+variable "domain_name" {
+  type = "string"
+}
+
+variable "origin_id" {
+  type = "string"
+}
