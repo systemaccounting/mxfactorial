6beab15f70c5c3ef3327e3955f1abbe8b94fcc54
oozywaters <oozy.waters@gmail.com>
2018-12-23 14:42:49 +0300

    Fix module output variables

--- terraform/modules/api-gateway/outputs.tf
+++ terraform/modules/api-gateway/outputs.tf
@@ -1,3 +1,7 @@
-output "domain_name" {
-  value = "${aws_api_gateway_domain_name.mxfactorial}"
+output "cloudfront_domain_name" {
+  value = "${aws_api_gateway_domain_name.mxfactorial.cloudfront_domain_name}"
+}
+
+output "cloudfront_zone_id" {
+  value = "${aws_api_gateway_domain_name.mxfactorial.cloudfront_zone_id}"
 }
