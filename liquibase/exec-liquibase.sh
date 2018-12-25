31b80ad56189e62abb4bebbfd5dfc3b19432af46
max funk <mxfactorial@users.noreply.github.com>
2018-12-24 20:12:52 -0800

    remove drafted aws cli commands

--- liquibase/exec-liquibase.sh
+++ liquibase/exec-liquibase.sh
@@ -8,16 +8,3 @@ liquibase \
 --password=$PASSWORD \
 --url=jdbc:mysql://$RDS_ENDPOINT:3306/mxfactorial?createDatabaseIfNotExist=true \
 $LIQUIBASE_COMMAND
-
-# https://docs.aws.amazon.com/cli/latest/reference/batch/submit-job.html
-# create batch job by replacing SOME_VALUE:
-# aws batch submit-job \
-# --region SOME_VALUE \
-# --job-name SOME_VALUE \
-# --job-queue SOME_VALUE \
-# --job-definition SOME_VALUE \
-# --container-overrides environment='[{name="BRANCH",value="SOME_VALUE"},{name="RDS_ENDPOINT",value="SOME_VALUE"},{name="USERNAME",value="SOME_VALUE"},{name="PASSWORD",value="SOME_VALUE"},{name="LIQUIBASE_COMMAND",value="SOME_VALUE"}]'
-
-# https://docs.aws.amazon.com/cli/latest/reference/batch/describe-jobs.html
-# query batch job status by replacing SOME_VALUE:
-# aws batch describe-jobs --region SOME_VALUE --jobs SOME_VALUE --query 'jobs[0].{status:status}'
