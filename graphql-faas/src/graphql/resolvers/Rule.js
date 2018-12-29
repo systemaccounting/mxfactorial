786848844b604aaa0ae8716d777fef879c8cc5ef
oozywaters <oozy.waters@gmail.com>
2018-12-29 23:03:45 +0300

    Add tax calculation to rules resolver

--- graphql-faas/src/graphql/resolvers/Rule.js
+++ graphql-faas/src/graphql/resolvers/Rule.js
@@ -1,5 +1,29 @@
+const TAX_TRANSACTION_NAME = '9% state sales tax'
+
 const GetRuleTransactionsResolver = args => {
-  return [{ id: '1' }]
+  // Remove any existing “9% state sales tax” item to avoid duplicating objects in the array
+  const accountItems = args.transactions.filter(item => {
+    return item.name !== TAX_TRANSACTION_NAME
+  })
+
+  // Add 9% sales tax.
+  let salesTaxValue = 0
+  accountItems.forEach(item => {
+    const { quantity = 1, price = 0 } = item
+    salesTaxValue += price * quantity * 0.09
+  })
+
+  if (salesTaxValue > 0) {
+    accountItems.push({
+      uuid: 'someUUID',
+      name: TAX_TRANSACTION_NAME,
+      price: salesTaxValue.toFixed(3),
+      quantity: 1,
+      creditor: 'StateOfCalifornia'
+    })
+  }
+
+  return accountItems
 }
 
 module.exports = {
