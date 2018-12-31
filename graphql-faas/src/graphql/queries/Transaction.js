47008edeed8fabb6d38cb338b3303959c929cd19
max funk <mxfactorial@users.noreply.github.com>
2018-12-30 20:11:49 -0800

    replace dynamodb with rds in graphql resolvers

--- graphql-faas/src/graphql/queries/Transaction.js
+++ graphql-faas/src/graphql/queries/Transaction.js
@@ -4,20 +4,23 @@ const {
 } = require('graphql')
 
 const { TransactionType } = require('../types/Transaction')
-const { GetTransactionResolver } = require('../resolvers/Transaction')
+const {
+  GetTransactionResolver,
+  connection
+} = require('../resolvers/Transaction')
 
 const TransactionQueryType = () => {
   return {
     type: new GraphQLList(TransactionType),
     description: 'Returns transactions',
     args: {
-      transaction: {
+      transactionId: {
         type: GraphQLString,
         description: 'Please specify transaction id'
       }
     },
     resolve(parentValue, args) {
-      return GetTransactionResolver(args.transaction)
+      return GetTransactionResolver(args.transactionId, connection)
     }
   }
 }
--- graphql-faas/src/graphql/resolvers/Transaction.js
+++ graphql-faas/src/graphql/resolvers/Transaction.js
@@ -1,127 +1,148 @@
-const AWS = require('aws-sdk')
-const uuidv1 = require('uuid/v1')
-AWS.config.update({ region: process.env.REGION })
-AWS.config.apiVersion = { dynamodb: '2012-08-10' }
-const documentClient = new AWS.DynamoDB.DocumentClient()
-// see aws_dynamodb_table name in envs/us-east-1/prod/dynamodb.tf
-const TABLE_NAME = 'transactions'
-const TIMEUUID = uuidv1()
+const mysql = require('mysql2')
 
+const connection = mysql.createConnection({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  database: 'mxfactorial',
+  connectTimeout: 30000 //serverless aurora
+})
 
-const GetTransactionResolver = arg => {
-  if (!arg) {
-    let params = {
-      TableName : TABLE_NAME,
-    }
-    console.log('Sending request...')
-    return documentClient.scan(params).promise()
-      .then(data => {
-        console.log('Receiving the data...')
-        let matchSchema = obj => {
-          let matchingObj = {}
-          matchingObj.id = obj.id
-          matchingObj.debitor = obj.debitor
-          matchingObj.debitor_profile_latlng = obj.debitor_profile_latlng
-          matchingObj.debitor_transaction_latlng = obj.debitor_transaction_latlng
-          matchingObj.debitor_approval_time = obj.debitor_approval_time
-          matchingObj.debitor_device = obj.debitor_device
-          matchingObj.creditor = obj.creditor
-          matchingObj.creditor_profile_latlng = obj.creditor_profile_latlng
-          matchingObj.creditor_transaction_latlng = obj.creditor_transaction_latlng
-          matchingObj.creditor_approval_time = obj.creditor_approval_time
-          matchingObj.creditor_device = obj.creditor_device
-          matchingObj.name = obj.name
-          matchingObj.price = obj.price
-          matchingObj.quantity = obj.quantity
-          matchingObj.unit_of_measurement = obj.unit_of_measurement
-          matchingObj.units_measured = obj.units_measured
-          matchingObj.rule_instance_id = obj.rule_instance_id
-          matchingObj.transaction_id = obj.transaction_id
-          matchingObj.debit_approver = obj.debit_approver
-          matchingObj.credit_approver = obj.credit_approver
-          matchingObj.author = obj.author
-          matchingObj.expiration_time = obj.expiration_time
-          matchingObj.creditor_rejection_time = obj.creditor_rejection_time
-          matchingObj.debitor_rejection_time = obj.debitor_rejection_time
-          return matchingObj
-        }
-        let modifiedItemsToMatchSchema = data.Items.map(matchSchema)
-        console.log(modifiedItemsToMatchSchema)
-        return modifiedItemsToMatchSchema
-      })
-      .catch(error => {
-        console.log('ERROR:', error)
-        return error
-      })
-  }
-  let params = {
-    TableName: TABLE_NAME,
-    ScanFilter: {
-      'id': {
-        ComparisonOperator: "CONTAINS",
-        AttributeValueList: [`${arg}`]
-      }
-    }
-  }
-  return documentClient.scan(params).promise()
-    .then(data => {
-      console.log(data.Items)
-      return data.Items
-    })
-    .catch(error => {
-      console.log('ERROR:', error)
-      return error
-    })
-}
-
-const AddTransactionResolver = arg => {
+const AddTransactionResolver = (arg, conn) => {
   if (!arg) {
     console.log(`Empty object received by resolver`)
     return `Please specify at least 1 transaction`
   }
-  let transactionRecord = arg
-  let params = {
-    TableName: TABLE_NAME,
-    Item: {
-      "id": TIMEUUID,
-      "debitor": transactionRecord.debitor,
-      "debitor_profile_latlng": transactionRecord.debitor_profile_latlng,
-      "debitor_transaction_latlng": transactionRecord.debitor_transaction_latlng,
-      "debitor_approval_time": transactionRecord.debitor_approval_time,
-      "debitor_device": transactionRecord.debitor_device,
-      "creditor": transactionRecord.creditor,
-      "creditor_profile_latlng": transactionRecord.creditor_profile_latlng,
-      "creditor_transaction_latlng": transactionRecord.creditor_transaction_latlng,
-      "creditor_approval_time": transactionRecord.creditor_approval_time,
-      "creditor_device": transactionRecord.creditor_device,
-      "name": transactionRecord.name,
-      "price": transactionRecord.price,
-      "quantity": transactionRecord.quantity,
-      "unit_of_measurement": transactionRecord.unit_of_measurement,
-      "units_measured": transactionRecord.units_measured,
-      "rule_instance_id": transactionRecord.rule_instance_id,
-      "transaction_id": transactionRecord.transaction_id,
-      "debit_approver": transactionRecord.debit_approver,
-      "credit_approver": transactionRecord.credit_approver,
-      "author": transactionRecord.author,
-      "expiration_time": transactionRecord.expiration_time,
-      "creditor_rejection_time": transactionRecord.creditor_rejection_time,
-      "debitor_rejection_time": transactionRecord.debitor_rejection_time
-    }
-  }
-  return documentClient.put(params).promise()
+  const transactionRecord = arg
+  var returnedRecord = transactionRecord
+  // conn.connect()
+  console.log(transactionRecord)
+  return conn.promise().query(
+    `INSERT INTO transactions (` +
+    `debitor,` +
+    `debitor_profile_latlng,` +
+    `debitor_transaction_latlng,` +
+    `debitor_approval_time,` +
+    `debitor_device,` +
+    `creditor,` +
+    `creditor_profile_latlng,` +
+    `creditor_transaction_latlng,` +
+    `creditor_approval_time,` +
+    `creditor_device,` +
+    `name,` +
+    `price,` +
+    `quantity,` +
+    `unit_of_measurement,` +
+    `units_measured,` +
+    `rule_instance_id,` +
+    `transaction_id,` +
+    `debit_approver,` +
+    `credit_approver,` +
+    `author,` +
+    `expiration_time,` +
+    `creditor_rejection_time,` +
+    `debitor_rejection_time` +
+    `) VALUES (` +
+    `?,?,?,?,?,?,?,?,?,?,` +
+    `?,?,?,?,?,?,?,?,?,?,` +
+    `?,?,?` +
+    `);`,
+    [
+    transactionRecord.debitor,
+    transactionRecord.debitor_profile_latlng,
+    transactionRecord.debitor_transaction_latlng,
+    transactionRecord.debitor_approval_time,
+    transactionRecord.debitor_device,
+    transactionRecord.creditor,
+    transactionRecord.creditor_profile_latlng,
+    transactionRecord.creditor_transaction_latlng,
+    transactionRecord.creditor_approval_time,
+    transactionRecord.creditor_device,
+    transactionRecord.name,
+    transactionRecord.price,
+    transactionRecord.quantity,
+    transactionRecord.unit_of_measurement,
+    transactionRecord.units_measured,
+    transactionRecord.rule_instance_id,
+    transactionRecord.transaction_id,
+    transactionRecord.debit_approver,
+    transactionRecord.credit_approver,
+    transactionRecord.author,
+    transactionRecord.expiration_time,
+    transactionRecord.creditor_rejection_time,
+    transactionRecord.debitor_rejection_time
+    ],
+    (err, rows, fields) => {
+      if (err) {
+        console.log(err)
+        return err
+      }
+      console.log(rows)
+      return rows
+    })
+  .then(data => {
+    // conn.destroy()
+    returnedRecord.id = data[0].insertId
+    console.log(data)
+    console.log(returnedRecord)
+    return returnedRecord
+  })
+  .catch(error => {
+    // conn.destroy()
+    console.log('ERROR:', error)
+    return error
+  })
+}
+
+const GetTransactionResolver = (arg, conn) => {
+  if (!arg) {
+    let emptyQuery = `SELECT * FROM transactions;`
+    return conn.promise().query(emptyQuery, [],
+      (err, rows, fields) => {
+        if (err) {
+          console.log(err)
+          return err
+        }
+        console.log(rows)
+        return rows
+      }
+    )
     .then(data => {
-      console.log(data)
-      return transactionRecord
+      // conn.destroy()
+      console.log(data[0])
+      return data[0]
     })
     .catch(error => {
+      // conn.destroy()
       console.log('ERROR:', error)
       return error
     })
+  }
+  let query = `SELECT * FROM transactions WHERE id = ?;`
+  return conn.promise().query(query, [arg],
+    (err, rows, fields) => {
+      if (err) {
+        console.log(err)
+        return err
+      }
+      console.log(rows)
+      return rows
+    }
+  )
+  .then(data => {
+    // conn.destroy()
+    console.log(data[0])
+    return data[0]
+  })
+  .catch(error => {
+    // conn.destroy()
+    console.log('ERROR:', error)
+    return error
+  })
 }
 
 module.exports = {
   GetTransactionResolver,
   AddTransactionResolver,
-  TIMEUUID
+  connection
 }
