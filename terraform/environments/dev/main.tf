0252b9b74f74ccf594b2bcb5336e5bfa257e822b
oozywaters <oozy.waters@gmail.com>
2019-01-05 23:46:00 +0300

    Change bucket name for dev environment

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     encrypt = true
-    bucket  = "global-mxfactorial-tf-state"
+    bucket  = "dev-mxfactorial-tf-state"
 
     // CHANGE ENVIRONMENT HERE
     dynamodb_table = "dev-mxfactorial-tf-state"
