776efa8674bfeef90e31f4e4a92b71fd35a8902d
oozywaters <oozy.waters@gmail.com>
2019-03-11 22:32:47 +0300

    Add sequelize

---
+++ services/transact-faas/src/storeTransactions.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const mysql = require('mysql2')
+
+const DB_NAME = 'mxfactorial'
+const DB_USERNAME = process.env.USER
+const DB_PASSWORD = process.env.PASSWORD
+const DB_HOST = process.env.HOST
+
+const storeTransactions = async transactions => {
+  const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: 'mysql',
+    pool: {
+      min: 0,
+      max: 5,
+      acquire: 30000,
+      idle: 10000
+    }
+  })
+
+  // Close connection
+  sequelize.close()
+}
+
+module.exports = storeTransactions
