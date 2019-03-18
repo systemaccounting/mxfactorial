7a883120ee56c7effb3a0cc893c944f38a276022
max funk <mxfactorial@users.noreply.github.com>
2019-03-17 21:37:35 -0700

    add cherry-picking

--- services/deploy.sh
+++ services/deploy.sh
@@ -1,11 +1,11 @@
 ENV=$1
 CURRENT_DIR=$(pwd)
 for dir in */; do
-  cd $CURRENT_DIR/$dir && yarn run deploy:$ENV
+  cd $CURRENT_DIR/$dir && . deploy.sh $ENV
 done
 
-# cherry-pick by commenting lines 3-5 above when commenting below in
-# DEPLOYING=$'rules-faas/\ntransact-faas/'
+# cherry-pick by commenting lines 3-5 above, then commenting below in
+# DEPLOYING=$'rules-faas/\ntransact-faas/\ngraphql-faas/'
 # for dir in $DEPLOYING; do
-#   cd $CURRENT_DIR/$dir && yarn run deploy:$ENV
+#   cd $CURRENT_DIR/$dir && . deploy.sh $ENV
 # done
