e03e64b13c513c1365770112bb327e9fa18a99de
oozywaters <oozy.waters@gmail.com>
2019-03-19 22:55:05 +0300

    Adapt rules service for invoking from graphql service

--- services/rules-faas/index.js
+++ services/rules-faas/index.js
@@ -1,13 +1,16 @@
 const applyRules = require('./src/applyRules')
 
 exports.handler = async event => {
-  const transactions = JSON.parse(event.body)
-  console.log('Received transactions: ', event.body)
-  const proxyResponse = {}
-  proxyResponse.body = JSON.stringify(applyRules(transactions))
-  proxyResponse.isBase64Encoded = false
-  proxyResponse.statusCode = 200
-  proxyResponse.headers = { 'Content-Type': 'application/json' }
-  proxyResponse.multiValueHeaders = {}
-  return proxyResponse
+  if (event.body) {
+    const transactions = JSON.parse(event.body)
+    console.log('Received transactions: ', event.body)
+    const proxyResponse = {}
+    proxyResponse.body = JSON.stringify(applyRules(transactions))
+    proxyResponse.isBase64Encoded = false
+    proxyResponse.statusCode = 200
+    proxyResponse.headers = { 'Content-Type': 'application/json' }
+    proxyResponse.multiValueHeaders = {}
+    return proxyResponse
+  }
+  return applyRules(event.transactions)
 }
