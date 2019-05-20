612d18566f19696dc3456da35141d005457fb573
max funk <mxfactorial@users.noreply.github.com>
2019-05-19 22:42:33 -0700

    add indirect var lookup in react start

--- react/start.sh
+++ react/start.sh
@@ -7,7 +7,8 @@ ENV=$1
 if [[ $CI ]]; then
   echo "Starting ${ENV} web server on CI..."
   UPPERCASE_ENV=$(echo $ENV | awk '{print toupper($0)}')
-  ENV_FILE=$(echo "${UPPERCASE_ENV}"_REACT_VARS | base64 --decode --ignore-garbage | tr -d '\0')
+  CI_VAR_FILE_NAME="${UPPERCASE_ENV}"_REACT_VARS
+  ENV_FILE=$(echo "${!CI_VAR_FILE_NAME//[[:space:]]/}" | base64 --decode --ignore-garbage | tr -d '\0')
   echo "${ENV_FILE}" > .env.current
   chmod u+r .env.current
   yarn run start:env
