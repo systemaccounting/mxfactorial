bed9660c78075d21accf47f783e24db20546fe28
oozywaters <oozy.waters@gmail.com>
2019-06-03 21:36:07 +0300

    Refetch transactions after createTransaction

--- react/src/decorators/withTransactions.js
+++ react/src/decorators/withTransactions.js
@@ -3,8 +3,9 @@ import { fetchTransactions } from 'queries/transactions'
 
 export default graphql(fetchTransactions, {
   options: props => ({ variables: { user: props.user.username } }),
-  props: ({ data: { transactions, loading } }) => ({
+  props: ({ data: { transactions, loading, refetch } }) => ({
     transactions,
+    refetchTransactions: refetch,
     isTransactionsLoading: loading
   })
 })
