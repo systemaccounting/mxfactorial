267080ffeea30910a8a1f4d8ad277fcc59839fe8
Ryan Ancheta <ryanjamesdeveloper@users.noreply.github.com>
2019-06-15 19:47:02 -0700

    made changes per pr comments

--- services/rules-faas/tests/applyRules.test.js
+++ services/rules-faas/tests/applyRules.test.js
@@ -1,94 +1,112 @@
-const applyRules  =  require('../src/applyRules') 
+const applyRules = require('../src/applyRules')
 const uuidv1 = require('uuid/v1')
 const TAX_TRANSACTION_NAME = '9% state sales tax'
 
 const transaction_items = [
-    {
-      name: 'Milk',
-      price: '4.4',
-      quantity: '2',
-      author: 'Joe Smith',
-      debitor: 'Joe Smith',
-      creditor: 'Mary'
-    },
-    {
-      name: 'Bread',
-      price: '6.25',
-      quantity: '5',
-      author: 'Joe Smith',
-      debitor: 'Joe Smith',
-      creditor: 'Mary'
-    },
-    {
-      name: 'Vitamins',
-      price: '24.23',
-      quantity: '1',
-      author: 'Joe Smith',
-      debitor: 'Joe Smith',
-      creditor: 'Mary'
-    },
-    {
-      name: 'NY Steak',
-      price: '12.54',
-      quantity: '2',
-      author: 'Joe Smith',
-      debitor: 'Joe Smith',
-      creditor: 'Mary'
-    },
-    {
-      name: 'Grapes',
-      price: '5.223',
-      quantity: '1',
-      author: 'Joe Namath',
-      debitor: 'Joe Smith',
-      creditor: 'Mary'
-    },
-    {
+  {
+    name: 'Milk',
+    price: '4.4',
+    quantity: '2',
+    author: 'JoeSmith',
+    debitor: 'JoeSmith',
+    creditor: 'Mary'
+  },
+  {
+    name: 'Bread',
+    price: '6.25',
+    quantity: '5',
+    author: 'JoeSmith',
+    debitor: 'JoeSmith',
+    creditor: 'Mary'
+  },
+  {
+    name: 'Vitamins',
+    price: '24.23',
+    quantity: '1',
+    author: 'JoeSmith',
+    debitor: 'JoeSmith',
+    creditor: 'Mary'
+  },
+  {
+    name: 'NY Steak',
+    price: '12.54',
+    quantity: '2',
+    author: 'JoeSmith',
+    debitor: 'JoeSmith',
+    creditor: 'Mary'
+  },
+  {
+    name: 'Grapes',
+    price: '5.223',
+    quantity: '1',
+    author: 'JoeNamath',
+    debitor: 'JoeSmith',
+    creditor: 'Mary'
+  },
+  {
     name: 'Paper Towels',
     price: '12.3348',
     quantity: '1',
-    author: 'Joe Namath',
-    debitor: 'Joe Smith',
-    creditor: 'Mary',
-    },
+    author: 'JoeNamath',
+    debitor: 'JoeSmith',
+    creditor: 'Mary'
+  }
 ]
 
-const with_sales_tax = applyRules(uuidv1(), transaction_items)
-
-describe('Apply Rules should verify', () => {
-    it('all non-rule objects have fixed number of properties', () => {
-        let length = Object.keys(with_sales_tax[0]).length
-        
-        with_sales_tax.forEach( item => { 
-            if(item.name !== TAX_TRANSACTION_NAME){
-                expect(Object.keys(item).length).toEqual(length)
-            } 
-        })
+describe('Apply Rules returns', () => {
+  it('identical property count for all user-generated objects', () => {
+    let withSalesTax = applyRules(uuidv1(), transaction_items)
+    let expectedPropertyCount = 6
+    with_sales_tax.forEach(item => {
+      if (item.name !== TAX_TRANSACTION_NAME) {
+        expect(Object.keys(item)).toHaveLength(expectedPropertyCount)
+      }
     })
+  })
 
-    it('decimal precision is fixed to thousandths', () => {
-        with_sales_tax.forEach( item => {
-            if( item.name == TAX_TRANSACTION_NAME ){
-                expect( item.price ).toEqual( parseFloat(item.price).toFixed(3) )
-            }
-        })
+  it('decimal precision is fixed to thousandths', () => {
+    let withSalesTax = applyRules(uuidv1(), transaction_items)
+    let ruleGeneratedObject = withSalesTax.filter(item => {
+      return !Object.keys(item).includes('rule_instance_id')
+    })[0]
+    expect(ruleGeneratedObject.price).toEqual('9.628')
+  })
+
+  it('+1 rule-generated object only', () => {
+    let withSalesTax = applyRules(uuidv1(), transaction_items)
+    let userGeneratedObjectCount = withSalesTax.filter(item => {
+      return !Object.keys(item).includes('rule_instance_id')
+    }).length
+    expect(withSalesTax).toHaveLength(userGeneratedObjectCount + 1)
+  })
+
+  it('checks debitor name match', () => {
+    let withSalesTax = applyRules(uuidv1(), transaction_items)
+    let userGeneratedItems = withSalesTax.filter(item => {
+      return item.name !== TAX_TRANSACTION_NAME
     })
-    
-    it('checks debitor name match', () => {
-        let items = with_sales_tax.filter( item => {
-            return item.name !== TAX_TRANSACTION_NAME
-        });
+    for (i = 0; i < userGeneratedItems.length; i++) {
+      expect(items[i].debitor).toEqual('JoeSmith')
+    }
+  })
 
-        for(i = 0; i < items.length; i++){
-            expect(items[i].author ).toEqual(transaction_items[i].author)
-        }
-    });
-
-    it('checks rule generated object has rule ID', () => {
-        with_sales_tax.forEach( item => {
-            if(item.name === TAX_TRANSACTION_NAME){
-                expect(Object.keys(item)).toContain('rule_instance_id')
-            }
-        })
+  it('ruleID property in rule-generated objects', () => {
+    let withSalesTax = applyRules(uuidv1(), transactionItems)
+    withSalesTax.forEach(item => {
+      if (item.name === TAX_TRANSACTION_NAME) {
+        expect(Object.keys(item)).toContain('rule_instance_id')
+      }
     })
+  })
+
+  it('ruleID property excluded from user-generated objects', () => {
+    let withSalesTax = applyRules(uuidv1(), transactionItems)
+    let userGeneratedItems = withSalesTax.filter(item => {
+      return item.name !== TAX_TRANSACTION_NAME
+    })
+    let userGeneratedItemsWithRuleID = userGeneratedItems.filter(item => {
+      return Object.keys(item).includes('rule_instance_id')
+    })
+    expect(userGeneratedItemsWithRuleID).toEqual(0)
+  })
 })
