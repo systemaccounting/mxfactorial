3bae00cf20deaf0c09529d50f1b1d1002f40fedb
oozywaters <oozy.waters@gmail.com>
2019-07-07 15:27:31 +0300

    Pass access token in every apollo request

--- react/src/providers/apollo.js
+++ react/src/providers/apollo.js
@@ -1,9 +1,18 @@
 import ApolloClient from 'apollo-boost'
+import Auth from '@aws-amplify/auth'
 
 const client = new ApolloClient({
   uri: `${process.env.REACT_APP_API_URL}`,
   fetchOptions: {
     credentials: 'include'
+  },
+  request: async operation => {
+    const { accessToken } = await Auth.currentSession()
+    operation.setContext({
+      headers: {
+        authorization: accessToken.jwtToken
+      }
+    })
   }
 })
 
