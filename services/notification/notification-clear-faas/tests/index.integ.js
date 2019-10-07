75c039883e527135d3a5fa4a1b8d26c5b4ccfc31
max funk <mxfactorial@users.noreply.github.com>
2019-10-06 22:36:01 -0700

    add integration test coverage to clear notification lambda

---
+++ services/notification/notification-clear-faas/tests/index.integ.js
@@ -0,0 +1,98 @@
+const AWS = require('aws-sdk')
+const WebSocket = require('ws')
+
+const {
+  createNotifications,
+  deleteNotifications,
+  createAccount,
+  deleteAccount,
+  getToken,
+  shapeClearNotificationsRequest
+} = require('./utils/integrationTestHelpers')
+
+const {
+  TEST_ACCOUNT,
+  pendingNotifications
+} = require('./utils/testData')
+
+const AWS_REGION = process.env.AWS_REGION
+const NOTIFICATIONS_TABLE_NAME = process.env.NOTIFICATIONS_TABLE_NAME
+const SECRET = process.env.SECRET
+const GET_NOTIFICATIONS_ACTION = {"action":"getnotifications"}
+const CLIENT_ID = process.env.CLIENT_ID
+const POOL_ID = process.env.POOL_ID
+const WSS_CLIENT_URL = process.env.WSS_CLIENT_URL
+
+const ddb = new AWS.DynamoDB.DocumentClient({ region: AWS_REGION })
+const cognitoIdsp = new AWS.CognitoIdentityServiceProvider()
+
+
+beforeAll(async () => {
+  await createAccount(cognitoIdsp, CLIENT_ID, TEST_ACCOUNT, SECRET)
+})
+
+afterAll(async() => {
+  await deleteAccount(cognitoIdsp, POOL_ID, TEST_ACCOUNT)
+})
+
+beforeEach(async () => {
+  await createNotifications(
+    ddb,
+    NOTIFICATIONS_TABLE_NAME,
+    pendingNotifications
+  )
+})
+
+afterEach(async () => {
+  await deleteNotifications(
+    ddb,
+    NOTIFICATIONS_TABLE_NAME,
+    pendingNotifications
+  )
+})
+
+describe('notification pending lambda', () => {
+  test('clears 3 pending messages', async done => {
+    let token = await getToken(cognitoIdsp, CLIENT_ID, TEST_ACCOUNT, SECRET)
+    let options = {
+      headers: { Authorization: token }
+    }
+    let ws = new WebSocket(WSS_CLIENT_URL, options)
+    ws.on('open', () => {
+      ws.send(JSON.stringify(GET_NOTIFICATIONS_ACTION))
+      ws.on('message', data => {
+        let event = JSON.parse(data)
+        if (event.message === 'Internal server error') {
+          ws.close()
+          done()
+          throw event
+        }
+        if (event.pending) {
+          let pending = event.pending
+          if (!pending.length) {
+            ws.close()
+            console.log('0 pending notifications available for test')
+            done()
+          }
+          let clearMessageRequest = shapeClearNotificationsRequest(pending)
+          ws.send(clearMessageRequest, () => {
+            ws.on('message', data => {
+              let event = JSON.parse(data)
+              if (event.cleared) {
+                let cleared = event.cleared
+                if (!cleared.length) {
+                  ws.close()
+                  console.log('0 cleared notifications available for test')
+                  done()
+                }
+                ws.close()
+                expect(cleared.length).toBe(3)
+                done()
+              }
+            })
+          })
+        }
+      })
+    })
+  }, 10000)
+})
