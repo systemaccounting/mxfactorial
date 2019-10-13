e000c1250333d9635cfb18f1a3bb5a729462e2a8
max funk <mxfactorial@users.noreply.github.com>
2019-10-13 15:54:21 -0700

    increase serverless rds wait before sleep

--- infrastructure/terraform/aws/modules/environment/rds.tf
+++ infrastructure/terraform/aws/modules/environment/rds.tf
@@ -19,7 +19,7 @@ resource "aws_rds_cluster" "default" {
     auto_pause               = true
     max_capacity             = 256
     min_capacity             = 2
-    seconds_until_auto_pause = 300
+    seconds_until_auto_pause = 1200
   }
 }
 
