4e6873ac92d0ef05b2588e7e358e693edc81c024
max funk <mxfactorial@users.noreply.github.com>
2019-10-19 16:38:11 -0700

    integrate node.js utility lambda with postgres

--- infrastructure/terraform/aws/modules/environment/common-bin/rds/index.js
+++ infrastructure/terraform/aws/modules/environment/common-bin/rds/index.js
@@ -1,55 +1,11 @@
-const mysql = require('mysql2')
-
-const connection = mysql.createConnection({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  database: 'mxfactorial',
-  connectTimeout: 30000 //serverless aurora
-})
+const { Client } = require('pg')
 
 exports.handler = async (event) => {
-  let deleteFakerQuery = `DELETE FROM transactions WHERE debitor = 'Faker';`
-    return await connection.promise().query(deleteFakerQuery, [],
-      (err, rows, fields) => {
-        if (err) {
-          console.log(err)
-          return err
-        }
-        console.log(rows)
-        return rows
-      }
-    )
-    .then(data => {
-      // connection.destroy()
-      console.log(data[0])
-      return data[0]
-    })
-    .catch(error => {
-      // connection.destroy()
-      console.log('ERROR:', error)
-      return error
-    })
+  let client = new Client()
+  await client.connect()
+  let res = await client.query("DELETE FROM transactions WHERE debitor = $1;", ['Faker'])
+  await client.end()
+  let msg = `Row count affected by ${res.command} command: ${res.rowCount}`
+  console.log(msg)
+  return msg
 }
-
-
-// https://github.com/aws/aws-sdk-js/issues/2376
-// RDSDataService not available in lambda-bundled aws-sdk 2.290.0 so
-// lambda using mysql2 (1mb) avoids uploading newer aws-sdk (38mb).
-// wait until lambda available Libraries updates to remove mysql2 package
-// const AWS = require('aws-sdk')
-// exports.handler = async (event) => {
-//   const rdsdataservice = new AWS.RDSDataService({apiVersion: '2018-08-01', region: 'us-east-1'})
-
-//   const params = {
-//     awsSecretStoreArn: 'rds-credentials-qa',
-//     dbClusterOrInstanceArn: 'mxfactorial-qa',
-//     sqlStatements: `DELETE FROM transactions WHERE debitor = 'Faker';`,
-//     database: 'mxfactorial'
-//   }
-
-//   rdsdataservice.executeSql(params, (err, data) => {
-//     if (err) console.log(err, err.stack)
-//     else console.log(data)
-//   })
-// }
