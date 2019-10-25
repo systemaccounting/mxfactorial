3e63e9b74bd9a39b388e86761ee56086392fcbb5
max funk <mxfactorial@users.noreply.github.com>
2019-10-24 22:47:30 -0700

    add unit test coverage to node.js transact service lambda

---
+++ services/transact-faas/src/storeTransactions.test.js
@@ -0,0 +1,59 @@
+const storeTransactions = require('./storeTransactions')
+const Sequelize = require('sequelize')
+
+jest.mock('sequelize', () => {
+  return jest.fn().mockImplementation(
+    () => {
+      return {
+        close: jest.fn().mockImplementation(
+          () => {
+            return {
+              then: jest.fn()
+            }
+          }
+        )
+      }
+    }
+  )
+})
+
+jest.mock('./models/Transaction', () => {
+  return jest.fn().mockImplementation(
+    () => {
+      return {
+        bulkCreate: jest.fn()
+      }
+    }
+  )
+})
+
+describe('storeTransactions', () => {
+  test('sequelize passed params', async () => {
+    process.env.PGDATABASE = 'testdb'
+    process.env.PGUSER = 'testuser'
+    process.env.PGPASSWORD = 'testpassword'
+    process.env.PGHOST = 'testhost'
+    process.env.PGPORT = 'testport'
+    let expectedConfig =     {
+      host: 'testhost',
+      operatorsAliases: false,
+      logging: console.log,
+      port: 'testport',
+      dialect: 'postgres',
+      pool: {
+        min: 0,
+        max: 5,
+        acquire: 30000,
+        idle: 10000,
+        handleDisconnects: true
+      }
+    }
+    await storeTransactions()
+    expect(Sequelize).toHaveBeenCalledWith(
+      'testdb',
+      'testuser',
+      'testpassword',
+      expectedConfig
+    )
+  })
+})
