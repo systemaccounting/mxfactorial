a26dc41d74cbd340727dde6e1ca310af26631361
max funk <mxfactorial@users.noreply.github.com>
2019-10-28 00:04:06 -0700

    lint react compose hoc

--- react/src/screens/LandingScreen/index.js
+++ react/src/screens/LandingScreen/index.js
@@ -4,4 +4,7 @@ import withUser from 'decorators/withUser'
 import withAuth from 'decorators/withAuth'
 import LandingScreen from './LandingScreen'
 
-export default compose(withUser, withAuth)(LandingScreen)
+export default compose(
+  withUser,
+  withAuth
+)(LandingScreen)
