37074d7566fdaf5fb772c922b78960b23b186517
max funk <mxfactorial@users.noreply.github.com>
2019-11-02 20:43:09 -0700

    add group transaction hoc to react request screen

--- react/src/screens/RequestScreen/index.js
+++ react/src/screens/RequestScreen/index.js
@@ -4,11 +4,13 @@ import withApi from 'decorators/withApi'
 import withAuth from 'decorators/withAuth'
 import withUser from 'decorators/withUser'
 import withTransactions from 'decorators/withTransactions'
+import withGroupedTransactions from '../../containers/GroupTransactionsContainer'
 import RequestScreen from './RequestScreen'
 
 export default compose(
   withUser,
   withAuth,
   withTransactions,
-  withApi
+  withApi,
+  withGroupedTransactions
 )(RequestScreen)
