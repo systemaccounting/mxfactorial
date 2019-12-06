d45d7f7edd1438be08901243bb75332eb8b02b99
max funk <mxfactorial@users.noreply.github.com>
2019-12-06 00:31:44 -0800

    add postgres timestamp trigger

--- schema/migrate-faas/migrations/1575600291930_websockets.js
+++ schema/migrate-faas/migrations/1575600291930_websockets.js
@@ -7,8 +7,23 @@ exports.up = pgm => {
     id: 'id',
     connection_id: { type: 'varchar(25)', notNull: true },
     created_at: { type: 'timestamp', notNull: true },
+    epoch_created_at: { type: 'bigint', notNull: true },
     account: { type: 'varchar(64)' }
   })
+
+  pgm.createTrigger(TABLE_NAME, 'convert_epoch_timestamp', {
+    when: 'BEFORE',
+    operation: 'INSERT',
+    level: 'ROW',
+    language: 'plpgsql',
+    replace: true
+  },
+  `
+  BEGIN
+    NEW.created_at = to_timestamp(NEW.epoch_created_at/1000);
+    RETURN NEW;
+  END;`
+  )
 }
 
 exports.down = pgm => {
