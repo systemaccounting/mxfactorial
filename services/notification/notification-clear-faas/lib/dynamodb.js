92303fb999cf875419e35c911101d15457cb3874
max funk <mxfactorial@users.noreply.github.com>
2019-12-07 10:00:11 -0800

    replace dynamodb with postgres in clear notification lambda

--- services/notification/notification-clear-faas/lib/dynamodb.js
+++ services/notification/notification-clear-faas/lib/dynamodb.js
@@ -1,61 +1,3 @@
-const updateItem = (
-  service,
-  table,
-  partitionKey,
-  partitionKeyValue,
-  newAttributeKey,
-  newAttributeValue,
-  ) => {
-  let params = {
-    TableName: table,
-    Key: {
-      [partitionKey]: partitionKeyValue
-    },
-    ExpressionAttributeNames: {
-      "#pk": partitionKey,
-      "#newkey": newAttributeKey,
-    },
-    ExpressionAttributeValues: {
-      ":pkv": partitionKeyValue,
-      ":newval": newAttributeValue,
-    },
-    UpdateExpression: `SET #newkey = :newval`,
-    ConditionExpression: `#pk = :pkv and attribute_not_exists(#newkey)`,
-    ReturnValues: "ALL_NEW"
-  }
-  return service.update(params)
-    .promise()
-    .then(async data => {
-      console.log("new attribute values added: ", JSON.stringify(data.Attributes))
-    })
-    .catch(async err => {
-      console.log(err, err.stack)
-      throw err
-    })
-}
-
-const queryTable = (service, table, key, val) => {
-  let params = {
-    TableName: table,
-    KeyConditions: {
-      [key]: {
-        ComparisonOperator: 'EQ',
-        AttributeValueList: [ val ]
-      }
-    }
-  }
-  return service.query(params)
-    .promise()
-    .then(async data => {
-      console.log(data.Items)
-      return data.Items
-    })
-    .catch(async err => {
-      console.log(err, err.stack)
-      throw err
-    })
-}
-
 const formatNotificationsToClear = notificationArray => {
   let ddbDeleteItems = []
   for (let i = 0; i < notificationArray.length; i++) {
@@ -91,33 +33,7 @@ const batchWriteTable = (service, table, formattedItems) => {
     })
 }
 
-const queryIndex = (service, table, indexName, key, val) => {
-  let params = {
-    TableName: table,
-    IndexName: indexName,
-    KeyConditions: {
-      [key]: {
-        ComparisonOperator: 'EQ',
-        AttributeValueList: [ val ]
-      }
-    }
-  }
-  return service.query(params)
-    .promise()
-    .then(async data => {
-      console.log(data.Items)
-      return data.Items
-    })
-    .catch(async err => {
-      console.log(err, err.stack)
-      throw err
-    })
-}
-
 module.exports = {
-  updateItem,
-  queryTable,
   formatNotificationsToClear,
   batchWriteTable,
-  queryIndex
 }
