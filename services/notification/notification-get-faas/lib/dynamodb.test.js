dd28a5289c7a2537ec82537199c510d098393655
max funk <mxfactorial@users.noreply.github.com>
2019-12-14 19:39:13 -0800

    incremental request unit test coverage in get lambda

--- services/notification/notification-get-faas/lib/dynamodb.test.js
+++ services/notification/notification-get-faas/lib/dynamodb.test.js
@@ -1,4 +1,8 @@
-const queryIndex = require('./dynamodb')
+const {
+  queryIndex,
+  setNotificationLimit,
+  computeRequestedNotificationCount
+} = require('./dynamodb')
 
 afterEach(() => {
   jest.clearAllMocks()
@@ -46,6 +50,69 @@ describe('dynamodb', () => {
       expected.KeyConditions.account.AttributeValueList[0]
     )
     await expect(ddb.query).toHaveBeenCalledWith(expected)
+  })
+
+  test('setNotificationLimit returns limit from exceeded limit', () => {
+    const testlimit = 20
+    const testrequested = 21
+    const result = setNotificationLimit(testlimit, testrequested)
+    expect(result).toBe(testlimit)
+  })
+
+  test('setNotificationLimit returns requested', () => {
+    const testlimit = 20
+    const testrequested = 19
+    const result = setNotificationLimit(testlimit, testrequested)
+    expect(result).toBe(testrequested)
+  })
+
+  test('setNotificationLimit returns limit requested', () => {
+    const testlimit = 20
+    const testrequested = 20
+    const result = setNotificationLimit(testlimit, testrequested)
+    expect(result).toBe(testrequested)
+  })
+
+  test('computeRequestedNotificationCount returns 20 from empty request count', () => {
+    const mockwebsocketmessage = {
+      action: "getnotifications",
+      token: "testtoken",
+    }
+    const result = computeRequestedNotificationCount(
+      mockwebsocketmessage,
+      setNotificationLimit,
+      parseInt(process.env.NOTIFICATION_RETRIEVAL_LIMIT_COUNT),
+    )
+    expect(result).toBe(20)
+  })
+
+  test('computeRequestedNotificationCount returns 20 from NaN request count', () => {
+    const mockwebsocketmessage = {
+      action: "getnotifications",
+      token: "testtoken",
+      count: "notanumber"
+    }
+    const result = computeRequestedNotificationCount(
+      mockwebsocketmessage,
+      setNotificationLimit,
+      parseInt(process.env.NOTIFICATION_RETRIEVAL_LIMIT_COUNT),
+    )
+    expect(result).toBe(20)
+  })
+
+  test('computeRequestedNotificationCount returns requested count', () => {
+    const expectedcount = 19
+    const mockwebsocketmessage = {
+      action: "getnotifications",
+      token: "testtoken",
+      count: expectedcount
+    }
+    const result = computeRequestedNotificationCount(
+      mockwebsocketmessage,
+      setNotificationLimit,
+      parseInt(process.env.NOTIFICATION_RETRIEVAL_LIMIT_COUNT),
+    )
+    expect(result).toBe(expectedcount)
   })
 
   // todo: test sendMessageToClient Data object type
