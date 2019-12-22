99ff8e33b9a82c67e23a00b3f61ce97df1e6ebbf
max funk <mxfactorial@users.noreply.github.com>
2019-12-22 14:57:22 -0800

    waitFor 1 sec in e2e to avoid failure on faster machines

--- react/e2e/private/notificationsMenu/notificationsMenu.test.js
+++ react/e2e/private/notificationsMenu/notificationsMenu.test.js
@@ -22,6 +22,7 @@ describe('Notifications menu', () => {
 
   it('should close menu on clear button click', async () => {
     const notificationBtn = await page.$(SELECTORS.notificationButton)
+    await page.waitFor(1000) // avoid Failed to execute 'send' on 'WebSocket': Still in CONNECTING state.
     notificationBtn.click()
     await page.waitForSelector(SELECTORS.notificationsMenu)
     expect(await page.$$(SELECTORS.notificationsMenu)).toHaveLength(1)
