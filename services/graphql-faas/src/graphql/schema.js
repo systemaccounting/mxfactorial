557096a9b06d3e30b7ace853f67bdf6d8ac8e790
max funk <mxfactorial@users.noreply.github.com>
2019-12-28 01:06:19 -0800

    graphql schema receives requests, transactions and rule instances

--- services/graphql-faas/src/graphql/schema.js
+++ services/graphql-faas/src/graphql/schema.js
@@ -1,27 +1,46 @@
-const { GraphQLObjectType, GraphQLSchema } = require('graphql')
+const { GraphQLObjectType, GraphQLSchema, GraphQLID } = require('graphql')
 
-const { TransactionQueryType } = require('./queries/Transaction')
-const { RuleQueryType } = require('./queries/Rule')
-const { TransactionCreateMutation } = require('./mutations/Transaction')
+const {
+  TransactionQueryByIDType,
+  TransactionQueryByAccountType
+} = require('./queries/Transaction')
 
-const RootQuery = new GraphQLObjectType({
-  name: 'Read',
-  description: 'Root query of app',
+const {
+  RequestQueryByIDType,
+  RequestQueryByAccountType
+} = require('./queries/Request')
+const {
+  RequestCreateMutation,
+  RequestApproveMutation
+} = require('./mutations/Request')
+const {
+  RuleQueryType,
+  RuleInstanceQueryType
+} = require('./queries/Rule')
+
+const Query = new GraphQLObjectType({
+  name: 'read',
+  description: 'queries',
   fields: {
-    transactions: TransactionQueryType(),
-    rules: RuleQueryType()
+    transactionsByID: TransactionQueryByIDType(),
+    transactionsByAccount: TransactionQueryByAccountType(),
+    requestsByID: RequestQueryByIDType(),
+    requestsByAccount: RequestQueryByAccountType(),
+    rules: RuleQueryType(),
+    ruleInstances: RuleInstanceQueryType()
   }
 })
 
-const RootMutation = new GraphQLObjectType({
-  name: 'Write',
-  description: 'Root mutation of app',
+const Mutation = new GraphQLObjectType({
+  name: 'write',
+  description: 'mutations',
   fields: {
-    createTransaction: TransactionCreateMutation()
+    createRequest: RequestCreateMutation(),
+    approveRequest: RequestApproveMutation()
   }
 })
 
 module.exports = new GraphQLSchema({
-  query: RootQuery,
-  mutation: RootMutation
+  query: Query,
+  mutation: Mutation
 })
