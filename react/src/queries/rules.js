8e7b06191e96154d4b7b5a9691c1e58fe4e1bb54
max funk <mxfactorial@users.noreply.github.com>
2019-12-28 00:28:24 -0800

    remove deprecated field from graphql query in react

--- react/src/queries/rules.js
+++ react/src/queries/rules.js
@@ -3,7 +3,6 @@ import gql from 'graphql-tag'
 const fetchRules = gql`
   query fetchRules($transactions: [TransactionInputType]) {
     rules(transactions: $transactions) {
-      uuid
       name
       price
       quantity
