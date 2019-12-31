350effbe8bcda2431bdf82c3f1acef8a8b53d2d4
max funk <mxfactorial@users.noreply.github.com>
2019-12-31 05:49:03 -0800

    request-approve lambda integration test helpers

---
+++ services/request-approve-faas/tests/utils/integrationTestHelpers.js
@@ -0,0 +1,93 @@
+const invokeLambda = (
+  service,
+  lambdaArn,
+  items,
+  graphqlRequestSender
+  ) => {
+  const params = {
+    FunctionName: lambdaArn,
+    Payload: JSON.stringify({ items, graphqlRequestSender })
+  }
+  return service
+    .invoke(params)
+    .promise()
+    .then(data => {
+      let response = JSON.parse(data.Payload)
+      return response
+    })
+    .catch(err => {
+      console.log(err.message)
+      throw new Error(err.message)
+    })
+}
+
+const queryPgTable = (service, key, val) => {
+  return service.findAll({
+    where: { [key]: val }
+  })
+}
+
+const deleteFromPgTable = (service, key, val) => service.destroy(
+  {
+    where: { [key]: val },
+    returning: 1
+  }
+)
+
+const queryDynamaDbTable = (service, table, hashKey, hashVal) => {
+  let params = {
+    TableName: table,
+    KeyConditionExpression: '#hkey = :hval',
+    ExpressionAttributeNames: {
+      '#hkey': hashKey
+    },
+    ExpressionAttributeValues: {
+      ':hval': hashVal
+    }
+  }
+  return service.query(params)
+    .promise()
+    .then(async data => {
+      // console.log(data.Items)
+      return data.Items
+    })
+    .catch(async err => {
+      console.log(err, err.stack)
+      throw err
+    })
+}
+
+const deleteMultipleNotifications = (service, table, notifications) => {
+  let ddbDeleteItems = []
+  for (let i = 0; i < notifications.length; i++) {
+    let { uuid, timestamp } = notifications[i]
+    ddbDeleteItems.push({
+      DeleteRequest: {
+        Key: { uuid, timestamp }
+      }
+    })
+  }
+  let params = {
+    RequestItems: {
+      [table]: ddbDeleteItems
+    }
+  }
+  return service.batchWrite(params)
+    .promise()
+    .then(data => {
+      // console.log(data)
+      if (!Object.keys(data.UnprocessedItems).length) {
+        return
+      }
+    })
+    .catch(err => console.log(err, err.stack))
+}
+
+
+module.exports = {
+  invokeLambda,
+  queryPgTable,
+  deleteFromPgTable,
+  queryDynamaDbTable,
+  deleteMultipleNotifications
+}
--- services/request-approve-faas/tests/utils/testData.js
+++ services/request-approve-faas/tests/utils/testData.js
@@ -1,12 +1,26 @@
+const uuid = require('uuid/v1')
+
+const fakerAccountWithSevenRandomDigits = () => {
+  const num = Math.floor(Math.random() * (9999999 - 1000000)) + 1000000
+  return 'Faker' + num.toString()
+}
+
+const TEST_ACCOUNTS = [
+  fakerAccountWithSevenRandomDigits(),
+  fakerAccountWithSevenRandomDigits()
+]
+
+const transactionID = uuid()
+
 const itemsUnderTestArray = [
   {
     name: 'Milk',
     price: '3',
     quantity: '2',
-    author: 'Joe Smith',
-    debitor: 'Joe Smith',
-    creditor: 'Mary',
-    // transaction_id: '662bc1a0-ed24-11e9-90ac-fd8810fc35b7'
+    author: TEST_ACCOUNTS[1],
+    debitor: TEST_ACCOUNTS[0],
+    creditor: TEST_ACCOUNTS[1],
+    transaction_id: transactionID
   }
 ]
 
@@ -15,18 +29,42 @@ const itemsStandardArray = [
     name: 'Milk',
     price: '3',
     quantity: '2',
-    author: 'Joe Smith',
-    debitor: 'Joe Smith',
-    creditor: 'Mary'
+    author: TEST_ACCOUNTS[1],
+    debitor: TEST_ACCOUNTS[0],
+    creditor: TEST_ACCOUNTS[1],
+    transaction_id: transactionID
   },
   {
     name: '9% state sales tax',
     quantity: 1,
     price: '0.540',
-    author: 'Joe Smith',
-    debitor: 'Joe Smith',
+    author: TEST_ACCOUNTS[1],
+    debitor: TEST_ACCOUNTS[0],
     creditor: 'StateOfCalifornia',
-    rule_instance_id: "8f93fd20-e60b-11e9-a7a9-2b4645cb9b8d"
+    rule_instance_id: "8f93fd20-e60b-11e9-a7a9-2b4645cb9b8d",
+    transaction_id: transactionID
+  }
+]
+
+const debitorStandardArray = [
+  {
+    name: 'Milk',
+    price: '3',
+    quantity: '2',
+    author: TEST_ACCOUNTS[0],
+    debitor: TEST_ACCOUNTS[1],
+    creditor: TEST_ACCOUNTS[0],
+    transaction_id: transactionID
+  },
+  {
+    name: '9% state sales tax',
+    quantity: 1,
+    price: '0.540',
+    author: TEST_ACCOUNTS[0],
+    debitor: TEST_ACCOUNTS[1],
+    creditor: 'StateOfCalifornia',
+    rule_instance_id: "8f93fd20-e60b-11e9-a7a9-2b4645cb9b8d",
+    transaction_id: transactionID
   }
 ]
 
@@ -35,14 +73,14 @@ const testedItemsIntendedForStorage = [
     name: 'Milk',
     price: '3',
     quantity: '2',
-    author: 'Joe Smith',
-    debitor: 'Joe Smith',
-    creditor: 'Mary',
-    transaction_id: '662bc1a0-ed24-11e9-90ac-fd8810fc35b7'
+    author: TEST_ACCOUNTS[1],
+    debitor: TEST_ACCOUNTS[0],
+    creditor: TEST_ACCOUNTS[1],
+    transaction_id: transactionID
   }
 ]
 
-const testRule = `let TAX_TRANSACTION_NAME = '9% state sales tax'; let accountItems = items.filter(item => {   return item.name !== TAX_TRANSACTION_NAME; }); let salesTaxValue = 0; accountItems.forEach(item => {   let quantity = item.quantity || 1;   let price = item.price || 0;   salesTaxValue += price * quantity * 0.09; }); if (salesTaxValue > 0) {   accountItems.push({     author: accountItems[0].author,     rule_instance_id: ruleId,     name: TAX_TRANSACTION_NAME,     price: salesTaxValue.toFixed(3),     quantity: 1,     creditor: 'StateOfCalifornia',     debitor: accountItems[0].debitor   }); }; console.log('Applied rules: ', JSON.stringify(accountItems)); return accountItems;`
+const testRule = `let TAX_TRANSACTION_NAME = '9% state sales tax'; let accountItems = items.filter(item => {   return item.name !== TAX_TRANSACTION_NAME; }); let salesTaxValue = 0; accountItems.forEach(item => {   let quantity = item.quantity || 1;   let price = item.price || 0;   salesTaxValue += price * quantity * 0.09; }); if (salesTaxValue > 0) {   accountItems.push({     author: accountItems[0].author,     rule_instance_id: ruleId,     name: TAX_TRANSACTION_NAME,     price: salesTaxValue.toFixed(3),     quantity: 1,     creditor: 'StateOfCalifornia',     creditor_approval_time: new Date().toISOString(),     debitor: accountItems[0].debitor,     transaction_id: accountItems[0].transaction_id   }); }; console.log('Applied rules: ', JSON.stringify(accountItems)); return accountItems;`
 
 const testRuleInstances = [
   {
@@ -60,7 +98,9 @@ const testNotification =  {
 module.exports = {
   itemsUnderTestArray,
   itemsStandardArray,
+  debitorStandardArray,
   testedItemsIntendedForStorage,
   testRuleInstances,
-  testNotification
+  testNotification,
+  TEST_ACCOUNTS
 }
