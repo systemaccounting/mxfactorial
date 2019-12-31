3237d23951a2666bbeafb3cc64bb4c340bb5e7e5
max funk <mxfactorial@users.noreply.github.com>
2019-12-31 05:44:36 -0800

    add approval timestamp to graphql unit test rule

--- services/graphql-faas/tests/utils/testData.js
+++ services/graphql-faas/tests/utils/testData.js
@@ -36,7 +36,7 @@ const itemsStandardArray = [
   }
 ]
 
-const testRule = `let TAX_TRANSACTION_NAME = '9% state sales tax'; let accountItems = items.filter(item => {   return item.name !== TAX_TRANSACTION_NAME; }); let salesTaxValue = 0; accountItems.forEach(item => {   let quantity = item.quantity || 1;   let price = item.price || 0;   salesTaxValue += price * quantity * 0.09; }); if (salesTaxValue > 0) {   accountItems.push({     author: accountItems[0].author,     rule_instance_id: ruleId,     name: TAX_TRANSACTION_NAME,     price: salesTaxValue.toFixed(3),     quantity: 1,     creditor: 'StateOfCalifornia',     debitor: accountItems[0].debitor   }); }; console.log('Applied rules: ', JSON.stringify(accountItems)); return accountItems;`
+const testRule = `let TAX_TRANSACTION_NAME = '9% state sales tax'; let accountItems = items.filter(item => {   return item.name !== TAX_TRANSACTION_NAME; }); let salesTaxValue = 0; accountItems.forEach(item => {   let quantity = item.quantity || 1;   let price = item.price || 0;   salesTaxValue += price * quantity * 0.09; }); if (salesTaxValue > 0) {   accountItems.push({     author: accountItems[0].author,     rule_instance_id: ruleId,     name: TAX_TRANSACTION_NAME,     price: salesTaxValue.toFixed(3),     quantity: 1,     creditor: 'StateOfCalifornia',     creditor_approval_time: new Date().toISOString(),     debitor: accountItems[0].debitor,     transaction_id: accountItems[0].transaction_id   }); }; console.log('Applied rules: ', JSON.stringify(accountItems)); return accountItems;`
 
 const testRuleInstances = [
   {
