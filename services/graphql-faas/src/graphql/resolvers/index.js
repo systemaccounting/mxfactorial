fbedeb779a37be9e8b93a7079856c7f9fdc6d225
max funk <mxfactorial@users.noreply.github.com>
2020-01-04 03:46:10 -0800

    pass graphql resolvers dependencies

--- services/graphql-faas/src/graphql/resolvers/index.js
+++ services/graphql-faas/src/graphql/resolvers/index.js
@@ -9,39 +9,87 @@ const {
 const GetTransactionsResolver = require('./Transaction')
 
 const {
+  getRules,
+  queryTable,
   GetRuleTransactionsResolver,
   GetRuleInstanceResolver
 } = require('./Rule')
 
+const {
+  goLambdaPromiseHandler,
+  nodeLambdaPromiseHandler
+} = require('./PromiseHandlers')
+
 const lambda = new AWS.Lambda()
+const ddb = new AWS.DynamoDB.DocumentClient({
+  region: process.env.AWS_REGION
+})
 
 module.exports = {
   Query: {
     transactionsByID: (parentValue, args, ctx) => {
-      return GetTransactionsResolver(lambda, args, ctx.graphqlRequestSender)
+      return GetTransactionsResolver(
+        lambda,
+        goLambdaPromiseHandler,
+        args,
+        ctx.graphqlRequestSender
+      )
     },
     transactionsByAccount: (parentValue, args, ctx) => {
-      return GetTransactionsResolver(lambda, args, ctx.graphqlRequestSender)
+      return GetTransactionsResolver(
+        lambda,
+        goLambdaPromiseHandler,
+        args,
+        ctx.graphqlRequestSender
+      )
     },
     requestsByID: (parentValue, args, ctx) => {
-      return GetRequestResolver(lambda, args, ctx.graphqlRequestSender)
+      return GetRequestResolver(
+        lambda,
+        goLambdaPromiseHandler,
+        args,
+        ctx.graphqlRequestSender
+      )
     },
     requestsByAccount: (parentValue, args, ctx) => {
-      return GetRequestResolver(lambda, args, ctx.graphqlRequestSender)
+      return GetRequestResolver(
+        lambda,
+        goLambdaPromiseHandler,
+        args,
+        ctx.graphqlRequestSender
+      )
     },
     rules: (parentValue, args) => {
-      return GetRuleTransactionsResolver(args)
+      return GetRuleTransactionsResolver(
+        lambda,
+        args
+      )
     },
     ruleInstances: (parentValue, args) => {
-      return GetRuleInstanceResolver(args.keySchema)
+      return GetRuleInstanceResolver(
+        ddb,
+        args.keySchema,
+        getRules,
+        queryTable
+      )
     },
   },
   Mutation: {
     createRequest: (parentValue, args, ctx) => {
-      return CreateRequestResolver(lambda, args, ctx.graphqlRequestSender)
+      return CreateRequestResolver(
+        lambda,
+        nodeLambdaPromiseHandler,
+        args,
+        ctx.graphqlRequestSender
+      )
     },
     approveRequest: (parentValue, args, ctx) => {
-      return ApproveRequestResolver(lambda, args, ctx.graphqlRequestSender)
+      return ApproveRequestResolver(
+        lambda,
+        nodeLambdaPromiseHandler,
+        args,
+        ctx.graphqlRequestSender
+      )
     }
   }
 }
