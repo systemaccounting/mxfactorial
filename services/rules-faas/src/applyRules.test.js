72cdfca7354dc03d828caa2e0be7c2d21a64be87
max funk <mxfactorial@users.noreply.github.com>
2020-01-04 04:12:11 -0800

    set test data values in rules lambda unit tests

--- services/rules-faas/src/applyRules.test.js
+++ services/rules-faas/src/applyRules.test.js
@@ -5,22 +5,34 @@ const {
 } = require('./applyRules')
 
 const {
-  itemsUnderTestArray,
-  itemsStandardArray,
+  fakerAccountWithSevenRandomDigits,
+  createRequestData,
   testRuleInstances
 } = require('../tests/utils/testData')
+
+// set test values in modules to avoid failure from
+// teardown of shared values in unfinished parallel tests
+const TEST_DEBITOR = fakerAccountWithSevenRandomDigits()
+const TEST_CREDITOR = fakerAccountWithSevenRandomDigits()
+const debitRequest = createRequestData(
+  TEST_DEBITOR,
+  TEST_CREDITOR,
+  'debit'
+)
+
+const taxExcluded = [ debitRequest[0] ]
 
 describe('applyRules', () => {
   test('rules applied to transactions', () => {
     let ruleIdParam = 'ruleId'
     let itemsParam = 'items'
     // avoid testing unpredictable approval time
-    let itemsWithoutApprovalTime = itemsStandardArray.map(
+    let itemsWithoutApprovalTime = debitRequest.map(
       // https://stackoverflow.com/a/46839399
       ({ creditor_approval_time, ...rest }) => rest
     )
     let result = applyRules(
-      itemsUnderTestArray,
+      taxExcluded,
       testRuleInstances,
       ruleIdParam,
       itemsParam
