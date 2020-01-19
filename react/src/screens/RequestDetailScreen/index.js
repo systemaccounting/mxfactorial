df051d701320b8407b7c361022b0a318bded83f4
oozywaters <oozy.waters@gmail.com>
2020-01-19 18:57:43 +0300

    Add request detail screen render props unit test

--- react/src/screens/RequestDetailScreen/index.js
+++ react/src/screens/RequestDetailScreen/index.js
@@ -7,37 +7,39 @@ import withUser from 'decorators/withUser'
 import { fetchRequestById } from 'queries/requests'
 import RequestDetailScreen from './RequestDetailScreen'
 
+export function renderProps({ data, ownProps }) {
+  const { username } = ownProps.user
+  // find non-rule generated request
+  const request =
+    data.requestsByID && data.requestsByID.length
+      ? data.requestsByID.find(item => !item.rule_instance_id)
+      : {}
+  const isCredit = request.creditor === username
+  const requestTotal = data.requestsByID
+    ? data.requestsByID.reduce(
+        (sum, item) => sum + item.price * item.quantity,
+        0
+      )
+    : 0
+
+  return {
+    isRequestLoading: data.loading,
+    isCredit,
+    requestTotal,
+    transactionId: request.transaction_id,
+    requestingAccount: isCredit ? request.debitor : request.creditor,
+    requestTime:
+      request.creditor_approval_time || request.debitor_approval_time,
+    requestItems: data.requestsByID || [],
+    ruleInstanceId: data.requestsByID
+      ? data.requestsByID.find(item => item.rule_instance_id).rule_instance_id
+      : ''
+  }
+}
+
 const withTransaction = graphql(fetchRequestById, {
   options: props => ({ variables: { transactionId: props.match.params.uuid } }),
-  props: ({ data, ownProps }) => {
-    const { username } = ownProps.user
-    // find non-rule generated request
-    const request =
-      data.requestsByID && data.requestsByID.length
-        ? data.requestsByID.find(item => !item.rule_instance_id)
-        : {}
-    const isCredit = request.creditor === username
-    const requestTotal = data.requestsByID
-      ? data.requestsByID.reduce(
-          (sum, item) => sum + item.price * item.quantity,
-          0
-        )
-      : 0
-
-    return {
-      isRequestLoading: data.loading,
-      isCredit,
-      requestTotal,
-      transactionId: request.transaction_id,
-      requestingAccount: isCredit ? request.debitor : request.creditor,
-      requestTime:
-        request.creditor_approval_time || request.debitor_approval_time,
-      requestItems: data.requestsByID || [],
-      ruleInstanceId: data.requestsByID
-        ? data.requestsByID.find(item => item.rule_instance_id).rule_instance_id
-        : 0
-    }
-  }
+  props: renderProps
 })
 
 export default compose(withApi, withUser, withTransaction)(RequestDetailScreen)
