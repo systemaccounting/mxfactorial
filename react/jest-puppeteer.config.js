21efb4f74d70923b148535a23f1628a17a774c4f
oozywaters <oozy.waters@gmail.com>
2020-07-18 12:44:53 +0300

    Add decorators unit tests

--- react/jest-puppeteer.config.js
+++ react/jest-puppeteer.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  server: {
-    command: 'yarn start:env',
-    port: 3000,
-    launchTimeout: 30000
-  },
+  // server: {
+  //   command: 'yarn start:env',
+  //   port: 3000,
+  //   launchTimeout: 30000
+  // },
   launch: {
     dumpio: false,
+    // headless: false,
+    // slowMo: 100,
     headless: process.env.HEADLESS !== 'false'
   },
   browserContext: 'default'
