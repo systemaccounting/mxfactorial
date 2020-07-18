f4b0c5e1441f26bcee37244e436561ba0f8bfeb4
oozywaters <oozy.waters@gmail.com>
2020-07-18 13:54:56 +0300

    Revert jest config

--- react/jest-puppeteer.config.js
+++ react/jest-puppeteer.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  // server: {
-  //   command: 'yarn start:env',
-  //   port: 3000,
-  //   launchTimeout: 30000
-  // },
+  server: {
+    command: 'yarn start:env',
+    port: 3000,
+    launchTimeout: 30000
+  },
   launch: {
     dumpio: false,
-    // headless: false,
-    // slowMo: 100,
     headless: process.env.HEADLESS !== 'false'
   },
   browserContext: 'default'
