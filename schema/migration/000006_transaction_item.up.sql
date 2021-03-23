b9f89d6b38c5a94c74bf1620df213ea716eec04c
max funk <mxfactorial@users.noreply.github.com>
2021-03-23 16:05:40 -0700

    transaction_item migrations

---
+++ schema/migration/000006_transaction_item.up.sql
@@ -0,0 +1,36 @@
+-- added to transaction by authors
+CREATE TABLE transaction_item (
+  id SERIAL PRIMARY KEY,
+  transaction_id integer NOT NULL,
+  item_id character varying(255) NOT NULL, -- temporary until item inserts added in code
+  price numeric NOT NULL CHECK (price > 0),
+  quantity numeric NOT NULL CHECK (quantity > 0),
+  rule_instance_id character varying(255), -- todo: change to int and add foreign key
+  unit_of_measurement character varying(255),
+  units_measured numeric,
+  debitor character varying(255) not null,
+  creditor character varying(255) not null,
+  debitor_profile_id int,
+  creditor_profile_id int,
+  debitor_approval_time timestamptz,
+  creditor_approval_time timestamptz,
+  debitor_expiration_time timestamptz,
+  creditor_expiration_time timestamptz,
+  debitor_rejection_time timestamptz,
+  creditor_rejection_time timestamptz,
+  CONSTRAINT fk_transaction_id
+    FOREIGN KEY(transaction_id)
+      REFERENCES transaction(id)
+  -- CONSTRAINT fk_debitor_profile_id -- todo
+  --   FOREIGN KEY(debitor_profile_id)
+  --     REFERENCES account_profile(id),
+  -- CONSTRAINT fk_creditor_profile_id -- todo
+  --   FOREIGN KEY(creditor_profile_id)
+  --     REFERENCES account_profile(id),
+  -- CONSTRAINT fk_rule_instance_id -- todo
+  --   FOREIGN KEY(rule_instance_id)
+  --     REFERENCES rule_instance(id),
+  -- CONSTRAINT fk_item_id -- todo
+  --   FOREIGN KEY(item_id)
+  --     REFERENCES item(id)
+);
