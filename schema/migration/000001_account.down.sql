9e2f2f5556b76e53e11f59dd69dd33c936c56e78
max funk <mxfactorial@users.noreply.github.com>
2021-03-23 16:04:51 -0700

    account migrations

---
+++ schema/migration/000001_account.down.sql
@@ -0,0 +1,15 @@
+DROP TABLE account_setting;
+
+DROP TRIGGER add_account_owner ON account;
+
+DROP FUNCTION add_account_owner;
+
+DROP TABLE account_owner;
+
+DROP TRIGGER add_subaccount ON account;
+
+DROP FUNCTION add_subaccount;
+
+DROP TABLE subaccount;
+
+DROP TABLE account;
