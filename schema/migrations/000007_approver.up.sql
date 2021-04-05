35cda32b5fc745a6fc9a1b61d2ec704d6b83f725
max funk <mxfactorial@users.noreply.github.com>
2021-04-05 09:15:12 -0700

    remove postgres approver function and trigger

--- schema/migrations/000007_approver.up.sql
+++ schema/migrations/000007_approver.up.sql
@@ -6,6 +6,7 @@
 -- on approval from all item approvers
 CREATE TABLE approver (
   id SERIAL PRIMARY KEY,
+  rule_instance_id int,
   transaction_id INTEGER NOT NULL,
   transaction_item_id INTEGER,
   account_name character varying(255) NOT NULL,
@@ -14,6 +15,10 @@ CREATE TABLE approver (
   device_latlng character varying(255), -- todo: inventory devices in separate table similar to items
   approval_time timestamptz,
   rejection_time timestamptz,
+  expiration_time timestamptz,
+  CONSTRAINT fk_rule_instance_id
+    FOREIGN KEY(rule_instance_id)
+      REFERENCES rule_instance(id),
   CONSTRAINT fk_transaction_id
     FOREIGN KEY(transaction_id)
       REFERENCES transaction(id),
@@ -36,55 +41,3 @@ CREATE TABLE approver (
       END
     )
 );
-
--- a transaction_item is approved by account_owner(s)
--- account_owner auto-added when transaction_item created
-CREATE OR REPLACE FUNCTION add_approver()
-	RETURNS trigger AS
-$$
-DECLARE
-  current_transaction transaction%ROWTYPE;
-  tested_author_role text; -- todo: switch to enum after above noted go-migrate feature deployed
-BEGIN
-    -- store current transaction in
-    -- memory for value test and insert
-    SELECT *
-    INTO current_transaction
-    FROM transaction
-    WHERE id = NEW.transaction_id;
-
-    -- test for transaction.author_role in transaction_item
-    -- todo: add rollback in app code for transaction.author_role != NEW.debitor or NEW.creditor error edge case
-    IF NEW.debitor = current_transaction.author THEN
-      tested_author_role := 'debitor';
-    ELSIF NEW.creditor = current_transaction.author THEN
-      tested_author_role := 'creditor';
-    END IF;
-    IF tested_author_role != NULL THEN
-      IF tested_author_role != current_transaction.author_role THEN
-        RAISE EXCEPTION 'transaction.author role not same in transaction_item';
-      END IF;
-    END IF;
-
-    -- insert debitor approver(s)
-    INSERT INTO approver (account_name, account_role, transaction_id, transaction_item_id)
-    SELECT DISTINCT coalesce(owner_account, '') || coalesce(owner_subaccount, ''), 'debitor', NEW.transaction_id , NEW.id
-    FROM account_owner
-    WHERE account_owner.owned_subaccount = NEW.debitor;
-
-    -- insert credtior approver(s)
-    INSERT INTO approver (account_name, account_role, transaction_id, transaction_item_id)
-    SELECT DISTINCT coalesce(owner_account, '') || coalesce(owner_subaccount, ''), 'creditor', NEW.transaction_id , NEW.id
-    FROM account_owner
-    WHERE account_owner.owned_subaccount = NEW.creditor;
-
-	RETURN NULL;
-END;
-$$
-LANGUAGE plpgsql;
-
--- trigger creates approver(s) after transaction_item insert
-CREATE TRIGGER add_approver
-AFTER INSERT ON transaction_item
-FOR EACH ROW
-EXECUTE PROCEDURE add_approver();
