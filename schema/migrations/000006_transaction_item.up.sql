72b410744c49eeb142b944b9ad7795db29186e47
max funk <mxfactorial@users.noreply.github.com>
2021-04-05 09:13:39 -0700

    support configurable transaction sequence

--- schema/migrations/000006_transaction_item.up.sql
+++ schema/migrations/000006_transaction_item.up.sql
@@ -5,7 +5,8 @@ CREATE TABLE transaction_item (
   item_id character varying(255) NOT NULL, -- temporary until item inserts added in code
   price numeric NOT NULL CHECK (price > 0),
   quantity numeric NOT NULL CHECK (quantity > 0),
-  rule_instance_id character varying(255), -- todo: change to int and add foreign key
+  debitor_first boolean not null default true,
+  rule_instance_id int,
   unit_of_measurement character varying(255),
   units_measured numeric,
   debitor character varying(255) not null,
@@ -20,16 +21,16 @@ CREATE TABLE transaction_item (
   creditor_rejection_time timestamptz,
   CONSTRAINT fk_transaction_id
     FOREIGN KEY(transaction_id)
-      REFERENCES transaction(id)
+      REFERENCES transaction(id),
   -- CONSTRAINT fk_debitor_profile_id -- todo
   --   FOREIGN KEY(debitor_profile_id)
   --     REFERENCES account_profile(id),
   -- CONSTRAINT fk_creditor_profile_id -- todo
   --   FOREIGN KEY(creditor_profile_id)
   --     REFERENCES account_profile(id),
-  -- CONSTRAINT fk_rule_instance_id -- todo
-  --   FOREIGN KEY(rule_instance_id)
-  --     REFERENCES rule_instance(id),
+  CONSTRAINT fk_rule_instance_id
+    FOREIGN KEY(rule_instance_id)
+      REFERENCES rule_instance(id)
   -- CONSTRAINT fk_item_id -- todo
   --   FOREIGN KEY(item_id)
   --     REFERENCES item(id)
