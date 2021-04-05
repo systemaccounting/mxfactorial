4fb5ceb9569e99ed00759c026cfa05dd93469ef4
max funk <mxfactorial@users.noreply.github.com>
2021-04-05 09:12:58 -0700

    add foreign key constraint in transaction migration

--- schema/migrations/000005_transaction.up.sql
+++ schema/migrations/000005_transaction.up.sql
@@ -5,11 +5,15 @@
 -- then, insert transaction_item(s)
 CREATE TABLE transaction (
   id SERIAL PRIMARY KEY,
+  rule_instance_id int,
   author character varying(255) not null, -- stores account belonging to earliest approval time in transaction_item to avoid expensive query
   author_device_id character varying(255), -- todo: inventory devices in separate table similar to items
   author_device_latlng point,
   author_role text not null,
   created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_rule_instance_id
+    FOREIGN KEY(rule_instance_id)
+      REFERENCES rule_instance(id),
   CONSTRAINT fk_author
     FOREIGN KEY(author)
       REFERENCES account(name)
