796c436bc22e34148373f74a072d7903079d6b69
max funk <mxfactorial@users.noreply.github.com>
2021-04-25 18:59:22 -0700

    pass policy arns and principals to go lambda module

--- infrastructure/terraform/aws/modules/go-lambda-service/outputs.tf
+++ infrastructure/terraform/aws/modules/go-lambda-service/outputs.tf
@@ -2,6 +2,10 @@ output "lambda_arn" {
   value = aws_lambda_function.default.arn
 }
 
+output "lambda_invoke_arn" {
+  value = aws_lambda_function.default.invoke_arn
+}
+
 output "lambda_role_name" {
   value = aws_iam_role.default.name
 }
--- infrastructure/terraform/aws/modules/go-lambda-service/variables.tf
+++ infrastructure/terraform/aws/modules/go-lambda-service/variables.tf
@@ -3,3 +3,21 @@ variable "service_name" {}
 variable "env" {}
 
 variable "env_vars" { type = map(any) }
+
+variable "invoke_principals" {
+  type        = list(string)
+  default     = []
+  description = "example: cognito-idp.amazonaws.com"
+}
+
+variable "attached_policy_arns" {
+  type        = list(string)
+  default     = []
+  description = "example: aws_iam_policy.apiv2_ddb.arn"
+}
+
+variable "create_secret" {
+  type = bool
+  default = false
+  description = "adds lambda arn in secrets manager for local testing"
+}
