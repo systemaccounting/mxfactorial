fea2cbff88be4957c789dc8dd4a5da6c8530fe7c
max funk <mxfactorial@users.noreply.github.com>
2021-05-04 00:04:11 -0700

    remove cloudformation artifact bucket

--- infrastructure/terraform/aws/modules/us-east-1/s3.tf
+++ infrastructure/terraform/aws/modules/us-east-1/s3.tf
@@ -2,8 +2,3 @@ resource "aws_s3_bucket" "artifacts" {
   bucket = "mxfactorial-artifacts-${var.environment}"
   # force_destroy = true
 }
-
-resource "aws_s3_bucket" "websocket-artifacts" {
-  bucket = "mxfactorial-websocket-artifacts-${var.environment}"
-  # force_destroy = true
-}
