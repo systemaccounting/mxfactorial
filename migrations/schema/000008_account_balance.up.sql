ebf01a470032171f7f7b0d6a08aa3a1d34ecd728
max funk <mxfactorial@users.noreply.github.com>
2021-05-05 12:01:30 -0700

    column naming and temp disable foreign key constraint

--- migrations/schema/000008_account_balance.up.sql
+++ migrations/schema/000008_account_balance.up.sql
@@ -1,15 +1,15 @@
 CREATE TABLE account_balance (
   account_name character varying(255) not null unique,
-  account_balance numeric default 0,
+  current_balance numeric default 0,
   current_transaction_item_id int, -- todo: not null, accounts created by transaction
   updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
   created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT fk_account_name
   FOREIGN KEY(account_name)
-    REFERENCES account(name),
-  CONSTRAINT fk_current_transaction_item_id
-    FOREIGN KEY(current_transaction_item_id)
-      REFERENCES transaction_item(id)
+    REFERENCES account(name)
+  -- CONSTRAINT fk_current_transaction_item_id
+  --   FOREIGN KEY(current_transaction_item_id)
+  --     REFERENCES transaction_item(id)
 );
 
 -- changes account balance on equilibrium
@@ -17,7 +17,7 @@ CREATE OR REPLACE FUNCTION change_account_balance()
 	RETURNS trigger AS
 $$
 BEGIN
-  NEW.account_balance = NEW.account_balance + OLD.account_balance;
+  NEW.current_balance = NEW.current_balance + OLD.current_balance;
   NEW.updated_at = NOW();
   RETURN NEW;
 END;
