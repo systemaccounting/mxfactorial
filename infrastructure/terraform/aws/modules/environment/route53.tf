208d0297bbb133d235403b5a722556d2c27f2a9b
max funk <mxfactorial@users.noreply.github.com>
2021-05-08 22:09:47 -0700

    delete api gateway dns after module move

--- infrastructure/terraform/aws/modules/environment/route53.tf
+++ infrastructure/terraform/aws/modules/environment/route53.tf
@@ -21,29 +21,3 @@ resource "aws_route53_record" "client_fqdn_ipv6" {
     evaluate_target_health = false
   }
 }
-
-resource "aws_route53_record" "api_fqdn" {
-  zone_id = data.aws_route53_zone.mxfactorial_io.zone_id
-
-  name = local.api_url
-  type = "A"
-
-  alias {
-    name                   = aws_api_gateway_domain_name.graphql.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.graphql.cloudfront_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "api_fqdn_ipv6" {
-  zone_id = data.aws_route53_zone.mxfactorial_io.zone_id
-
-  name = local.api_url
-  type = "AAAA"
-
-  alias {
-    name                   = aws_api_gateway_domain_name.graphql.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.graphql.cloudfront_zone_id
-    evaluate_target_health = false
-  }
-}
