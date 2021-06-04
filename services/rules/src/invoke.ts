c3d34b91af548f66565e28c6df29b2f0721f7cfd
max funk <mxfactorial@users.noreply.github.com>
2021-06-04 16:43:27 -0700

    local invoke rules module for convenient local development

---
+++ services/rules/src/invoke.ts
@@ -0,0 +1,21 @@
+import type { ITransactionItem } from "./index.d"
+import test from "../../gopkg/testdata/preRuleTrItems.json"
+import { handler } from "./index"
+
+const event: ITransactionItem[] = test;
+
+// invokes function during local development
+(async function () {
+	const response = await handler(event);
+	// test for error string return value
+	if (typeof response == "string") {
+		console.log(response);
+	} else {
+		// or print transaction
+		console.log(response);
+		for (const item of response.transaction.transaction_items) {
+			// and print transaction items
+			console.log(item);
+		};
+	};
+})();
