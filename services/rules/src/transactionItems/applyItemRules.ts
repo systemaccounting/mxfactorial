54b23ce19da4b8804859d19d0d08f999582ea170
max funk <mxfactorial@users.noreply.github.com>
2021-06-04 16:32:00 -0700

    functions applying rules to transactions items

---
+++ services/rules/src/transactionItems/applyItemRules.ts
@@ -0,0 +1,25 @@
+import c from "../constants"
+import type { IRuleInstance, ITransactionItem } from "../index.d"
+
+export default async function (ruleInstances: IRuleInstance[], transactionItem: ITransactionItem): Promise<ITransactionItem[]> {
+
+	let ruleAppliedItems: ITransactionItem[] = new Array();
+	for (var r of ruleInstances) {
+		const ruleInstanceId = r.id;
+		const ruleName = r.rule_name;
+		const ruleAccountRole = r.account_role;
+		const ruleAccountName = r.account_name;
+		const ruleInstancePath = c.RELATIVE_RULES_PATH + "/" + ruleName;
+		const { default: itemRuleModule } = await import(ruleInstancePath);
+		const ruleVariableValues = r.variable_values;
+		ruleAppliedItems = [...itemRuleModule( // spread returned items from rules
+			ruleInstanceId,
+			ruleName,
+			ruleAccountRole,
+			ruleAccountName,
+			transactionItem,
+			...ruleVariableValues,
+		)];
+	};
+	return ruleAppliedItems;
+};
