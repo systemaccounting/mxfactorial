b1aaaec7dfecb0f2468a53b8e858147e5eeabd88
max funk <mxfactorial@users.noreply.github.com>
2021-06-04 17:34:34 -0700

    client icons

---
+++ client/src/icons/SubtractIcon.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let size: number;
+	export let style: string;
+</script>
+
+<svg {style} viewBox="0 0 1024 1024" width={size} height={size}>
+	<path
+		fill="currentColor"
+		d="M0 416v192c0 17.672 14.328 32 32 32h960c17.672 0 32-14.328 32-32v-192c0-17.672-14.328-32-32-32h-960c-17.672 0-32 14.328-32 32z"
+	/>
+</svg>
