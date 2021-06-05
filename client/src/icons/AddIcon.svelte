ea91a13f4c751a05f7b849feebf6d1e09d36930c
max funk <mxfactorial@users.noreply.github.com>
2021-06-04 17:34:34 -0700

    client icons

---
+++ client/src/icons/AddIcon.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let size: number;
+	export let style: string;
+</script>
+
+<svg {style} viewBox="0 0 1024 1024" width={size} height={size}>
+	<path
+		fill="currentColor"
+		d="M992 384h-352v-352c0-17.672-14.328-32-32-32h-192c-17.672 0-32 14.328-32 32v352h-352c-17.672 0-32 14.328-32 32v192c0 17.672 14.328 32 32 32h352v352c0 17.672 14.328 32 32 32h192c17.672 0 32-14.328 32-32v-352h352c17.672 0 32-14.328 32-32v-192c0-17.672-14.328-32-32-32z"
+	/>
+</svg>
---
+++ client/src/icons/Icon.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Fa from "svelte-fa";
+	import type { IconDefinition } from "@fortawesome/fontawesome-common-types";
+	export let iconName: IconDefinition;
+	export let addedStyle: string;
+	export let dataIDValue: string;
+
+	const max: number = 1.0;
+	const min: number = 0.7;
+
+	let opacity: number = min;
+
+	function enter(): void {
+		opacity = max;
+	}
+
+	function leave(): void {
+		opacity = min;
+	}
+</script>
+
+<span on:mouseenter={enter} on:mouseleave={leave} data-id={dataIDValue}>
+	<Fa
+		size="lg"
+		color="#fff"
+		style="{addedStyle} opacity: {opacity};"
+		icon={iconName}
+	/>
+</span>
