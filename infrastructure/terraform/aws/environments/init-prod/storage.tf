cccdfc764c72d92b2a3754a3c1edfc7871a353fb
max funk <mxfactorial@users.noreply.github.com>
2021-07-22 22:44:36 -0700

    storage tf stack naming

--- infrastructure/terraform/aws/environments/init-prod/storage.tf
+++ infrastructure/terraform/aws/environments/init-prod/storage.tf
@@ -3,8 +3,8 @@ locals {
   ARTIFACTS_PREFIX = jsondecode(file("${path.module}/../../../../../project.json")).artifacts_bucket_name_prefix
 }
 
-module "artifact_storage_prod" {
-  source                           = "../../modules/artifact-storage/v001"
+module "project_storage_prod" {
+  source                           = "../../modules/project-storage/v001"
   env                              = "prod"
   artifacts_bucket_name_prefix     = local.ARTIFACTS_PREFIX
   client_origin_bucket_name_prefix = local.ORIGIN_PREFIX
