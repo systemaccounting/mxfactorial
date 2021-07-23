d0dd6a9a790e99ba5b794e9eb99403eae0ceee3e
max funk <mxfactorial@users.noreply.github.com>
2021-07-22 22:44:01 -0700

    tf 1 bump

--- infrastructure/terraform/aws/environments/init-dev/main.tf
+++ infrastructure/terraform/aws/environments/init-dev/main.tf
@@ -1,14 +1,5 @@
 terraform {
-  required_version = "~> 0.15.0"
-
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "systemaccounting"
-
-    workspaces {
-      name = "aws-init-dev" // !!! change value when duplicating !!!
-    }
-  }
+  required_version = "~> 1.0.3"
 }
 
 provider "aws" {
--- infrastructure/terraform/aws/environments/init-prod/main.tf
+++ infrastructure/terraform/aws/environments/init-prod/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 0.15.0"
+  required_version = "~> 1.0.3"
 
   backend "remote" {
     hostname     = "app.terraform.io"
