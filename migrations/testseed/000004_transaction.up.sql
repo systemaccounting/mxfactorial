cbc01b2f324efa0e0d24512f4dfeaaf3f27420fc
max funk <mxfactorial@users.noreply.github.com>
2022-02-13 03:29:44 -0700

    simulate sales tax

--- migrations/testseed/000004_transaction.up.sql
+++ migrations/testseed/000004_transaction.up.sql
@@ -9,19 +9,19 @@ butter as (
 	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_approval_time, creditor_approval_time) values ((select id from insert_transaction), null, 'butter', 2.000, 1, 'JohnSmith', 'GroceryCo', NOW(), NOW()) returning id
 ),
 tax_butter as (
-	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_approval_time, creditor_approval_time) values ((select id from insert_transaction), (select id from item_tax_rule_instance), '9% state sales tax', 0.180, 1, 'GroceryCo', 'StateOfCalifornia', NOW(), NOW()) returning id
+	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_approval_time, creditor_approval_time) values ((select id from insert_transaction), (select id from item_tax_rule_instance), '9% state sales tax', 0.180, 1, 'JohnSmith', 'StateOfCalifornia', NOW(), NOW()) returning id
 ),
 grapes as (
 	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_approval_time, creditor_approval_time) values ((select id from insert_transaction), null, 'grapes', 2.000, 2, 'JohnSmith', 'GroceryCo', NOW(), NOW()) returning id
 ),
 tax_grapes as (
-	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_approval_time, creditor_approval_time) values ((select id from insert_transaction), (select id from item_tax_rule_instance), '9% state sales tax', 0.180, 2, 'GroceryCo', 'StateOfCalifornia', NOW(), NOW()) returning id
+	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_approval_time, creditor_approval_time) values ((select id from insert_transaction), (select id from item_tax_rule_instance), '9% state sales tax', 0.180, 2, 'JohnSmith', 'StateOfCalifornia', NOW(), NOW()) returning id
 ),
 tea as (
 	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_approval_time, creditor_approval_time) values ((select id from insert_transaction), null, 'tea', 3.000, 1, 'JohnSmith', 'GroceryCo', NOW(), NOW()) returning id
 ),
 tax_tea as (
-	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_approval_time, creditor_approval_time) values ((select id from insert_transaction), (select id from item_tax_rule_instance), '9% state sales tax', 0.270, 1, 'GroceryCo', 'StateOfCalifornia', NOW(), NOW()) returning id
+	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_approval_time, creditor_approval_time) values ((select id from insert_transaction), (select id from item_tax_rule_instance), '9% state sales tax', 0.270, 1, 'JohnSmith', 'StateOfCalifornia', NOW(), NOW()) returning id
 ),
 appr_igor_debitor_rule_instance as (
 	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveDebitStateOfCalifornia' AND account_name = 'IgorPetrov'
@@ -33,7 +33,7 @@ appr_ben_creditor_rule_instance as (
 	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'BenRoss'
 ),
 appr_jacob_creditor_rule_instance as (
-	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'JacobWebb'
+	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'DanLee'
 ),
 appr_miriam_creditor_rule_instance as (
 	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'MiriamLevy'
