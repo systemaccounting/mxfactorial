67bb1b8980774a58af267202f4a30f4f982adef8
max funk <mxfactorial@users.noreply.github.com>
2022-03-18 16:46:48 -0700

    switch graphql api to payload format version 2

--- services/graphql/server.go
+++ services/graphql/server.go
@@ -11,6 +11,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/core"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
 	"github.com/gorilla/mux"
 	"github.com/systemaccounting/mxfactorial/services/graphql/graph"
@@ -21,29 +22,26 @@ const playgroundTitle string = "playground"
 const rootRoute string = "/"
 const queryRoute string = rootRoute + "query"
 
-// lambda code
 func lambdaFn(
 	ctx context.Context,
-	e events.APIGatewayProxyRequest,
+	e core.SwitchableAPIGatewayRequest,
 	schema graphql.ExecutableSchema,
 	server *handler.Server,
 	r *mux.Router,
-	ma *gorillamux.GorillaMuxAdapter) (events.APIGatewayProxyResponse, error) {
+	ma *gorillamux.GorillaMuxAdapter) (events.APIGatewayV2HTTPResponse, error) {
 	r.Handle(queryRoute, server)
 	r.Handle(rootRoute, playground.Handler(playgroundTitle, queryRoute))
 	resp, err := ma.ProxyWithContext(ctx, e)
 	if err != nil {
 		log.Println(err)
 	}
-	resp.Headers = make(map[string]string)
-	resp.Headers["Access-Control-Allow-Origin"] = "*"
-	return resp, err
+	return *resp.Version2(), err
 }
 
 // wraps lambdaFn with deps
 func handleEvent(
 	ctx context.Context,
-	e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	e core.SwitchableAPIGatewayRequest) (events.APIGatewayV2HTTPResponse, error) {
 	var r *mux.Router = mux.NewRouter()
 	var muxAdapter *gorillamux.GorillaMuxAdapter = gorillamux.New(r)
 	var schema graphql.ExecutableSchema = generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})
