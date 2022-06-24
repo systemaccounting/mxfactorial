79854f53eb6b168ba5f0e772c42f30b67bf5ef2f
max funk <mxfactorial@users.noreply.github.com>
2022-06-24 14:02:35 -0700

    reusable last n query in transactions by account service

--- services/transactions-by-account/cmd/main.go
+++ services/transactions-by-account/cmd/main.go
@@ -12,31 +12,11 @@ import (
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/jackc/pgx/v4"
+	"github.com/systemaccounting/mxfactorial/services/gopkg/data"
 	lpg "github.com/systemaccounting/mxfactorial/services/gopkg/lambdapg"
 	sqlb "github.com/systemaccounting/mxfactorial/services/gopkg/sqlbuilder"
 	"github.com/systemaccounting/mxfactorial/services/gopkg/tools"
 	"github.com/systemaccounting/mxfactorial/services/gopkg/types"
-)
-
-// 1. from subquery, get approved transaction items
-// 			by account, and limit the subquery return with
-// 			configured RETURN_RECORD_LIMIT
-// 2. query all transaction items by distinct transaction_id
-// 3. return all transaction items in desc order
-const (
-	sql string = `
-	SELECT * FROM transaction_item
-	WHERE transaction_id IN (
-		SELECT DISTINCT transaction_id
-		FROM transaction_item
-		WHERE (debitor = $1 OR creditor = $1)
-		AND debitor_approval_time IS NOT NULL
-		AND creditor_approval_time IS NOT NULL
-		ORDER BY transaction_id
-		DESC
-		LIMIT $2
-	) ORDER BY transaction_id
-	DESC;`
 )
 
 var (
@@ -71,26 +51,20 @@ func lambdaFn(
 	}
 	defer db.Close(context.Background())
 
-	// query
-	rows, err := db.Query(
-		ctx,
-		sql,
-		e.AccountName,
-		recordLimit,
-	)
+	// create transaction sql with true boolean value as arg #2
+	transactionSQL, transactionArgs := sqlb.SelectLastNReqsOrTransByAccount(e.AuthAccount, true, recordLimit)
+
+	// get transactions
+	transactions, err := data.GetTransactionsWithTrItemsAndApprovalsByID(db, transactionSQL, transactionArgs)
 	if err != nil {
 		log.Print(err)
 		return "", err
 	}
 
-	// unmarshal query response
-	trItems, err := lpg.UnmarshalTrItems(rows)
-	if err != nil {
-		log.Print("UnmarshalTrItems ", err.Error())
-		return "", err
-	}
+	// test for empty transaction list
+	if len(transactions) == 0 {
+		log.Println("0 transactions found")
 
-	if len(trItems) == 0 {
 		// create empty response to client
 		intraTrs := tools.CreateIntraTransactions(e.AuthAccount, []*types.Transaction{})
 
@@ -98,43 +72,8 @@ func lambdaFn(
 		return tools.MarshalIntraTransactions(&intraTrs)
 	}
 
-	var trIDs []interface{}
-	for _, v := range trItems {
-		if tools.IsCustomIDUnique(*v.TransactionID, trIDs) {
-			trIDs = append(trIDs, *v.TransactionID)
-		}
-	}
-
-	// create sql to get current transaction
-	trsSQL, trsArgs := sqlb.SelectTransactionsByIDsSQL(
-		trIDs,
-	)
-
-	// get transactions
-	trRows, err := db.Query(context.Background(), trsSQL, trsArgs...)
-	if err != nil {
-		log.Print("query transactions ", err.Error())
-		return "", err
-	}
-
-	// unmarshal transactions
-	trs, err := lpg.UnmarshalTransactions(trRows)
-	if err != nil {
-		log.Print("UnmarshalTransactions ", err.Error())
-		return "", err
-	}
-
-	// add transaction items to each transaction
-	for _, v := range trs {
-		for _, w := range trItems {
-			if *w.TransactionID == *v.ID {
-				v.TransactionItems = append(v.TransactionItems, w)
-			}
-		}
-	}
-
 	// create for response to client
-	intraTrs := tools.CreateIntraTransactions(e.AuthAccount, trs)
+	intraTrs := tools.CreateIntraTransactions(e.AuthAccount, transactions)
 
 	// send string or error response to client
 	return tools.MarshalIntraTransactions(&intraTrs)
