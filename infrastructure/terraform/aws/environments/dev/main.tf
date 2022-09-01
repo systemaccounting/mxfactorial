21af5d6872b5f135a37dd0c394fe8f7b3ec3663e
max funk <mxfactorial@users.noreply.github.com>
2022-09-01 00:56:28 -0700

    bump terraform version

--- infrastructure/terraform/aws/environments/dev/main.tf
+++ infrastructure/terraform/aws/environments/dev/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.0.3"
+  required_version = "~> 1.2.7"
 }
 
 provider "aws" {
--- infrastructure/terraform/aws/environments/prod/main.tf
+++ infrastructure/terraform/aws/environments/prod/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.0.3"
+  required_version = "~> 1.2.7"
 
   backend "remote" {
     hostname     = "app.terraform.io"
