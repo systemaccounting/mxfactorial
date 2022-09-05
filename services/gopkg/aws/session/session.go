02acfb0cca7f71f7e9c7664e5c8da8072a1e46e4
max funk <mxfactorial@users.noreply.github.com>
2022-09-05 13:46:04 -0700

    include aws config in go session package

--- services/gopkg/aws/session/session.go
+++ services/gopkg/aws/session/session.go
@@ -1,12 +1,21 @@
 package session
 
-import "github.com/aws/aws-sdk-go/aws/session"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
 
 type AWSSession struct {
 	*session.Session
 }
 
-func NewAWSSession() *AWSSession {
-	s := session.Must(session.NewSession())
+func NewAWSConfig(awsRegion *string) *aws.Config {
+	return &aws.Config{
+		Region: awsRegion,
+	}
+}
+
+func NewAWSSession(config *aws.Config) *AWSSession {
+	s := session.Must(session.NewSession(config))
 	return &AWSSession{Session: s}
 }
