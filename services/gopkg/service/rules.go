f8a599ae4f9516e24fbfc888aaaac89f96bdddb4
max funk <mxfactorial@users.noreply.github.com>
2022-09-05 13:50:36 -0700

    rules in go service package

--- services/gopkg/service/rules.go
+++ services/gopkg/service/rules.go
@@ -1,115 +1,39 @@
 package service
 
 import (
+	"github.com/systemaccounting/mxfactorial/services/gopkg/aws/lambda"
 	"github.com/systemaccounting/mxfactorial/services/gopkg/logger"
-	"github.com/systemaccounting/mxfactorial/services/gopkg/postgres"
 	"github.com/systemaccounting/mxfactorial/services/gopkg/types"
 )
 
-type IRuleInstanceService interface {
-	InsertApproveAllCreditRuleInstance(string) error
-	SelectApproveAllCreditRuleInstance(string) (types.RuleInstance, error)
-	InsertApproveAllCreditRuleInstanceIfDoesNotExist(string) error
-	AddRuleInstance(string, string, string, string, string, string) error
-	GetRuleInstanceByCurrentlyUsedValues(string, string, string, string, string, string) error
+type RulesService struct {
+	lambda.ILambdaService
 }
 
-type RuleInstanceService struct {
-	*postgres.RuleInstanceModel
+type IRulesService interface {
+	GetRuleAppliedIntraTransactionFromTrItems(types.TransactionItems) (*types.IntraTransaction, error)
 }
 
-func (ri RuleInstanceService) InsertApproveAllCreditRuleInstance(
-	accountName string,
-) error {
+func (r RulesService) GetRuleAppliedIntraTransactionFromTrItems(trItems types.TransactionItems) (*types.IntraTransaction, error) {
 
-	err := ri.RuleInstanceModel.InsertApproveAllCreditRuleInstance(accountName)
+	response, err := r.ILambdaService.Invoke(trItems)
 	if err != nil {
 		logger.Log(logger.Trace(), err)
-		return err
+		return nil, err
 	}
 
-	return nil
-}
-
-func (ri RuleInstanceService) SelectApproveAllCreditRuleInstance(
-	accountName string,
-) (types.RuleInstance, error) {
-
-	err := ri.RuleInstanceModel.SelectApproveAllCreditRuleInstance(accountName)
+	var ruleTested types.IntraTransaction
+	err = ruleTested.Unmarshal(response)
 	if err != nil {
 		logger.Log(logger.Trace(), err)
-		return types.RuleInstance{}, err
+		return nil, err
 	}
 
-	return ri.RuleInstance, nil
+	return &ruleTested, nil
 }
 
-func (ri RuleInstanceService) InsertApproveAllCreditRuleInstanceIfDoesNotExist(
-	accountName string,
-) error {
-
-	err := ri.RuleInstanceModel.InsertApproveAllCreditRuleInstanceIfDoesNotExist(accountName)
-	if err != nil {
-		logger.Log(logger.Trace(), err)
-		return err
-	}
-
-	return nil
-}
-
-func (ri RuleInstanceService) AddRuleInstance(
-	ruleType,
-	ruleName,
-	ruleInstanceName,
-	accountRole,
-	accountName,
-	variableValuesArray string,
-) error {
-
-	err := ri.RuleInstanceModel.InsertRuleInstance(
-		ruleType,
-		ruleName,
-		ruleInstanceName,
-		accountRole,
-		accountName,
-		variableValuesArray,
-	)
-	if err != nil {
-		logger.Log(logger.Trace(), err)
-		return err
-	}
-
-	return nil
-}
-
-// complicated name intended
-func (ri RuleInstanceService) GetRuleInstanceByCurrentlyUsedValues(
-	ruleType,
-	ruleName,
-	ruleInstanceName,
-	accountRole,
-	accountName,
-	variableValuesArray string,
-) error {
-
-	err := ri.RuleInstanceModel.SelectRuleInstance(
-		ruleType,
-		ruleName,
-		ruleInstanceName,
-		accountRole,
-		accountName,
-		variableValuesArray,
-	)
-	if err != nil {
-		logger.Log(logger.Trace(), err)
-		return err
-	}
-
-	return nil
-}
-
-func NewRuleInstanceService(db *postgres.DB) *RuleInstanceService {
-	return &RuleInstanceService{
-		RuleInstanceModel: postgres.NewRuleInstanceModel(db),
+func NewRulesService(lambdaFnName, awsRegion *string) *RulesService {
+	return &RulesService{
+		ILambdaService: lambda.NewLambdaService(lambdaFnName, nil),
 	}
 }
