2133b7041d453abc0ce131539cd623f10ed19d47
max funk <mxfactorial@users.noreply.github.com>
2022-10-30 14:02:18 -0700

    unmarshal multiple transactions for go unit tests

--- services/gopkg/testdata/unmarshal.go
+++ services/gopkg/testdata/unmarshal.go
@@ -21,3 +21,17 @@ func GetTestTransaction(path string) types.Transaction {
 	}
 	return *i.Transaction
 }
+
+func GetTestTransactions(path string) []*types.IntraTransaction {
+	f, err := os.ReadFile(path)
+	if err != nil {
+		msgErr := fmt.Errorf("read fail %s: %v", path, err)
+		panic(msgErr)
+	}
+	var i []*types.IntraTransaction
+	if err := json.Unmarshal(f, &i); err != nil {
+		msgErr := fmt.Errorf("unmarshal fail: %v", err)
+		panic(msgErr)
+	}
+	return i
+}
