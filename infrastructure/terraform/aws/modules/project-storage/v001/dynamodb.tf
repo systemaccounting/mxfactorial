5d75dabe7d90a6e0f43e0d9ab027ff8769b82c71
max funk <mxfactorial@users.noreply.github.com>
2022-11-13 16:26:03 -0700

    add bucket backend for environment terraform state

--- infrastructure/terraform/aws/modules/project-storage/v001/dynamodb.tf
+++ infrastructure/terraform/aws/modules/project-storage/v001/dynamodb.tf
@@ -1,26 +1,22 @@
 locals {
-  PROJECT_JSON = "../../../../../project.json"
-  DDB_TABLE_NAME_PREFIX = jsondecode(file(local.PROJECT_JSON)).github_workflows.dynamodb_table.name_prefix
-  DDB_TABLE_HASH_KEY = jsondecode(file(local.PROJECT_JSON)).github_workflows.dynamodb_table.hash_key
+  PROJECT_JSON          = jsondecode(file("../../../../../project.json"))
+  DDB_TABLE_NAME_PREFIX = local.PROJECT_JSON.github_workflows.dynamodb_table.name_prefix
+  DDB_TABLE_HASH_KEY    = local.PROJECT_JSON.github_workflows.dynamodb_table.hash_key
 }
 
 // avoids dependency on github workflow job outputs when sharing values
 resource "aws_dynamodb_table" "github_workflows" {
-  name           = "${local.DDB_TABLE_NAME_PREFIX}-${var.env}"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = local.DDB_TABLE_HASH_KEY
+  name         = "${var.ddb_table_name_prefix}-${var.env_id}-${var.env}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = var.ddb_table_hash_key
 
   attribute {
-    name = local.DDB_TABLE_HASH_KEY
+    name = var.ddb_table_hash_key
     type = "S"
   }
 
   ttl {
     attribute_name = "ttl"
     enabled        = true
-  }
-
-  tags = {
-    Environment = var.env
   }
 }
--- infrastructure/terraform/aws/modules/project-storage/v001/s3.tf
+++ infrastructure/terraform/aws/modules/project-storage/v001/s3.tf
@@ -1,10 +1,15 @@
 resource "aws_s3_bucket" "artifacts" {
-  bucket        = "${var.artifacts_bucket_name_prefix}-${var.env}"
+  bucket        = "${var.artifacts_bucket_name_prefix}-${var.env_id}-${var.env}"
   force_destroy = true
 }
 
+resource "aws_s3_bucket" "tfstate" {
+  bucket        = "${var.tfstate_bucket_name_prefix}-${var.env_id}-${var.env}"
+  force_destroy = var.force_destroy_tfstate
+}
+
 resource "aws_s3_bucket" "client_origin" {
-  bucket = "${var.client_origin_bucket_name_prefix}-${var.env}"
+  bucket = "${var.client_origin_bucket_name_prefix}-${var.env_id}-${var.env}"
 
   website {
     index_document = "index.html"
