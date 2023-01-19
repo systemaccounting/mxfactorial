cc2218805b37f7990bc683fde5c3e0453b0e0a77
max funk <mxfactorial@users.noreply.github.com>
2023-01-18 22:54:55 -0700

    graphql queries for integration tests

---
+++ test/query/getRequestsByAccount.ts
@@ -0,0 +1,48 @@
+import { gql } from "graphql-request"
+
+export const getRequestsByAccount = gql`
+query getRequestsByAccount($account_name: String!, $auth_account: String!) {
+  requestsByAccount(account_name: $account_name, auth_account: $auth_account) {
+    id
+    rule_instance_id
+    author
+    author_device_id
+    author_device_latlng
+    author_role
+    sum_value
+    transaction_items {
+        id
+        transaction_id
+        item_id
+        price
+        quantity
+        debitor_first
+        rule_instance_id
+        unit_of_measurement
+        units_measured
+        debitor
+        creditor
+        debitor_profile_id
+        creditor_profile_id
+        debitor_approval_time
+        creditor_approval_time
+        debitor_expiration_time
+        creditor_expiration_time
+        debitor_rejection_time
+        creditor_rejection_time
+        approvals {
+            id
+            rule_instance_id
+            transaction_id
+            transaction_item_id
+            account_name
+            account_role
+            device_id
+            device_latlng
+            approval_time
+            rejection_time
+            expiration_time
+        }
+    }
+  }
+}`
