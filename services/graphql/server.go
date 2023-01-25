0a7989c8b926bbf06b8f1bd8dc7d59d80447e61b
max funk <mxfactorial@users.noreply.github.com>
2023-01-25 16:52:25 -0700

    replace aws-lambda-go-api-proxy with web server

--- services/graphql/server.go
+++ services/graphql/server.go
@@ -2,73 +2,75 @@ package main
 
 import (
 	"context"
-	"log"
 	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/awslabs/aws-lambda-go-api-proxy/core"
-	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
-	"github.com/gorilla/mux"
-	"github.com/rs/cors"
+	"github.com/gin-gonic/gin"
+	cors "github.com/rs/cors/wrapper/gin"
+
 	"github.com/systemaccounting/mxfactorial/services/graphql/graph"
 	"github.com/systemaccounting/mxfactorial/services/graphql/graph/generated"
 )
 
-const playgroundTitle string = "playground"
-const rootRoute string = "/"
-const queryRoute string = rootRoute + "query"
+var (
+	readinessCheckPath string = os.Getenv("READINESS_CHECK_PATH")
+	rootRoute                 = "/"
+	queryRoute                = rootRoute + "query"
+)
 
-func lambdaFn(
-	ctx context.Context,
-	e core.SwitchableAPIGatewayRequest,
-	server *handler.Server,
-	r *mux.Router,
-	ma *gorillamux.GorillaMuxAdapter,
-) (events.APIGatewayV2HTTPResponse, error) {
-	r.Handle(queryRoute, server)
-	r.Handle(rootRoute, playground.Handler(playgroundTitle, queryRoute))
-	resp, err := ma.ProxyWithContext(ctx, e)
-	if err != nil {
-		log.Println(err)
+// https://github.com/99designs/gqlgen/blob/9d22d98c792ba7214dc1aad4366e3f7eba0299f7/docs/content/recipes/gin.md
+func graphqlHandler() gin.HandlerFunc {
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
 	}
-	return *resp.Version2(), err
 }
 
-// wraps lambdaFn with deps
-func handleEvent(
-	ctx context.Context,
-	e core.SwitchableAPIGatewayRequest,
-) (events.APIGatewayV2HTTPResponse, error) {
-	var r *mux.Router = mux.NewRouter()
-	var muxAdapter *gorillamux.GorillaMuxAdapter = gorillamux.New(r)
-	var schema graphql.ExecutableSchema = generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})
-	var server *handler.Server = handler.NewDefaultServer(schema)
-	return lambdaFn(ctx, e, server, r, muxAdapter)
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", queryRoute)
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func GinContextToContextMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := context.WithValue(c.Request.Context(), "GinContextKey", c)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
 }
 
 func main() {
 
-	// test for lambda env
-	if len(os.Getenv("LOCAL_ENV")) == 0 {
-		lambda.Start(handleEvent)
-	} else {
-		// or serve from local
-		schema := generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})
-		server := handler.NewDefaultServer(schema)
+	r := gin.Default()
 
-		r := mux.NewRouter()
-		r.Handle(queryRoute, server)
-		r.Handle(rootRoute, playground.Handler(playgroundTitle, queryRoute))
+	r.Use(
+		cors.New(cors.Options{
+			AllowOriginFunc: func(origin string) bool { return true },
+			AllowedMethods: []string{
+				http.MethodOptions,
+				http.MethodHead,
+				http.MethodGet,
+				http.MethodPost,
+			},
+			AllowedHeaders:   []string{"*"},
+			AllowCredentials: false,
+		}),
+		GinContextToContextMiddleware(),
+	)
 
-		// create web server for local development
-		port := "8080"
-		log.Printf("connect to http://localhost:%s/ for playground", port)
-		handler := cors.Default().Handler(r) // allow any origin in local dev env
-		log.Fatal(http.ListenAndServe(":"+port, handler))
-	}
+	// aws-lambda-web-adapter READINESS_CHECK_*
+	r.GET(readinessCheckPath, func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
+
+	r.POST(queryRoute, graphqlHandler())
+
+	r.GET(rootRoute, playgroundHandler())
+
+	r.Run()
 }
