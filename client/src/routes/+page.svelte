ef5fce3de229648593c8e1e43e801729491f466c
max funk <mxfactorial@users.noreply.github.com>
2023-02-08 15:28:45 -0700

    disable auth on landing screen during local development

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Landing from '../containers/Landing.svelte';
+	import LandingDev from '../containers/LandingDev.svelte';
+</script>
+
+<svelte:head>
+	<title>mxfactorial</title>
+	<meta name="mxfactorial" content="demo client" />
+</svelte:head>
+
+{#if !process.env.CLIENT_ID && !process.env.POOL_ID}
+	<LandingDev />
+{:else}
+	<Landing />
+{/if}
