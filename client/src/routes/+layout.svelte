4579ac1d813e932c7692429d383e1405d784f953
max funk <mxfactorial@users.noreply.github.com>
2023-02-08 15:27:40 -0700

    init root route for sveltekit filesystem-based router

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import MobileLayout from '../containers/MobileLayout.svelte';
+	import client from '../client/graphql';
+	import { setContext } from 'svelte';
+	setContext('client', client);
+</script>
+
+<MobileLayout>
+	<slot />
+</MobileLayout>
