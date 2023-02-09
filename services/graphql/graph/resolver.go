66dcfb36a8351af2c8fadc70c9662c3fc980eedd
max funk <mxfactorial@users.noreply.github.com>
2023-02-09 13:50:31 -0700

    go generate command

--- services/graphql/graph/resolver.go
+++ services/graphql/graph/resolver.go
@@ -1,5 +1,7 @@
 package graph
 
+//go:generate go run github.com/99designs/gqlgen generate
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
