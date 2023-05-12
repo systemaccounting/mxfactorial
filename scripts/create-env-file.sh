dfc536abac0299ef68de83fe2ba80c9993aeacdd
max funk <mxfactorial@users.noreply.github.com>
2023-05-11 18:41:48 -0700

    set default values when creating env files for local dev

--- scripts/create-env-file.sh
+++ scripts/create-env-file.sh
@@ -19,10 +19,16 @@ done
 
 PROJECT_CONF=project.yaml
 
-APP_CONF_PATH=$(source scripts/list-conf-paths.sh --type all | grep --color=never -E "$APP_NAME$|$APP_NAME\"]$")
+if [[ $APP_NAME != 'root' ]]; then
+	APP_CONF_PATH=$(source scripts/list-conf-paths.sh --type all | grep --color=never -E "$APP_NAME$|$APP_NAME\"]$")
+	APP_DIR_PATH=$(source scripts/list-dir-paths.sh --type all | grep --color=never "$APP_NAME$")
+else
+	APP_CONF_PATH=
+	APP_DIR_PATH=.
+fi
 
 # early term when 0 secrets required
-if [[ $(yq "$APP_CONF_PATH | has(\"env_var\")" $PROJECT_CONF) == 'false' ]]; then
+if [[ $APP_CONF_PATH ]] && [[ $(yq "$APP_CONF_PATH | has(\"env_var\")" $PROJECT_CONF) == 'false' ]]; then
 	echo '0 secrets required. skipping'
 	exit 0
 fi
@@ -37,18 +43,21 @@ if [[ $(yq "$APP_CONF_PATH.env_var.get | length" $PROJECT_CONF) -eq 0 ]]; then
 	exit 0
 fi
 
-SECRETS=($(yq "$APP_CONF_PATH.env_var.get | join(\" \")" $PROJECT_CONF))
-SSM_VERSION=$(yq '.infrastructure.terraform.aws.modules.environment.env_var.set.SSM_VERSION.default' $PROJECT_CONF)
-ENV_ID=$(source scripts/print-env-id.sh)
+# avoid env-id when developing locally
+if [[ $ENV != 'local' ]]; then
+	ENV_ID=$(source scripts/print-env-id.sh)
+fi
 
+SSM_VERSION=$(yq '.infrastructure.terraform.aws.modules.environment.env_var.set.SSM_VERSION.default' $PROJECT_CONF)
+REGION=$(yq '.infrastructure.terraform.aws.modules.environment.env_var.set.REGION.default' $PROJECT_CONF)
+SECRETS=($(yq "$APP_CONF_PATH.env_var.get | join(\" \")" $PROJECT_CONF))
 PARAMS=($(yq "$APP_CONF_PATH.params | join(\" \")" $PROJECT_CONF))
 ENABLE_API_AUTH=$(yq '.infrastructure.terraform.aws.modules.environment.env_var.set.ENABLE_API_AUTH.default' $PROJECT_CONF)
 ENABLE_NOTIFICATIONS=$(yq '.infrastructure.terraform.aws.modules.environment.env_var.set.ENABLE_NOTIFICATIONS.default' $PROJECT_CONF)
-APP_DIR_PATH=$(source scripts/list-dir-paths.sh --type all | grep --color=never "$APP_NAME$")
 ENV_FILE_NAME=$(yq '.env_var.set.ENV_FILE_NAME.default' $PROJECT_CONF)
 ENV_FILE="$APP_DIR_PATH/$ENV_FILE_NAME"
-REGION=$(yq '.infrastructure.terraform.aws.modules.environment.env_var.set.REGION.default' $PROJECT_CONF)
-
+LOCAL_ADDRESS=$(yq '.env_var.set.LOCAL_ADDRESS.default' $PROJECT_CONF)
+HOST="http://$LOCAL_ADDRESS"
 
 function test_env_file() {
 	if [[ ! -s $ENV_FILE ]]; then
@@ -59,16 +68,41 @@ function test_env_file() {
 	fi
 }
 
+function set_default_values() {
+	for s in "${SECRETS[@]}"; do
+		if [[ "$s" == *'_URL' ]]; then
+			SVC_NAME=$(printf '%s' "$s" | sed 's/_URL//')
+			PORT_ENV_VAR="$SVC_NAME"_PORT
+			PORT_VAL=$(yq "... | select(has(\"$PORT_ENV_VAR\")).$PORT_ENV_VAR.default" $PROJECT_CONF)
+			echo "$s=$HOST:$PORT_VAL" >> $ENV_FILE
+		elif [[ "$s" == 'GRAPHQL_URI' ]] || [[ "$s" == 'CLIENT_URI' ]]; then # todo: change *_URI to *_URL
+			SVC_NAME=$(printf '%s' "$s" | sed 's/_URI//')
+			PORT_ENV_VAR="$SVC_NAME"_PORT
+			PORT_VAL=$(yq "... | select(has(\"$PORT_ENV_VAR\")).$PORT_ENV_VAR.default" $PROJECT_CONF)
+			echo "$s=$HOST:$PORT_VAL" >> $ENV_FILE
+		else
+			ENV_VAR=$(yq "... | select(has(\"$s\")).$s.default" $PROJECT_CONF)
+			echo $s=$ENV_VAR >> $ENV_FILE
+		fi
+	done
+}
+
 function set_secrets() {
 	for s in "${SECRETS[@]}"; do
-		SSM_SUFFIX=$(yq "... | select(has(\"$s\")).$s.ssm" $PROJECT_CONF)
-		ENV_VAR=$(aws ssm get-parameter \
-			--name "/$ENV_ID/$SSM_VERSION/$ENV/$SSM_SUFFIX" \
-			--query 'Parameter.Value' \
-			--region $REGION \
-			--with-decryption \
-			--output text)
+		CONF_OBJ=$(yq "... | select(has(\"$s\")).$s" $PROJECT_CONF)
+		if [[ $(echo "$CONF_OBJ" | yq .ssm) != 'null' ]]; then
+			SSM_SUFFIX=$(yq "... | select(has(\"$s\")).$s.ssm" $PROJECT_CONF)
+			ENV_VAR=$(aws ssm get-parameter \
+				--name "/$ENV_ID/$SSM_VERSION/$ENV/$SSM_SUFFIX" \
+				--query 'Parameter.Value' \
+				--region $REGION \
+				--with-decryption \
+				--output text)
+		else
+			ENV_VAR=$(echo "$CONF_OBJ" | yq ".default")
+		fi
 		echo $s=$ENV_VAR >> $ENV_FILE
+		unset ENV_VAR
 	done
 }
 
@@ -85,6 +119,10 @@ function set_params() {
 }
 
 rm -f $ENV_FILE
-set_secrets
+if [[ $ENV == 'local' ]]; then
+	set_default_values
+else
+	set_secrets
+fi
 set_params
 test_env_file
