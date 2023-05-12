c8b6f455515a3053d1c2776d99f1497aefdd5abb
max funk <mxfactorial@users.noreply.github.com>
2023-05-11 18:44:53 -0700

    reduce devcontainer bootstrapping

--- scripts/post-create-cmd.sh
+++ scripts/post-create-cmd.sh
@@ -3,12 +3,16 @@
 # used as postCreateCommand in .devcontainer.json
 
 go mod download
-cargo install cross --git https://github.com/cross-rs/cross
-cargo install cargo-watch
+
+# https://github.com/evanw/esbuild/issues/1819#issuecomment-1018771557
+rm -rf client/node_modules
+
 if [[ $CODESPACES ]]; then
 	source ./scripts/rebuild-client-image.sh
 fi
+
 make compose-up
+
 if [[ $CODESPACES ]]; then
 	gh codespace ports visibility 8080:public -c "$CODESPACE_NAME"
 fi
