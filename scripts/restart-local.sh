3766cfcfac7a82d7306e8b366dd3e47d4b77da32
max funk <mxfactorial@users.noreply.github.com>
2023-05-14 15:28:23 -0700

    consolidate

--- scripts/restart-local.sh
+++ scripts/restart-local.sh
@@ -18,7 +18,7 @@ function restart() {
 	local REQ_PID_COUNT="$1"
 	while [[ "$(make --no-print-directory list-pids | wc -l | xargs)" -lt "$REQ_PID_COUNT" ]] && [[ "$RESTART_COUNT" -le "$LOCAL_RESTART_LIMIT" ]]; do
 		RESTARTS=$(($LOCAL_RESTART_LIMIT-$RESTART_COUNT+1))
-		echo -e -n  "\n${RED}*** only "$(make --no-print-directory list-pids | wc -l | xargs)" apps started when $REQ_PID_COUNT were required. $RESTARTS restart(s) left before exiting. list of started apps:${RESET}\n"
+		echo -e -n  "\n${RED}*** only "$(make --no-print-directory list-pids | wc -l | xargs)" apps started when $REQ_PID_COUNT required. $RESTARTS restart(s) left before exiting. list of started apps:${RESET}\n"
 		make --no-print-directory list-pids
 		source ./scripts/stop-local.sh
 		source ./scripts/start-local.sh
@@ -26,19 +26,16 @@ function restart() {
 		RESTART_COUNT=$(($RESTART_COUNT+1))
 	done
 	if [[ "$(make --no-print-directory list-pids | wc -l | xargs)" -lt "$REQ_PID_COUNT" ]]; then
-		echo -e -n  "\n${RED}*** only "$(make --no-print-directory list-pids | wc -l | xargs)" apps started when $REQ_PID_COUNT were required. list of started apps:${RESET}\n\n"
+		echo -e -n  "\n${RED}*** only "$(make --no-print-directory list-pids | wc -l | xargs)" apps started when $REQ_PID_COUNT required. list of started apps:${RESET}\n\n"
 		make --no-print-directory list-pids
 		echo -e -n  "\n${RED}*** \"make stop-dev && make dev\" in a separate shell to try again${RESET}\n"
 	fi
 }
 
-if [[ "$CI" ]]; then # -1, client not required in workflows
+if [[ $CI ]]; then # -1, client not required in workflows
 	restart "$REQUIRED_CI_PID_COUNT"
 else
 	restart "$REQUIRED_PID_COUNT"
-fi
-
-if ! [[ "$CI" ]]; then
 	echo -e -n "\n${YELLOW}*** OPEN a shell and leave this one to follow logs in project root ${NOHUP_LOG}. \"make stop-dev\" in a separate shell to tear down${RESET}\n"
 	tail -F "$NOHUP_LOG"
 fi
