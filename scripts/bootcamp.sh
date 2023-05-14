bb0ff7249b2f95cde2104fab707829882ec2b1cd
max funk <mxfactorial@users.noreply.github.com>
2023-05-13 18:39:54 -0700

    bootcamp with local environment

--- scripts/bootcamp.sh
+++ scripts/bootcamp.sh
@@ -11,14 +11,6 @@ function eval_with_no_print_directory() {
 	eval "make --no-print-directory $(echo "$1" | sed 's/make //')"
 }
 
-while [[ $CMD != "make compose-up" ]]; do
-	read -rep $'\n\e[32m"make compose-up" to create services in docker compose\e[0m\n\n> ' CMD
-done
-
-eval_with_no_print_directory "$CMD"
-
-echo ""
-
 while [[ $CMD != "make rule" ]]; do
 	read -rep $'\e[32m"make rule" to see a request and response from services/rule\e[0m\n\n> ' CMD
 done
@@ -26,6 +18,7 @@ done
 eval_with_no_print_directory "$CMD"
 
 echo -e -n "\n${YELLOW}*** OBSERVE the additional \"9% state sales tax\" transaction_items returned by the rule service${RESET}\n"
+
 echo ""
 
 while [[ $CMD != "make request-create" ]]; do
@@ -35,6 +28,7 @@ done
 eval_with_no_print_directory "$CMD"
 
 echo -e -n "\n${YELLOW}*** OBSERVE timestamp values in \"creditor_approval_time\" AND null values in \"debitor_approval_time\": the creditor sent a transaction request to the debitor${RESET}\n"
+
 echo ""
 
 while [[ $CMD != "make request-approve" ]]; do
@@ -44,6 +38,7 @@ done
 eval_with_no_print_directory "$CMD"
 
 echo -e -n "\n${YELLOW}*** OBSERVE timestamp values in \"creditor_approval_time\" AND \"debitor_approval_time\": the debitor approved the transaction request from the creditor${RESET}\n"
+
 echo ""
 
 while [[ $CMD != "make balance-by-account" ]]; do
@@ -86,44 +81,13 @@ eval_with_no_print_directory "$CMD"
 
 echo ""
 
-while [[ $CMD != "make rebuild-db" ]]; do
-	read -rep $'\e[32m"make rebuild-db" to reset the database for integration tests\e[0m\n\n> ' CMD
-done
-
-eval_with_no_print_directory "$CMD"
-
-echo ""
-
-while [[ $CMD != "make test-docker" ]]; do
-	read -rep $'\e[32m"make test-docker" to test service integration in docker compose (<1 min run time)\e[0m\n\n> ' CMD
-done
-
-eval_with_no_print_directory "$CMD"
-
-echo ""
-
-while [[ $CMD != "make rebuild-transactions-by-account" ]]; do
-	read -rep $'\e[32mnow add log.Println("hello cadet") at the top of func main() in services/transactions-by-account/cmd/main.go and "make rebuild-transactions-by-account" to rebuild\e[0m\n\n> ' CMD
-done
-
-eval_with_no_print_directory "$CMD"
-
-echo ""
-
-while [[ $CMD != "docker logs mxf-transactions-by-account-1" ]]; do
-	read -rep $'\e[32m"docker logs mxf-transactions-by-account-1" to view "hello cadet" entry in docker logs\e[0m\n\n> ' CMD
-done
-
-eval "$CMD"
-
-echo ""
-
-echo -e -n "${GREEN}press any key to print all rebuild commands available during development:${RESET}\n\n>"
+echo -e -n "${GREEN}now add log.Println(\"hello cadet\") at the top of func main() in services/transactions-by-account/cmd/main.go to restart the service with a code change. press any key to continue${RESET}\n\n>"
 read -n 1
 
 echo ""
 
-grep -E '^rebuild-' ./makefile | sed 's/.$//' | awk '$0="make " $0'
+echo -e -n "${GREEN}switch to the initial shell to view the log entry from the transactions-by-account code change. press any key to continue${RESET}\n\n>"
+read -n 1
 
 echo ""
 
@@ -140,36 +104,55 @@ eval "$CMD"
 
 echo ""
 
+while [[ $CMD != "make reset-db" ]]; do
+	read -rep $'\e[32m"make reset-db" to reset the database for integration tests\e[0m\n\n> ' CMD
+done
+
+eval_with_no_print_directory "$CMD"
+
+echo ""
+
 echo -e -n "${GREEN}navigate to $GRAPHQL_URI in a browser to view the graphiql explorer. press any key to continue${RESET}\n\n>"
 read -n 1
 
 echo ""
 
-if [[ $GITPOD_WORKSPACE_URL ]]; then
+while [[ $CMD != "make -C test test-local" ]]; do
+	read -rep $'\e[32m"make -C test test-local" to test service integration locally\e[0m\n\n> ' CMD
+done
+
+eval "$CMD"
+
+echo ""
+
+while [[ $CMD != "make list-pids" ]]; do
+	read -rep $'\e[32m"make list-pids" to print list of running services and their pids\e[0m\n\n> ' CMD
+done
+
+eval_with_no_print_directory "$CMD"
+
+echo ""
+
+if [[ $GITPOD_WORKSPACE_URL ]] || [[ $CODESPACES ]]; then
 	echo -e -n "${GREEN}open the vscode command palette (Shift+Command+P on Mac, Ctrl+Shift+P on Windows/Linux) and run \"View: Toggle Ports\" to view ports opened by running services. press any key to continue${RESET}\n\n>"
 	read -n 1
 
 	echo ""
 fi
 
-if [[ $GITPOD_WORKSPACE_URL ]]; then
-	echo -e -n "${GREEN}open the thunder client by selecting the lightning bolt icon on the activity bar (left side) and navigate to the collections tab to test graphql requests. press any key to continue${RESET}\n\n>"
-else
-	echo -e -n "${GREEN}install the rangav.vscode-thunder-client vscode extension and navigate to the collections tab to test graphql requests. press any key to continue${RESET}\n\n>"
-fi
-read -n 1
-
-echo ""
-
 echo -e -n "${GREEN}OPTIONAL: \"aws configure && make build-dev\" to build a cloud dev environment in your own aws account (cost: 0.60/day). press any key to continue${RESET}\n\n>"
 read -n 1
 
 echo ""
 
-echo -e "${GREEN}\"make compose-down\" to clean up${RESET}"
+while [[ $CMD != "make stop-dev" ]]; do
+	read -rep $'\e[32m"make stop-dev" to stop services locally\e[0m\n\n> ' CMD
+done
+
+eval "$CMD"
 
 echo ""
 
-echo -e "\033[0;36mwelcome to systemaccounting${RESET}"
+echo -e "\033[0;36m\"make dev\" to start services localy again. welcome to systemaccounting${RESET}"
 
 echo ""
