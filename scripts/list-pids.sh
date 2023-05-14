ad4cbbf7c496e61f1a3c95b5bef48391d322f857
max funk <mxfactorial@users.noreply.github.com>
2023-05-14 15:31:28 -0700

    separate workflow from dev process patterns

--- scripts/list-pids.sh
+++ scripts/list-pids.sh
@@ -1,25 +1,51 @@
 #!/bin/bash
 
 PROJECT_CONF=project.yaml
-SERVICE_PROCESSES=($(yq '.scripts.env_var.set.SERVICE_PROCESSES.default | join("|")' $PROJECT_CONF))
-LIST_SIZE=$(yq '.scripts.env_var.set.SERVICE_PROCESSES.default | length' $PROJECT_CONF)
-CLIENT_PROCESS=($(yq '.scripts.env_var.set.CLIENT_PROCESS.default' $PROJECT_CONF))
-SERVICE_PROCESSES+=("|") # add IFS to terminate last index
+
+# custom IFS since process list entries have spaces
+CUSTOM_IFS=$(yq '.scripts.env_var.set.CUSTOM_IFS.default' $PROJECT_CONF)
+
+# local/devcontainer env
+SERVICE_PROCESSES=($(yq ".scripts.env_var.set.SERVICE_PROCESSES.default | join(\"$CUSTOM_IFS\")" $PROJECT_CONF))
+CLIENT_PROCESS=$(yq '.scripts.env_var.set.CLIENT_PROCESS.default' $PROJECT_CONF)
+
+# workflows
+CI_SERVICE_PROCESSES=($(yq ".scripts.env_var.set.CI_SERVICE_PROCESSES.default | join(\"$CUSTOM_IFS\")" $PROJECT_CONF))
+
+# measure array length with yq since CUSTOM_IFS used
+if [[ $CI ]]; then
+	SIZE=($(yq '.scripts.env_var.set.CI_SERVICE_PROCESSES.default | length' $PROJECT_CONF))
+else
+	SIZE=($(yq '.scripts.env_var.set.SERVICE_PROCESSES.default | length' $PROJECT_CONF))
+fi
 
 declare GREP_ARGS
 
-COUNT=1
-while IFS='|' read -r -d '|' line; do
-	ADJUSTED="$line" # removing trailing whitespace in last index
-	if [[ $COUNT -eq $LIST_SIZE ]]; then
-		ADJUSTED="${line::-1}"
-	fi
-	GREP_ARGS+="-e '$ADJUSTED' "
-	COUNT=$(($COUNT+1))
-done < <(echo "${SERVICE_PROCESSES[@]}")
+function create_grep_args() {
+	local PROCESSES=("$@")
+	PROCESSES+=($CUSTOM_IFS) # add IFS to terminate last index
+	local COUNT=1
 
-GREP_ARGS="${GREP_ARGS::-1}" # removing trailing whitespace in last append
+	while IFS='|' read -r -d '|' line; do
+		# required to remove trailing whitespace added by terminating IFS in last index
+		ADJUSTED="$line"
+		if [[ $COUNT -eq $SIZE ]]; then
+			ADJUSTED="${line::-1}" # remove trailing whitespace
+		fi
+		GREP_ARGS+="-e '$ADJUSTED' "
+		COUNT=$(($COUNT+1))
+	done < <(echo "${PROCESSES[@]}")
 
+	GREP_ARGS="${GREP_ARGS::-1}" # removing trailing whitespace in last append
+}
 
-ps aux | eval "grep $GREP_ARGS" | grep -v 'grep' | awk '{len=split($14, a, "/"); print $2" "a[len-1]}'
-ps aux | eval "grep -e '$CLIENT_PROCESS'" | grep -v 'grep' | awk '{print $2" client"}'
+# calling create_grep_args appends to GREP_ARGS variable
+
+if [[ $CI ]]; then # client not used in workflows
+	create_grep_args "${CI_SERVICE_PROCESSES[@]}"
+	ps aux | eval "grep $GREP_ARGS" | grep -v 'grep' | awk '{print $2}'
+else
+	create_grep_args "${SERVICE_PROCESSES[@]}"
+	ps aux | eval "grep $GREP_ARGS" | grep -v 'grep' | awk '{len=split($14, a, "/"); print $2" "a[len-1]}'
+	ps aux | eval "grep -e '$CLIENT_PROCESS'" | grep -v 'grep' | awk '{print $2" client"}'
+fi
--- scripts/stop-local.sh
+++ scripts/stop-local.sh
@@ -2,24 +2,73 @@
 
 PROJECT_CONF=project.yaml
 
-SERVICE_PROCESSES=($(yq '.scripts.env_var.set.SERVICE_PROCESSES.default | join("|")' $PROJECT_CONF))
-LIST_SIZE=$(yq '.scripts.env_var.set.SERVICE_PROCESSES.default | length' $PROJECT_CONF)
-CLIENT_PROCESS=($(yq '.scripts.env_var.set.CLIENT_PROCESS.default' $PROJECT_CONF))
-SERVICE_PROCESSES+=("|") # add IFS to terminate last index
+# custom IFS since process list entries have spaces
+CUSTOM_IFS=$(yq '.scripts.env_var.set.CUSTOM_IFS.default' $PROJECT_CONF)
 
-declare -a SVC_PATTERNS
+# services started by cargo-watch
+SERVICE_PROCESSES=($(yq ".scripts.env_var.set.SERVICE_PROCESSES.default | join(\"$CUSTOM_IFS\")" $PROJECT_CONF))
+SERVICE_PROCESSES+=($CUSTOM_IFS) # add IFS to terminate last index
+SVC_LIST_SIZE=($(yq '.scripts.env_var.set.SERVICE_PROCESSES.default | length' $PROJECT_CONF))
 
-COUNT=1
-while IFS='|' read -r -d '|' line; do
-	ADJUSTED="$line" # removing trailing whitespace to last index
-	if [[ $COUNT -eq $LIST_SIZE ]]; then
-		ADJUSTED="${line::-1}"
-	fi
-	SVC_PATTERNS+=("'$ADJUSTED'")
-	COUNT=$(($COUNT+1))
-done < <(echo "${SERVICE_PROCESSES[@]}")
+# left over processes to term
+ADDED_PROCESSES_TO_TERM=($(yq ".scripts.env_var.set.ADDED_PROCESSES_TO_TERM.default | join(\"$CUSTOM_IFS\")" $PROJECT_CONF))
+ADDED_PROCESSES_TO_TERM+=($CUSTOM_IFS)
+ADDED_LIST_SIZE=($(yq '.scripts.env_var.set.ADDED_PROCESSES_TO_TERM.default | length' $PROJECT_CONF))
 
-PATTERNS=("${SVC_PATTERNS[@]}" "'$CLIENT_PROCESS'" "'go-build'" "'npm run dev'" "'npm run build'" "'vite build'" "'tail -F'")
+# services started in workflows without cargo-watch
+CI_SERVICE_PROCESSES=($(yq ".scripts.env_var.set.CI_SERVICE_PROCESSES.default | join(\"$CUSTOM_IFS\")" $PROJECT_CONF))
+CI_SERVICE_PROCESSES+=($CUSTOM_IFS)
+CI_SVC_LIST_SIZE=($(yq '.scripts.env_var.set.CI_SERVICE_PROCESSES.default | length' $PROJECT_CONF))
+
+CLIENT_PROCESS=$(yq '.scripts.env_var.set.CLIENT_PROCESS.default' $PROJECT_CONF)
+
+declare -a PATTERNS
+
+if [[ $CI ]]; then
+	# add ci processes to PATTERNS
+	COUNT=1
+	while IFS='|' read -r -d '|' line; do
+		ADJUSTED="$line"
+		if [[ $COUNT -eq $CI_SVC_LIST_SIZE ]]; then
+			ADJUSTED="${line::-1}"
+		fi
+		PATTERNS+=("'$ADJUSTED'")
+		COUNT=$(($COUNT+1))
+	done < <(echo "${CI_SERVICE_PROCESSES[@]}")
+
+	COUNT=1
+	while IFS='|' read -r -d '|' line; do
+		ADJUSTED="$line"
+		if [[ $COUNT -eq $ADDED_LIST_SIZE ]]; then
+			ADJUSTED="${line::-1}"
+		fi
+		PATTERNS+=("'$ADJUSTED'")
+		COUNT=$(($COUNT+1))
+	done < <(echo "${ADDED_PROCESSES_TO_TERM[@]}")
+else
+	# add cargo-watch processes to PATTERNS when local/devcontainer
+	COUNT=1
+	while IFS='|' read -r -d '|' line; do
+		ADJUSTED="$line" # remove trailing whitespace from last index
+		if [[ $COUNT -eq $SVC_LIST_SIZE ]]; then
+			ADJUSTED="${line::-1}"
+		fi
+		PATTERNS+=("'$ADJUSTED'")
+		COUNT=$(($COUNT+1))
+	done < <(echo "${SERVICE_PROCESSES[@]}")
+
+	COUNT=1
+	while IFS='|' read -r -d '|' line; do
+		ADJUSTED="$line"
+		if [[ $COUNT -eq $ADDED_LIST_SIZE ]]; then
+			ADJUSTED="${line::-1}"
+		fi
+		PATTERNS+=("'$ADJUSTED'")
+		COUNT=$(($COUNT+1))
+	done < <(echo "${ADDED_PROCESSES_TO_TERM[@]}")
+
+	PATTERNS+=("'$CLIENT_PROCESS'") # add client process
+fi
 
 source ./scripts/manage-cde-ports.sh
 disable_cde_ports
