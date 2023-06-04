93b8635417f572787df046305baf507128c0a6f7
max funk <mxfactorial@users.noreply.github.com>
2023-06-04 15:31:50 -0700

    add client graphql queries

---
+++ client/src/graphql/query/transactionByID.ts
@@ -0,0 +1,38 @@
+import { gql } from "@urql/core";
+
+const TRANSACTION_BY_ID_QUERY = gql`
+	query getTransactionByID($id: String!, $auth_account: String!) {
+		transactionByID(id: $id, auth_account: $auth_account) {
+			id
+			rule_instance_id
+			author
+			author_device_id
+			author_device_latlng
+			author_role
+			sum_value
+			transaction_items {
+				id
+				transaction_id
+				item_id
+				price
+				quantity
+				debitor_first
+				rule_instance_id
+				unit_of_measurement
+				units_measured
+				debitor
+				creditor
+				debitor_profile_id
+				creditor_profile_id
+				debitor_approval_time
+				creditor_approval_time
+				debitor_expiration_time
+				creditor_expiration_time
+				debitor_rejection_time
+				creditor_rejection_time
+			}
+		}
+	}
+`;
+
+export default TRANSACTION_BY_ID_QUERY;
