0f349511340321452bd370bc2df66e0fcaafc311
max funk <mxfactorial@users.noreply.github.com>
2023-06-04 15:25:08 -0700

    add client constants

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,9 @@
+const STORAGE_PREFIX = 'mxf';
+
+export default {
+	ACCOUNT_KEY: `${STORAGE_PREFIX}:account`,
+	REQUEST_CREATE_KEY: `${STORAGE_PREFIX}:requestCreate`,
+	REQUESTS_PENDING_KEY: `${STORAGE_PREFIX}:requestsPending`,
+	HISTORY_KEY: `${STORAGE_PREFIX}:history`,
+	CLIENT_CTX_KEY: 'client',
+}
