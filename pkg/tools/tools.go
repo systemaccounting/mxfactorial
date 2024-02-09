e1bbe26443bee99ad46a219de4df81659daee2e8
max funk <mxfactorial@users.noreply.github.com>
2024-02-08 17:27:46 -0700

    remove unused go package

--- pkg/tools/tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-// +build tools
-
-package tools
-
-import (
-	_ "github.com/99designs/gqlgen"
-	_ "github.com/golang/mock/mockgen" // todo: switch mockgen to go:generate comments
-)
