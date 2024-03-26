4fa44e6edef1b547f8b03cdec9c21a75aeb2c10d
max funk <mxfactorial@users.noreply.github.com>
2024-03-25 21:52:48 -0700

    switch lambdas to provided lambda terraform module

--- infrastructure/terraform/aws/modules/environment/v001/lambda-services.tf
+++ infrastructure/terraform/aws/modules/environment/v001/lambda-services.tf
@@ -1,39 +1,39 @@
 // modules require ${service_name}-src.zip in artifacts s3
 
 module "request_create" {
-  source       = "../../web-adapter-lambda/v001"
+  source       = "../../provided-lambda/v001"
   service_name = "request-create"
   env          = var.env
   ssm_prefix   = var.ssm_prefix
   env_id       = var.env_id
   env_vars = merge(local.POSTGRES_VARS, {
-    RULE_URL             = module.rule.lambda_function_url
+    RULE_URL = module.rule.lambda_function_url
   })
-  aws_lwa_port              = local.REQUEST_CREATE_PORT
-  invoke_url_principals     = [aws_iam_role.graphql_role.arn]
-  artifacts_bucket_name     = var.artifacts_bucket_name
-  attached_policy_arns      = []
-  create_secret             = true // suppports local testing
+  aws_lwa_port          = local.REQUEST_CREATE_PORT
+  invoke_url_principals = [aws_iam_role.graphql_role.arn]
+  artifacts_bucket_name = var.artifacts_bucket_name
+  attached_policy_arns  = []
+  create_secret         = true // suppports local testing
 }
 
 ##########################
 
 module "request_approve" {
-  source       = "../../web-adapter-lambda/v001"
-  service_name = "request-approve"
-  env          = var.env
-  ssm_prefix   = var.ssm_prefix
-  env_id       = var.env_id
-  env_vars = merge(local.POSTGRES_VARS, {})
-  aws_lwa_port              = local.REQUEST_APPROVE_PORT
-  invoke_url_principals     = [aws_iam_role.graphql_role.arn]
-  artifacts_bucket_name     = var.artifacts_bucket_name
-  create_secret             = true
-  attached_policy_arns      = []
+  source                = "../../provided-lambda/v001"
+  service_name          = "request-approve"
+  env                   = var.env
+  ssm_prefix            = var.ssm_prefix
+  env_id                = var.env_id
+  env_vars              = merge(local.POSTGRES_VARS, {})
+  aws_lwa_port          = local.REQUEST_APPROVE_PORT
+  invoke_url_principals = [aws_iam_role.graphql_role.arn]
+  artifacts_bucket_name = var.artifacts_bucket_name
+  create_secret         = true
+  attached_policy_arns  = []
 }
 
 module "requests_by_account" {
-  source       = "../../web-adapter-lambda/v001"
+  source       = "../../provided-lambda/v001"
   service_name = "requests-by-account"
   env          = var.env
   ssm_prefix   = var.ssm_prefix
@@ -41,27 +41,27 @@ module "requests_by_account" {
   env_vars = merge(local.POSTGRES_VARS, {
     RETURN_RECORD_LIMIT = local.RETURN_RECORD_LIMIT
   })
-  aws_lwa_port              = local.REQUESTS_BY_ACCOUNT_PORT
-  invoke_url_principals     = [aws_iam_role.graphql_role.arn]
-  artifacts_bucket_name     = var.artifacts_bucket_name
-  create_secret             = true
+  aws_lwa_port          = local.REQUESTS_BY_ACCOUNT_PORT
+  invoke_url_principals = [aws_iam_role.graphql_role.arn]
+  artifacts_bucket_name = var.artifacts_bucket_name
+  create_secret         = true
 }
 
 module "request_by_id" {
-  source                    = "../../web-adapter-lambda/v001"
-  service_name              = "request-by-id"
-  env                       = var.env
-  ssm_prefix                = var.ssm_prefix
-  env_id                    = var.env_id
-  env_vars                  = merge(local.POSTGRES_VARS, {})
-  aws_lwa_port              = local.REQUEST_BY_ID_PORT
-  invoke_url_principals     = [aws_iam_role.graphql_role.arn]
-  artifacts_bucket_name     = var.artifacts_bucket_name
-  create_secret             = true
+  source                = "../../provided-lambda/v001"
+  service_name          = "request-by-id"
+  env                   = var.env
+  ssm_prefix            = var.ssm_prefix
+  env_id                = var.env_id
+  env_vars              = merge(local.POSTGRES_VARS, {})
+  aws_lwa_port          = local.REQUEST_BY_ID_PORT
+  invoke_url_principals = [aws_iam_role.graphql_role.arn]
+  artifacts_bucket_name = var.artifacts_bucket_name
+  create_secret         = true
 }
 
 module "transactions_by_account" {
-  source       = "../../web-adapter-lambda/v001"
+  source       = "../../provided-lambda/v001"
   service_name = "transactions-by-account"
   env          = var.env
   ssm_prefix   = var.ssm_prefix
@@ -70,39 +70,39 @@ module "transactions_by_account" {
   env_vars = merge(local.POSTGRES_VARS, {
     RETURN_RECORD_LIMIT = local.RETURN_RECORD_LIMIT
   })
-  invoke_url_principals     = [aws_iam_role.graphql_role.arn]
-  artifacts_bucket_name     = var.artifacts_bucket_name
-  create_secret             = true
+  invoke_url_principals = [aws_iam_role.graphql_role.arn]
+  artifacts_bucket_name = var.artifacts_bucket_name
+  create_secret         = true
 }
 
 module "transaction_by_id" {
-  source                    = "../../web-adapter-lambda/v001"
-  service_name              = "transaction-by-id"
-  env                       = var.env
-  ssm_prefix                = var.ssm_prefix
-  env_id                    = var.env_id
-  env_vars                  = merge(local.POSTGRES_VARS, {})
-  aws_lwa_port              = local.TRANSACTION_BY_ID_PORT
-  invoke_url_principals     = [aws_iam_role.graphql_role.arn]
-  artifacts_bucket_name     = var.artifacts_bucket_name
-  create_secret             = true
+  source                = "../../provided-lambda/v001"
+  service_name          = "transaction-by-id"
+  env                   = var.env
+  ssm_prefix            = var.ssm_prefix
+  env_id                = var.env_id
+  env_vars              = merge(local.POSTGRES_VARS, {})
+  aws_lwa_port          = local.TRANSACTION_BY_ID_PORT
+  invoke_url_principals = [aws_iam_role.graphql_role.arn]
+  artifacts_bucket_name = var.artifacts_bucket_name
+  create_secret         = true
 }
 
 module "balance_by_account" {
-  source                    = "../../web-adapter-lambda/v001"
-  service_name              = "balance-by-account"
-  env                       = var.env
-  ssm_prefix                = var.ssm_prefix
-  env_id                    = var.env_id
-  env_vars                  = merge(local.POSTGRES_VARS, {})
-  aws_lwa_port              = local.BALANCE_BY_ACCOUNT_PORT
-  invoke_url_principals     = [aws_iam_role.graphql_role.arn]
-  artifacts_bucket_name     = var.artifacts_bucket_name
-  create_secret             = true
+  source                = "../../provided-lambda/v001"
+  service_name          = "balance-by-account"
+  env                   = var.env
+  ssm_prefix            = var.ssm_prefix
+  env_id                = var.env_id
+  env_vars              = merge(local.POSTGRES_VARS, {})
+  aws_lwa_port          = local.BALANCE_BY_ACCOUNT_PORT
+  invoke_url_principals = [aws_iam_role.graphql_role.arn]
+  artifacts_bucket_name = var.artifacts_bucket_name
+  create_secret         = true
 }
 
 module "auto_confirm" {
-  source       = "../../go-lambda/v001"
+  source       = "../../provided-lambda/v001"
   service_name = "auto-confirm"
   env          = var.env
   ssm_prefix   = var.ssm_prefix
@@ -182,7 +182,7 @@ module "notifications_clear" {
 }
 
 module "rule" {
-  source       = "../../web-adapter-lambda/v001"
+  source       = "../../provided-lambda/v001"
   service_name = "rule"
   env          = var.env
   ssm_prefix   = var.ssm_prefix
@@ -195,7 +195,7 @@ module "rule" {
     aws_iam_role.graphql_role.arn,
     module.request_create.lambda_role_arn,
   ]
-  artifacts_bucket_name     = var.artifacts_bucket_name
-  attached_policy_arns      = []
-  create_secret             = true // suppports local testing
+  artifacts_bucket_name = var.artifacts_bucket_name
+  attached_policy_arns  = []
+  create_secret         = true // suppports local testing
 }
--- infrastructure/terraform/aws/modules/environment/v001/locals.tf
+++ infrastructure/terraform/aws/modules/environment/v001/locals.tf
@@ -6,7 +6,7 @@ locals {
   SERVICES_CONF                = local.PROJECT_CONF.services
   RULE_PORT                    = local.SERVICES_CONF.rule.env_var.set.RULE_PORT.default
   GRAPHQL_PORT                 = local.SERVICES_CONF.graphql.env_var.set.GRAPHQL_PORT.default
-  READINESS_CHECK_PATH         = local.PROJECT_CONF.infrastructure.terraform.aws.modules.web-adapter-lambda.env_var.set.READINESS_CHECK_PATH.default
+  READINESS_CHECK_PATH         = local.PROJECT_CONF.infrastructure.terraform.aws.modules.environment.env_var.set.READINESS_CHECK_PATH.default
   BALANCE_BY_ACCOUNT_PORT      = local.SERVICES_CONF.balance-by-account.env_var.set.BALANCE_BY_ACCOUNT_PORT.default
   TRANSACTION_BY_ID_PORT       = local.SERVICES_CONF.transaction-by-id.env_var.set.TRANSACTION_BY_ID_PORT.default
   TRANSACTIONS_BY_ACCOUNT_PORT = local.SERVICES_CONF.transactions-by-account.env_var.set.TRANSACTIONS_BY_ACCOUNT_PORT.default
@@ -15,7 +15,8 @@ locals {
   REQUEST_APPROVE_PORT         = local.SERVICES_CONF.request-approve.env_var.set.REQUEST_APPROVE_PORT.default
   REQUEST_CREATE_PORT          = local.SERVICES_CONF.request-create.env_var.set.REQUEST_CREATE_PORT.default
   RETURN_RECORD_LIMIT          = local.SERVICES_CONF.env_var.set.RETURN_RECORD_LIMIT.default
-  WEB_ADAPTER_LAYER_VERSION    = local.PROJECT_CONF.infrastructure.terraform.aws.modules.web-adapter-lambda.env_var.set.WEB_ADAPTER_LAYER_VERSION.default
+  WEB_ADAPTER_LAYER_VERSION    = local.PROJECT_CONF.infrastructure.terraform.aws.modules.environment.env_var.set.WEB_ADAPTER_LAYER_VERSION.default
+  WEB_ADAPTER_LAYER_ARN        = "arn:aws:lambda:${data.aws_region.current.name}:753240598075:layer:LambdaAdapterLayerX86:${local.WEB_ADAPTER_LAYER_VERSION}"
   GO_MIGRATE_PORT              = local.PROJECT_CONF.migrations.go-migrate.env_var.set.GO_MIGRATE_PORT.default
   BINARY_NAME                  = local.SERVICES_CONF.env_var.set.BINARY_NAME.default
   LAMBDA_RUNTIME               = local.PROJECT_CONF.infrastructure.terraform.aws.modules.env_var.set.LAMBDA_RUNTIME.default
