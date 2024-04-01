7b681a4997aabbc1e390ca28daac35f13f7455d1
max funk <mxfactorial@users.noreply.github.com>
2024-03-31 22:36:49 -0700

    remove bucket config deprecation warning

--- infrastructure/terraform/aws/modules/project-storage/v001/s3.tf
+++ infrastructure/terraform/aws/modules/project-storage/v001/s3.tf
@@ -1,26 +1,30 @@
 resource "aws_s3_bucket" "artifacts" {
   bucket        = "${var.artifacts_bucket_name_prefix}-${local.ID_ENV}"
-  force_destroy = true
+  force_destroy = var.force_destroy_storage
 }
 
 resource "aws_s3_bucket" "tfstate" {
   bucket        = "${var.tfstate_bucket_name_prefix}-${local.ID_ENV}"
-  force_destroy = var.force_destroy_tfstate
+  force_destroy = var.force_destroy_storage
 }
 
 resource "aws_s3_bucket" "client_origin" {
-  bucket = "${var.client_origin_bucket_name_prefix}-${local.ID_ENV}"
+  bucket        = "${var.client_origin_bucket_name_prefix}-${local.ID_ENV}"
+  force_destroy = var.force_destroy_storage
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+resource "aws_s3_bucket_website_configuration" "client_origin" {
+  bucket = aws_s3_bucket.client_origin.id
+  index_document {
+    suffix = "index.html"
   }
-
-  force_destroy = true
+  error_document {
+    key = "error.html"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "client_origin" {
-  bucket = aws_s3_bucket.client_origin.id
+  bucket                  = aws_s3_bucket.client_origin.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
