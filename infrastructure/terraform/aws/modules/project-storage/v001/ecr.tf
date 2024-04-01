a7d3a9489d879a790183679887160ad00a93ee76
max funk <mxfactorial@users.noreply.github.com>
2024-03-31 22:37:25 -0700

    set ecr repo name with local

--- infrastructure/terraform/aws/modules/project-storage/v001/ecr.tf
+++ infrastructure/terraform/aws/modules/project-storage/v001/ecr.tf
@@ -1,8 +1,7 @@
 resource "aws_ecr_repository" "go_migrate" {
-  name                 = "go-migrate-${local.ID_ENV}"
+  name                 = "${local.GO_MIGRATE}-${local.ID_ENV}"
   image_tag_mutability = "MUTABLE"
-  force_delete         = true
-
+  force_delete         = var.force_destroy_storage
   image_scanning_configuration {
     scan_on_push = true
   }
