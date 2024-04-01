9d00a09912dfb20150fd1422404bebd2dd708418
max funk <mxfactorial@users.noreply.github.com>
2024-03-31 22:35:17 -0700

    test for missing project.yaml values in tf locals

--- infrastructure/terraform/aws/modules/project-storage/v001/locals.tf
+++ infrastructure/terraform/aws/modules/project-storage/v001/locals.tf
@@ -1,3 +1,19 @@
 locals {
-  ID_ENV = "${var.env_id}-${var.env}"
+  ID_ENV                = "${var.env_id}-${var.env}"
+  PROJECT_CONF          = "project.yaml"
+  CONF_FILE             = yamldecode(file("../../../../../${local.PROJECT_CONF}"))
+  STORAGE_ENV_VAR       = local.CONF_FILE.infrastructure.terraform.aws.modules.project-storage.env_var.set
+  DDB_TABLE_NAME_PREFIX = local.STORAGE_ENV_VAR.DDB_TABLE_NAME_PREFIX.default
+  DDB_TABLE_HASH_KEY    = local.STORAGE_ENV_VAR.DDB_TABLE_HASH_KEY.default
+  GO_MIGRATE            = "go-migrate"
+}
+
+// fails if services are not found in project.yaml
+resource "terraform_data" "locals_test" {
+  lifecycle {
+    precondition {
+      condition     = lookup(local.CONF_FILE.migrations, local.GO_MIGRATE, null) != null
+      error_message = "${local.GO_MIGRATE} not found in ${local.PROJECT_CONF}"
+    }
+  }
 }
