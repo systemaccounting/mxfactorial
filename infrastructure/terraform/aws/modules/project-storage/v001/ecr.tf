bd9ca72d119af9c54e57a0107a909a0526902aa8
max funk <mxfactorial@users.noreply.github.com>
2024-03-31 22:57:48 -0700

    add ecr repos for remaining services

--- infrastructure/terraform/aws/modules/project-storage/v001/ecr.tf
+++ infrastructure/terraform/aws/modules/project-storage/v001/ecr.tf
@@ -6,3 +6,94 @@ resource "aws_ecr_repository" "go_migrate" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "auto_confirm" {
+  name                 = "${local.AUTO_CONFIRM}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+
+resource "aws_ecr_repository" "balance_by_account" {
+  name                 = "${local.BALANCE_BY_ACCOUNT}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "graphql" {
+  name                 = "${local.GRAPHQL}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "request_approve" {
+  name                 = "${local.REQUEST_APPROVE}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "request_by_id" {
+  name                 = "${local.REQUEST_BY_ID}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "requests_by_account" {
+  name                 = "${local.REQUESTS_BY_ACCOUNT}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "request_create" {
+  name                 = "${local.REQUEST_CREATE}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "rule" {
+  name                 = "${local.RULE}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "transaction_by_id" {
+  name                 = "${local.TRANSACTION_BY_ID}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "transactions_by_account" {
+  name                 = "${local.TRANSACTIONS_BY_ACCOUNT}-${local.ID_ENV}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = var.force_destroy_storage
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
--- infrastructure/terraform/aws/modules/project-storage/v001/locals.tf
+++ infrastructure/terraform/aws/modules/project-storage/v001/locals.tf
@@ -1,19 +1,80 @@
 locals {
-  ID_ENV                = "${var.env_id}-${var.env}"
-  PROJECT_CONF          = "project.yaml"
-  CONF_FILE             = yamldecode(file("../../../../../${local.PROJECT_CONF}"))
-  STORAGE_ENV_VAR       = local.CONF_FILE.infrastructure.terraform.aws.modules.project-storage.env_var.set
-  DDB_TABLE_NAME_PREFIX = local.STORAGE_ENV_VAR.DDB_TABLE_NAME_PREFIX.default
-  DDB_TABLE_HASH_KEY    = local.STORAGE_ENV_VAR.DDB_TABLE_HASH_KEY.default
-  GO_MIGRATE            = "go-migrate"
+  ID_ENV                  = "${var.env_id}-${var.env}"
+  PROJECT_CONF            = "project.yaml"
+  CONF_FILE               = yamldecode(file("../../../../../${local.PROJECT_CONF}"))
+  STORAGE_ENV_VAR         = local.CONF_FILE.infrastructure.terraform.aws.modules.project-storage.env_var.set
+  DDB_TABLE_NAME_PREFIX   = local.STORAGE_ENV_VAR.DDB_TABLE_NAME_PREFIX.default
+  DDB_TABLE_HASH_KEY      = local.STORAGE_ENV_VAR.DDB_TABLE_HASH_KEY.default
+  GO_MIGRATE              = "go-migrate"
+  AUTO_CONFIRM            = "auto-confirm"
+  BALANCE_BY_ACCOUNT      = "balance-by-account"
+  GRAPHQL                 = "graphql"
+  REQUEST_APPROVE         = "request-approve"
+  REQUEST_BY_ID           = "request-by-id"
+  REQUESTS_BY_ACCOUNT     = "requests-by-account"
+  REQUEST_CREATE          = "request-create"
+  RULE                    = "rule"
+  TRANSACTION_BY_ID       = "transaction-by-id"
+  TRANSACTIONS_BY_ACCOUNT = "transactions-by-account"
+
 }
 
-// fails if services are not found in project.yaml
+// fails if services not found in project.yaml
 resource "terraform_data" "locals_test" {
   lifecycle {
     precondition {
       condition     = lookup(local.CONF_FILE.migrations, local.GO_MIGRATE, null) != null
       error_message = "${local.GO_MIGRATE} not found in ${local.PROJECT_CONF}"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.AUTO_CONFIRM, null) != null
+      error_message = "${local.AUTO_CONFIRM} not found in ${local.PROJECT_CONF}"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.BALANCE_BY_ACCOUNT, null) != null
+      error_message = "${local.BALANCE_BY_ACCOUNT} not found in ${local.PROJECT_CONF}"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.GRAPHQL, null) != null
+      error_message = "${local.GRAPHQL} not found in ${local.PROJECT_CONF}"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.REQUEST_APPROVE, null) != null
+      error_message = "${local.REQUEST_APPROVE} not found in ${local.PROJECT_CONF}"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.REQUEST_BY_ID, null) != null
+      error_message = "${local.REQUEST_BY_ID} not found in ${local.PROJECT_CONF}"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.REQUESTS_BY_ACCOUNT, null) != null
+      error_message = "${local.REQUESTS_BY_ACCOUNT} not found in ${local.PROJECT_CONF}"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.REQUEST_CREATE, null) != null
+      error_message = "${local.REQUEST_CREATE} not found in ${local.PROJECT_CONF}"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.RULE, null) != null
+      error_message = "${local.RULE} not found in ${local.PROJECT_CONF}"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.TRANSACTION_BY_ID, null) != null
+      error_message = "${local.TRANSACTION_BY_ID} not found"
+    }
+
+    precondition {
+      condition     = lookup(local.CONF_FILE.services, local.TRANSACTIONS_BY_ACCOUNT, null) != null
+      error_message = "${local.TRANSACTIONS_BY_ACCOUNT} not found in ${local.PROJECT_CONF}"
     }
   }
 }
