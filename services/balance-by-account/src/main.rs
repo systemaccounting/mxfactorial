51bc1d1cd57ff1a98cc50a829da405c51900149f
max funk <mxfactorial@users.noreply.github.com>
2024-04-12 17:28:30 -0700

    remove default port assignment

--- services/balance-by-account/src/main.rs
+++ services/balance-by-account/src/main.rs
@@ -53,7 +53,7 @@ async fn main() {
 
     let hostname_or_ip = env::var("HOSTNAME_OR_IP").unwrap_or("0.0.0.0".to_string());
 
-    let port = env::var("BALANCE_BY_ACCOUNT_PORT").unwrap_or("10004".to_string());
+    let port = env::var("BALANCE_BY_ACCOUNT_PORT").unwrap();
 
     let serve_addr = format!("{hostname_or_ip}:{port}");
 
--- services/graphql/src/main.rs
+++ services/graphql/src/main.rs
@@ -268,7 +268,7 @@ async fn main() {
 
     let hostname_or_ip = env::var("HOSTNAME_OR_IP").unwrap_or("0.0.0.0".to_string());
 
-    let port = env::var("GRAPHQL_PORT").unwrap_or("10000".to_string());
+    let port = env::var("GRAPHQL_PORT").unwrap();
 
     let serve_addr = format!("{hostname_or_ip}:{port}");
 
