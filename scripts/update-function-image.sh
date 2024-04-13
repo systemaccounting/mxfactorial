031b1618d97d07ab62c475ae6de952e0087943aa
max funk <mxfactorial@users.noreply.github.com>
2024-04-12 17:38:51 -0700

    pass image tag to function deploy script

--- scripts/update-function-image.sh
+++ scripts/update-function-image.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-if [[ "$#" -ne 4 ]]; then
+if [[ "$#" -ne 6 ]]; then
 	cat <<- 'EOF'
 	use:
 	bash scripts/update-function-image.sh \
 	        --app-name go-migrate \
+			--curr-tag 123456789101.dkr.ecr.us-east-1.amazonaws.com/rule-12345-dev:93496996 \
 	        --env dev
 	EOF
 	exit 1
@@ -15,6 +16,7 @@ fi
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --app-name) APP_NAME="$2"; shift ;;
+		--curr-tag) CURR_TAG="$2"; shift ;;
         --env) ENV="$2"; shift ;;
         *) echo "unknown parameter passed: $1"; exit 1 ;;
     esac
@@ -24,17 +26,14 @@ done
 PROJECT_CONF=project.yaml
 ENV_ID=$(source scripts/print-env-id.sh)
 ID_ENV="$ENV_ID-$ENV"
-REPO=$(source scripts/print-ecr-repo-name.sh --image-prefix $APP_NAME --env $ENV)
-TAG_VERSION=latest
-IMAGE_NAME=$REPO:$TAG_VERSION
 LAMBDA_NAME="$APP_NAME-$ID_ENV"
 REGION=$(yq '.infrastructure.terraform.aws.modules.environment.env_var.set.REGION.default' $PROJECT_CONF)
 
-MOD=$(aws lambda update-function-code \
+LAST_MOD=$(aws lambda update-function-code \
 		--function-name="$LAMBDA_NAME" \
-		--image-uri=$IMAGE_NAME \
+		--image-uri=$CURR_TAG \
 		--region=$REGION \
 		--query 'LastModified' \
 		--output text)
 
-echo "*** $LAMBDA_NAME lambda deployed @ $MOD"
+echo "*** $LAMBDA_NAME lambda deployed @ $LAST_MOD"
