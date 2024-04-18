71b140dfdfde155a6b8a7245e89fbdee41f09d07
max funk <mxfactorial@users.noreply.github.com>
2024-04-17 23:04:54 -0700

    remove go mod download

--- scripts/post-create-cmd.sh
+++ scripts/post-create-cmd.sh
@@ -2,8 +2,6 @@
 
 # used as postCreateCommand in .devcontainer.json
 
-go mod download
-
 rustup component add llvm-tools-preview --toolchain stable-x86_64-unknown-linux-gnu
 
 make --no-print-directory start
