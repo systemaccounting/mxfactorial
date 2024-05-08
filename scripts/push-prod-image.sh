67cf4ce897961b775204dae8807ee5d91a38e03c
max funk <mxfactorial@users.noreply.github.com>
2024-05-08 10:13:20 -0700

    current config path

--- scripts/push-prod-image.sh
+++ scripts/push-prod-image.sh
@@ -35,7 +35,7 @@ ID_ENV="$ENV_ID-$ENV"
 ID_ENV_PREFIX="$ENV_ID/$ENV"
 REGION=$(yq '.infrastructure.terraform.aws.modules.environment.env_var.set.REGION.default' $PROJECT_CONF)
 REPO_NAME="$ID_ENV_PREFIX/$APP_NAME"
-PROD_ENV_ID=$(yq '.infrastructure.terraform.env-id.prod.env_var.set.PROD_ENV_ID.default' $PROJECT_CONF)
+PROD_ENV_ID=$(yq '.env_var.set.PROD_ENV_ID.default' $PROJECT_CONF)
 SHORT_GIT_SHA_LENGTH=$(yq '.scripts.env_var.set.SHORT_GIT_SHA_LENGTH.default' $PROJECT_CONF)
 
 MERGE_COMMIT_HASH=$(git rev-parse --short=$SHORT_GIT_SHA_LENGTH HEAD)
