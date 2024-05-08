83fe15f24bf392d75dc46a970ce8545f1f3f5892
max funk <mxfactorial@users.noreply.github.com>
2024-05-08 12:43:10 -0700

    remove vars from region terraform module and stack

--- infrastructure/terraform/aws/environments/region/main.tf
+++ infrastructure/terraform/aws/environments/region/main.tf
@@ -1,11 +1,6 @@
 locals {
   PROJECT_CONF = yamldecode(file("../../../../../project.yaml"))
   REGION       = local.PROJECT_CONF.infrastructure.terraform.aws.modules.environment.env_var.set.REGION.default
-  ENV_ID       = jsondecode(file("../../../env-id/terraform.tfstate")).outputs.env_id.value
-}
-
-terraform {
-  backend "s3" {} // override with scripts/terraform-init-dev.sh
 }
 
 provider "aws" {
@@ -14,6 +9,4 @@ provider "aws" {
 
 module "apigw_logging" {
   source = "../../modules/region/v001"
-  region = local.REGION
-  env_id = local.ENV_ID
 }
