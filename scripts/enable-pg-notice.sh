be3bbd408b900f32ff3f727bf125c74656900bcb
max funk <mxfactorial@users.noreply.github.com>
2024-08-13 11:53:21 -0700

    enable notice logging in docker postgres

---
+++ scripts/enable-pg-notice.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROJECT_CONF=project.yaml
+ENV_FILE_NAME=$(yq '.env_var.set.ENV_FILE_NAME.default' $PROJECT_CONF)
+MIGRATIONS_DIR_PATH=./migrations
+ENV_FILE="$MIGRATIONS_DIR_PATH/$ENV_FILE_NAME"
+
+source $ENV_FILE
+
+until pg_isready -h $PGHOST -U $PGUSER -d $PGDATABASE; do sleep 1; done
+
+psql -U $PGUSER -d $PGDATABASE -h $PGHOST -c "ALTER SYSTEM SET log_min_messages TO NOTICE;"
+psql -U $PGUSER -d $PGDATABASE -h $PGHOST -c "SELECT pg_reload_conf();"
