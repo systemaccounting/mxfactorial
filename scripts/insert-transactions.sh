0301d8bf6d0cf235ff0624bcdf1f02f6b678a736
max funk <mxfactorial@users.noreply.github.com>
2024-08-23 18:41:12 -0700

    support inserting test transactions in a long loop

--- scripts/insert-transactions.sh
+++ scripts/insert-transactions.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+if [[ "$#" -ne 0 ]] && [[ "$#" -ne 1 ]]; then
+	echo "use: bash scripts/insert-transactions.sh --continue"
+	exit 1
+fi
+
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+	--continue) CONT=1 ;;
+	*)
+		echo "unknown parameter passed: $1"
+		exit 1
+		;;
+	esac
+	shift
+done
+
 PROJECT_CONF=project.yaml
 LOCAL_ADDRESS=$(yq '.env_var.set.LOCAL_ADDRESS.default' $PROJECT_CONF)
 HOST="http://$LOCAL_ADDRESS"
@@ -12,9 +28,9 @@ REQUEST_APPROVE_URL=$HOST:$REQUEST_APPROVE_PORT
 TEST_DATA_DIR=./tests/testdata
 TEST_DATA_FILE_NAME=requests.json
 TEST_DATA_FILE=$TEST_DATA_DIR/$TEST_DATA_FILE_NAME
+TEST_DATA_FILE_LENGTH=$(yq -o=json 'length' $TEST_DATA_FILE)
 MIGRATIONS_DIR=./migrations
 TEST_ENV=dev
-STARTING_TRANS_ID=3 # start from 3 since transaction id 1 & 2 are created by migrations/testseed
 
 ENV_VAR_PATH='infrastructure.terraform.aws.modules.environment.env_var.set'
 export PGDATABASE=$(yq ".${ENV_VAR_PATH}.PGDATABASE.default" $PROJECT_CONF)
@@ -24,21 +40,21 @@ export PGHOST=$(yq ".${ENV_VAR_PATH}.PGHOST.default" $PROJECT_CONF)
 export PGPORT=$(yq ".${ENV_VAR_PATH}.PGPORT.default" $PROJECT_CONF)
 
 # reset postgres in docker
-(cd $MIGRATIONS_DIR; make resetdocker DB=test)
+(
+	cd $MIGRATIONS_DIR
+	make resetdocker DB=test
+)
 echo "*** finished migrations"
 
 echo ""
-echo "*** adding a mix of requests and transactions from $TEST_DATA_FILE"
 
-TRANSACTION_ID=$STARTING_TRANS_ID
-
-yq -I0 -o=json '.[]' $TEST_DATA_FILE | while IFS='\n' read transaction; do
-
+function transact() {
+	local transaction="$1"
 	# create requests for each in tests/testdata/requests.json
-	curl -s -H 'Content-Type: application/json' -d "$transaction" $REQUEST_CREATE_URL >/dev/null
+	TRANSACTION_ID=$(curl -s -H 'Content-Type: application/json' -d "$transaction" $REQUEST_CREATE_URL | yq '.transaction.id')
 
 	# approve every other request to create mix of requests and transactions
-	if [[ $(("$TRANSACTION_ID"%2)) -eq 0 ]]; then
+	if [[ $(("$TRANSACTION_ID" % 2)) -eq 0 ]]; then
 
 		# get debitor approver from user (NOT rule) added transaction item
 		DEBITOR_APPROVER=$(echo -n "$transaction" | yq -I0 '[.transaction.transaction_items[] | select(.rule_instance_id == null)][0] | .debitor')
@@ -49,11 +65,25 @@ yq -I0 -o=json '.[]' $TEST_DATA_FILE | while IFS='\n' read transaction; do
 		# approve transaction request
 		curl -s -H 'Content-Type: application/json' -d "$APPROVAL" $REQUEST_APPROVE_URL >/dev/null
 	fi
+}
 
-	printf '%s' '.'
-
-	TRANSACTION_ID=$((TRANSACTION_ID+1))
-done
-
-echo ""
-echo "*** $(yq -o=json 'length' $TEST_DATA_FILE) requests and transactions (mixed) added from $TEST_DATA_FILE"
+# continue inserting random transactions from testdata file indefinitely
+if [[ -n "$CONT" ]]; then
+	echo "*** adding a random mix of requests and transactions from $TEST_DATA_FILE indefinitely"
+	# increase account balances to avoid insufficient balance error
+	psql -U $PGUSER -d $PGDATABASE -h $PGHOST -c "UPDATE account_balance SET current_balance = 999999000 WHERE true;" >/dev/null
+	while true; do
+		RANDOM_INDEX=$((RANDOM % TEST_DATA_FILE_LENGTH))
+		transaction=$(yq -I0 -o=json ".[$RANDOM_INDEX]" $TEST_DATA_FILE)
+		transact "$transaction"
+	done
+else
+	echo "*** adding a mix of requests and transactions from $TEST_DATA_FILE"
+	# insert all transactions from testdata file
+	yq -I0 -o=json '.[]' $TEST_DATA_FILE | while IFS='\n' read transaction; do
+		transact "$transaction"
+		printf '%s' '.'
+	done
+	echo ""
+	echo "*** $(yq -o=json 'length' $TEST_DATA_FILE) requests and transactions (mixed) added from $TEST_DATA_FILE"
+fi
