b01c826dc62f7ae2604ed6d60f9c7b33c95c2213
max funk <mxfactorial@users.noreply.github.com>
2024-08-28 16:37:36 -0700

    mix of transactions and requests as optional

--- scripts/insert-transactions.sh
+++ scripts/insert-transactions.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-if [[ "$#" -ne 0 ]] && [[ "$#" -ne 1 ]]; then
-	echo "use: bash scripts/insert-transactions.sh --continue"
+if [[ "$#" -gt 2 ]]; then
+	echo "use: bash scripts/insert-transactions.sh --continue --mix"
 	exit 1
 fi
 
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
 	--continue) CONT=1 ;;
+	--mix) MIX=1 ;;
 	*)
 		echo "unknown parameter passed: $1"
 		exit 1
@@ -48,42 +49,70 @@ echo "*** finished migrations"
 
 echo ""
 
-function transact() {
-	local transaction="$1"
-	# create requests for each in tests/testdata/requests.json
-	TRANSACTION_ID=$(curl -s -H 'Content-Type: application/json' -d "$transaction" $REQUEST_CREATE_URL | yq '.transaction.id')
-
-	# approve every other request to create mix of requests and transactions
-	if [[ $(("$TRANSACTION_ID" % 2)) -eq 0 ]]; then
-
-		# get debitor approver from user (NOT rule) added transaction item
-		DEBITOR_APPROVER=$(echo -n "$transaction" | yq -I0 '[.transaction.transaction_items[] | select(.rule_instance_id == null)][0] | .debitor')
-
-		# mock lambda input event
-		APPROVAL="{\"auth_account\":\""$DEBITOR_APPROVER"\",\"id\":\""$TRANSACTION_ID"\",\"account_name\":\""$DEBITOR_APPROVER"\",\"account_role\":\"debitor\"}"
-
-		# approve transaction request
-		curl -s -H 'Content-Type: application/json' -d "$APPROVAL" $REQUEST_APPROVE_URL >/dev/null
-	fi
+function request() {
+	local request="$1"
+	# create transaction request
+	TRANSACTION_ID=$(curl -s -H 'Content-Type: application/json' -d "$request" $REQUEST_CREATE_URL | yq '.transaction.id')
+	# get debitor approver from user (NOT rule) added transaction item
+	DEBITOR_APPROVER=$(echo -n "$request" | yq -I0 '[.transaction.transaction_items[] | select(.rule_instance_id == null)][0] | .debitor')
 }
 
-# continue inserting random transactions from testdata file indefinitely
+function approve() {
+	# mock lambda input event
+	APPROVAL="{\"auth_account\":\""$DEBITOR_APPROVER"\",\"id\":\""$TRANSACTION_ID"\",\"account_name\":\""$DEBITOR_APPROVER"\",\"account_role\":\"debitor\"}"
+	# approve transaction request
+	curl -s -H 'Content-Type: application/json' -d "$APPROVAL" $REQUEST_APPROVE_URL >/dev/null
+	unset TRANSACTION_ID
+	unset DEBITOR_APPROVER
+}
+
+# insert random transactions indefinitely
 if [[ -n "$CONT" ]]; then
-	echo "*** adding a random mix of requests and transactions from $TEST_DATA_FILE indefinitely"
+	if [[ -n "$MIX" ]]; then
+		echo "*** adding a random mix of requests and transactions from $TEST_DATA_FILE indefinitely"
+	else
+		echo "*** adding transactions from $TEST_DATA_FILE indefinitely"
+	fi
+
 	# increase account balances to avoid insufficient balance error
 	psql -U $PGUSER -d $PGDATABASE -h $PGHOST -c "UPDATE account_balance SET current_balance = 999999000 WHERE true;" >/dev/null
+
 	while true; do
 		RANDOM_INDEX=$((RANDOM % TEST_DATA_FILE_LENGTH))
 		transaction=$(yq -I0 -o=json ".[$RANDOM_INDEX]" $TEST_DATA_FILE)
-		transact "$transaction"
+		request "$transaction"
+		# skip approving odd transactions when mixing
+		if [[ -n "$MIX" ]] && [[ $(("$TRANSACTION_ID" % 2)) -ne 0 ]]; then
+			continue
+		else
+			approve
+		fi
+		# sleep for 100ms to avoid burdening cpu, e.g. 1000/4.5 minutes vs 1000/2.5 minutes without sleep
+		sleep 0.1
 	done
-else
-	echo "*** adding a mix of requests and transactions from $TEST_DATA_FILE"
+else # insert all transactions from testdata file
+	if [[ -n "$MIX" ]]; then
+		echo "*** adding a mix of requests and transactions from $TEST_DATA_FILE"
+	else
+		echo "*** adding transactions from $TEST_DATA_FILE"
+	fi
+
 	# insert all transactions from testdata file
 	yq -I0 -o=json '.[]' $TEST_DATA_FILE | while IFS='\n' read transaction; do
-		transact "$transaction"
-		printf '%s' '.'
+		request "$transaction"
+		# skip approving odd transactions when mixing
+		if [[ -n "$MIX" ]] && [[ $(("$TRANSACTION_ID" % 2)) -ne 0 ]]; then
+			continue
+		else
+			approve
+		fi
 	done
+
 	echo ""
-	echo "*** $(yq -o=json 'length' $TEST_DATA_FILE) requests and transactions (mixed) added from $TEST_DATA_FILE"
+
+	if [[ -n "$MIX" ]]; then
+		echo "*** $TEST_DATA_FILE_LENGTH requests and transactions (mixed) added from $TEST_DATA_FILE"
+	else
+		echo "*** $TEST_DATA_FILE_LENGTH transactions added from $TEST_DATA_FILE"
+	fi
 fi
