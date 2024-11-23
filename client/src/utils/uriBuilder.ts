4ebcd41fcd7a904f4b039f4fac84698a91df7b17
max funk <mxfactorial@users.noreply.github.com>
2024-11-22 17:31:16 -0700

    add uriBuilder client module

---
+++ client/src/utils/uriBuilder.ts
@@ -0,0 +1,25 @@
+import url from 'url';
+
+export default function (uri: string): string {
+	// if protocol prefix missing
+	if (!uri.includes('://')) {
+		// if its a websocket uri
+		if (uri.includes('/ws')) {
+			// add ws:// prefix
+			uri = 'ws://' + uri;
+		} else {
+			// add http:// prefix
+			uri = 'http://' + uri;
+		}
+	}
+	const parsed = url.parse(uri);
+	if (process.env.ENABLE_TLS && process.env.ENABLE_TLS === 'true') {
+		if (parsed.protocol === 'http:') {
+			parsed.protocol = 'https:';
+		}
+		if (parsed.protocol === 'ws:') {
+			parsed.protocol = 'wss:';
+		}
+	}
+	return url.format(parsed);
+}
