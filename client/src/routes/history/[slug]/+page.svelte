7e89bbd7584c6d646c496407262e824c4247f939
max funk <mxfactorial@users.noreply.github.com>
2025-03-04 09:52:50 -0700

    remove client side graphql queries

--- client/src/routes/history/[slug]/+page.svelte
+++ client/src/routes/history/[slug]/+page.svelte
@@ -4,39 +4,11 @@
 	import Card from '../../../components/Card.svelte';
 	import Icon from '../../../icons/Icon.svelte';
 	import { faArrowLeft, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-	import TRANSACTION_BY_ID_QUERY from '../../../graphql/query/transactionByID';
-	import {
-		requestTime,
-		isCreditor,
-		getTransContraAccount,
-		sortTrItems,
-	} from '../../../utils/transactions';
+	import { requestTime, isCreditor, getTransContraAccount } from '../../../utils/transactions';
 	import { fromNow } from '../../../utils/date';
 	import DisputeIcon from '../../../icons/DisputeIcon.svelte';
-	import { account as currentAccount } from '../../../stores/account';
 	import Nav from '../../../components/Nav.svelte';
-	import { page } from '$app/stores';
-	import client from '../../../graphql/client';
-	import { account } from '../../../stores/account';
-
-	let transactionID = $page.params.slug;
-
-	async function getTransactionByID(): Promise<App.ITransaction> {
-		const variables = {
-			id: transactionID,
-			account_name: $account,
-			auth_account: $account
-		};
-
-		const res = await client.query(TRANSACTION_BY_ID_QUERY, variables).toPromise();
-
-		if (!res.data || !res.data.transactionByID) {
-			return {} as App.ITransaction;
-		} else {
-			sortTrItems(res.data.transactionByID.transaction_items);
-			return res.data.transactionByID;
-		}
-	}
+	import { page } from '$app/state';
 </script>
 
 <Nav>
@@ -45,7 +17,7 @@
 			<DetailHeader>
 				{#snippet left()}
 					<span data-id="backBtn">
-						<a href={$page.url.pathname.substring(0, $page.url.pathname.lastIndexOf('/'))}>
+						<a href={page.url.pathname.substring(0, page.url.pathname.lastIndexOf('/'))}>
 							<Icon addedStyle="" iconName={faArrowLeft} dataIDValue="backBtn" />
 						</a>
 					</span>
@@ -58,87 +30,83 @@
 				{/snippet}
 			</DetailHeader>
 		</div>
-		{#await getTransactionByID() then transaction}
-			<div
-				data-id="contraAccount"
-				data-id-contra-account={getTransContraAccount($account, transaction)}
-				class="container"
-			>
-				<Card minHeight="2rem">
-					{#snippet children()}
-						<strong>{getTransContraAccount($account, transaction)}</strong>
-					{/snippet}
-				</Card>
-			</div>
-			<div data-id="sumValue" class="container">
-				<Card minHeight="2rem">
-					{#snippet children()}
-						<strong
-							>{isCreditor($currentAccount, transaction.transaction_items)
-								? ''
-								: '-'}{transaction.sum_value}</strong
-						>
-					{/snippet}
-				</Card>
-			</div>
-			<div data-id="transactionTime" class="container">
-				<Card>
-					{#snippet children()}
-						<small>
-							<p class="time-title">
-								<strong> Equilibrium time </strong>
+		<div
+			data-id="contraAccount"
+			data-id-contra-account={getTransContraAccount(page.data.account, page.data.transaction)}
+			class="container"
+		>
+			<Card minHeight="2rem">
+				{#snippet children()}
+					<strong>{getTransContraAccount(page.data.account, page.data.transaction)}</strong>
+				{/snippet}
+			</Card>
+		</div>
+		<div data-id="sumValue" class="container">
+			<Card minHeight="2rem">
+				{#snippet children()}
+					<strong
+						>{isCreditor(page.data.account, page.data.transaction.transaction_items)
+							? ''
+							: '-'}{page.data.transaction.sum_value}</strong
+					>
+				{/snippet}
+			</Card>
+		</div>
+		<div data-id="transactionTime" class="container">
+			<Card>
+				{#snippet children()}
+					<small>
+						<p class="time-title">
+							<strong> Equilibrium time </strong>
+						</p>
+					</small>
+					<span class="time-content">
+						{fromNow(requestTime(page.data.transaction.transaction_items))}
+					</span>
+				{/snippet}
+			</Card>
+		</div>
+		<hr />
+		<div class="container">
+			{#each page.data.transaction.transaction_items as trItem, i}
+				<div data-id="transaction-item" data-id-index={i} class="container">
+					<Card>
+						{#snippet children()}
+							<p class="transaction-item-title">
+								{trItem.item_id}
 							</p>
-						</small>
-						<span class="time-content">
-							{fromNow(requestTime(transaction.transaction_items))}
-						</span>
-					{/snippet}
-				</Card>
-			</div>
-			<hr />
-			<div class="container">
-				{#each transaction.transaction_items as trItem, i}
-					<div data-id="transaction-item" data-id-index={i} class="container">
-						<Card>
-							{#snippet children()}
-								<p class="transaction-item-title">
-									{trItem.item_id}
-								</p>
-								<p class="quantity-price">
-									<span class="transaction-item-quantity">{trItem.quantity}</span> x
-									<span class="transaction-item-price">{trItem.price}</span>
-								</p>
-							{/snippet}
-						</Card>
+							<p class="quantity-price">
+								<span class="transaction-item-quantity">{trItem.quantity}</span> x
+								<span class="transaction-item-price">{trItem.price}</span>
+							</p>
+						{/snippet}
+					</Card>
+				</div>
+			{/each}
+		</div>
+		<hr />
+		<div class="container">
+			<Card>
+				{#snippet children()}
+					<div style="clear: both;">
+						<p class="left zeros">Transaction ID</p>
+						<p class="right zeros">
+							{page.data.transaction.id}
+						</p>
 					</div>
-				{/each}
-			</div>
-			<hr />
-			<div class="container">
-				<Card>
-					{#snippet children()}
-						<div style="clear: both;">
-							<p class="left zeros">Transaction ID</p>
-							<p class="right zeros">
-								{transaction.id}
-							</p>
-						</div>
-					{/snippet}
-				</Card>
-			</div>
-			<div class="btn-group">
-				<Button disabled={false} class="dispute-btn">
-					{#snippet children()}
-						<DisputeIcon
-							style="transform: translate(-6px, 4px)
+				{/snippet}
+			</Card>
+		</div>
+		<div class="btn-group">
+			<Button disabled={false} class="dispute-btn">
+				{#snippet children()}
+					<DisputeIcon
+						style="transform: translate(-6px, 4px)
 ;"
-						/><span class="dispute-btn-text"> Dispute</span>
-					{/snippet}
-				</Button>
-			</div>
-		{:catch error}
-			<p>{error.message}</p>
-		{/await}
+					/><span class="dispute-btn-text"> Dispute</span>
+				{/snippet}
+			</Button>
+		</div>
 	{/snippet}
 </Nav>
 
