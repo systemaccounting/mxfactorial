29691e8bcc1f50d87b141678726ed7e22517d1ce
max funk <mxfactorial@users.noreply.github.com>
2025-03-04 10:16:19 -0700

    support containerized client in scripts

--- scripts/delete-ecr-repos.sh
+++ scripts/delete-ecr-repos.sh
@@ -5,7 +5,7 @@ ENV=dev
 ENV_ID=$(source scripts/print-env-id.sh)
 REGION=$(yq '.infra.terraform.aws.modules.environment.env_var.set.REGION.default' $PROJECT_CONF)
 
-for APP_NAME in $(bash scripts/list-deployments.sh | grep -v client | xargs basename -a); do
+for APP_NAME in $(bash scripts/list-deployments.sh | xargs basename -a); do
 	IMAGE_NAME="$ENV_ID/$ENV/$APP_NAME"
 	aws ecr delete-repository --repository-name $IMAGE_NAME --region $REGION --force
 done
