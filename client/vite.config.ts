c572da3545416d60068f4e53dfa2205052859fdf
max funk <mxfactorial@users.noreply.github.com>
2025-03-04 09:38:50 -0700

    only pass client port to vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,18 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import dotenv from 'dotenv';
 
 const { parsed: vars = {} } = dotenv.config();
+const port = vars.CLIENT_PORT ? parseInt(vars.CLIENT_PORT) : undefined;
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	define: {
-		'process.env': {
-			...vars
-		}
-	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	server: {
-		port: vars.CLIENT_PORT
+		port
 	}
 });
