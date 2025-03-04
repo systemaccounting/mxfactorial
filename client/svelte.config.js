179e295c1b9832a07d4a27705fd252f3608f7a49
max funk <mxfactorial@users.noreply.github.com>
2025-03-04 09:31:51 -0700

    switch client build adapter to node

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: false
+		}
 	},
 
 	onwarn: (warning, handler) => {
