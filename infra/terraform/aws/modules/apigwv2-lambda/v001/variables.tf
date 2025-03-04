010c77f73d28d4f5cf95ac20b5d1fa4bcbc6b4bb
max funk <mxfactorial@users.noreply.github.com>
2025-03-04 09:58:55 -0700

    avoid cloud managed apigwv2 stage with terraform

--- infra/terraform/aws/modules/apigwv2-lambda/v001/variables.tf
+++ infra/terraform/aws/modules/apigwv2-lambda/v001/variables.tf
@@ -8,3 +8,11 @@ variable "enable_api_auth" {}
 variable "cognito_endpoint" {}
 variable "cognito_client_id" {}
 variable "authorization_header_key" {}
+# variable "apigwv2_logging_level" {
+#   validation {
+#     condition     = can(regex("^(OFF|INFO|ERROR)$", var.apigwv2_logging_level))
+#     error_message = "apigwv2_logging_level must be one of OFF, INFO, ERROR"
+#   }
+# }
+variable "apigwv2_burst_limit" {}
+variable "apigwv2_rate_limit" {}
