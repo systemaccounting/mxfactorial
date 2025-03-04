29a2ab5914417cea91ec7333965526102a0f0432
max funk <mxfactorial@users.noreply.github.com>
2025-03-04 09:40:47 -0700

    convert to client side auth

--- client/src/auth/cognito.ts
+++ client/src/auth/cognito.ts
@@ -1,94 +1,135 @@
-import { CognitoUserPool, AuthenticationDetails, CognitoUser} from 'amazon-cognito-identity-js';
-import type { ICognitoUserPoolData, CognitoUserSession } from 'amazon-cognito-identity-js';
-import b64 from 'base-64';
-import c from '../utils/constants';
+import {
+	CognitoUserPool,
+	AuthenticationDetails,
+	CognitoUser,
+	CookieStorage
+} from 'amazon-cognito-identity-js';
+import type { ICognitoUserPoolData, ICognitoUserData, ICookieStorageData } from 'amazon-cognito-identity-js';
+import JsCookies from 'js-cookie';
 
-const poolData: ICognitoUserPoolData = {
-	UserPoolId: process.env.POOL_ID ? b64.decode(process.env.POOL_ID).trim() : "",
-	ClientId: process.env.CLIENT_ID ? b64.decode(process.env.CLIENT_ID).trim(): "",
+// cookie key prefix defined in getSession method:
+// client/node_modules/amazon-cognito-identity-js/src/CognitoUser.js
+// local dev only:
+const LastAuthUserKey = (clientId: string): string => `CognitoIdentityServiceProvider.${clientId}.LastAuthUser`;
+const IdToken = (clientId: string, account: string): string => {
+	return `CognitoIdentityServiceProvider.${clientId}.${account}.idToken`
 };
 
-let userPool: CognitoUserPool;
-let cognitoUser: CognitoUser | null;
-
-if (c.ENABLE_AUTH) {
-	userPool = new CognitoUserPool(poolData);
-	cognitoUser = userPool.getCurrentUser();
+function cookieConf(domain: string): ICookieStorageData {
+	return {
+		domain,
+		secure: (domain === 'localhost') ? false : true,
+		expires: 1,
+		path: '/',
+		sameSite: 'lax',
+	};
 }
 
-function signIn(account: string, password: string) {
-    const authDetails = new AuthenticationDetails({
-        Username: account,
-        Password: password,
-    });
+function configureUserPool(poolId: string, clientId: string, domain: string): CognitoUserPool {
+	const cookieOpts = cookieConf(domain);
+	const poolData: ICognitoUserPoolData = {
+		UserPoolId: poolId,
+		ClientId: clientId,
+		Storage: new CookieStorage(cookieOpts),
+	};
+	return new CognitoUserPool(poolData);
+}
 
-    const userData = {
-        Username: account,
-        Pool: userPool,
-    };
+function createAuthDetails(username: string, password: string): AuthenticationDetails {
+	return new AuthenticationDetails({
+		Username: username,
+		Password: password,
+	});
+}
 
-    cognitoUser = new CognitoUser(userData);
+function createCognitoUser(userData: ICognitoUserData): CognitoUser {
+	return new CognitoUser(userData);
+}
 
-    return new Promise((res, rej) => {
-        if (cognitoUser) {
-            cognitoUser.authenticateUser(authDetails, {
-                onSuccess: function () { // passes "data"
-                    res({});
-                },
-                onFailure: function (err) {
-                    console.error(err.message)
-                    rej(err)
-                }
-            });
-        }
-    });
-};
-
-function signOut() {
-	if (cognitoUser) {
-		try {
-			cognitoUser.signOut();
-		} catch (e) {
-			console.log(e);
-		};
+function signIn(
+	poolId: string,
+	clientId: string,
+	domain: string,
+	account: string,
+	password: string): Promise<CognitoUser | void> {
+	// local env
+	if (clientId == 'localhost') {
+		JsCookies.set(LastAuthUserKey(clientId), account, cookieConf(domain));
+		JsCookies.set(IdToken(clientId, account), 'localhost', cookieConf(domain));
+		return Promise.resolve()
 	}
-};
+	// cloud env
+	return new Promise((resolve, reject) => {
+		const cookieOpts = cookieConf(domain);
+		const cognitoUser = createCognitoUser({
+			Username: account,
+			Pool: configureUserPool(poolId, clientId, domain),
+			Storage: new CookieStorage(cookieOpts),
+		});
+		const authDetails = createAuthDetails(account, password);
+		cognitoUser.authenticateUser(authDetails, {
+			onSuccess: () => resolve(cognitoUser),
+			onFailure: (err) => reject(err)
+		});
+	});
+}
 
-function signUp(account: string, password: string) {
-	return new Promise((res, rej) => {
-		userPool.signUp(account, password, [], [], function (err, result): Promise<string>|void {
+function signUp(
+	poolId: string,
+	clientId: string,
+	domain: string,
+	account: string,
+	password: string): Promise<CognitoUser | void> {
+	// local env
+	if (clientId == 'localhost') {
+		JsCookies.set(LastAuthUserKey(clientId), account, cookieConf(domain));
+		JsCookies.set(IdToken(clientId, account), 'localhost', cookieConf(domain));
+		return Promise.resolve()
+	}
+	// cloud env
+	const userPool = configureUserPool(poolId, clientId, domain);
+	return new Promise((resolve, reject) => {
+		userPool.signUp(account, password, [], [], (err, result) => {
 			if (err) {
-				rej(err)
-				return;
-			}
-			if (result) {
-				cognitoUser = result.user;
-				res(cognitoUser.getUsername());
+				reject(err);
 			} else {
-				rej("No result")
+				if (result) {
+					resolve(result.user);
+				} else {
+					reject('empty response');
+				}
 			}
-		})
+		});
 	});
 };
 
-function getIdToken(cb: (token: string) => void) {
-	if (cognitoUser) {
-		cognitoUser.getSession(function(err: Error | null, session: CognitoUserSession | null) {
-			if (err) {
-				alert(err.message || JSON.stringify(err));
-				return;
-			}
-			if (session) {
-				cb(session.getIdToken().getJwtToken());
-			}
-		})
+function signOut(
+	poolId: string,
+	clientId: string,
+	domain: string,
+): Promise<void> {
+	// get session from cookie storage
+	const lastAuthUser = JsCookies.get(LastAuthUserKey(clientId));
+	if (!lastAuthUser) {
+		console.error('last auth user not found');
+		return Promise.resolve();
 	}
+	// local env
+	if (clientId === 'localhost') {
+		JsCookies.remove(LastAuthUserKey(clientId), cookieConf(domain));
+		JsCookies.remove(IdToken(clientId, lastAuthUser), cookieConf(domain));
+		return Promise.resolve()
+	}
+	// cloud env
+	const cognitoUser = new CognitoUser({
+		Username: lastAuthUser,
+		Pool: configureUserPool(poolId, clientId, domain),
+		Storage: new CookieStorage(cookieConf(domain)),
+	})
+	return new Promise((resolve) => {
+		cognitoUser.signOut();
+		resolve();
+	});
 }
 
-export {
-	userPool,
-	signIn,
-	signOut,
-	signUp,
-	getIdToken,
-}
+export { signIn, signUp, signOut };
