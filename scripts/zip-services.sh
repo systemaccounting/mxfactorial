118879d1d66ab02246be63c536767f6159ea2bc6
max funk <mxfactorial@users.noreply.github.com>
2025-12-18 20:12:44 -0800

    parallel build client image

--- scripts/zip-services.sh
+++ scripts/zip-services.sh
@@ -16,6 +16,7 @@ zip -r $SERVICES_ZIP \
 	Cargo.toml \
 	Cargo.lock \
 	.cargo \
+	client \
 	docker \
 	make \
 	makefile \
@@ -29,4 +30,7 @@ zip -r $SERVICES_ZIP \
 	--exclude='*/README.md' \
 	--exclude='tests/testdata/*' \
 	--exclude='tests/thunder-tests/*' \
-	--exclude='services/graphql/postman/*' 1>/dev/null
+	--exclude='services/graphql/postman/*' \
+	--exclude='client/node_modules/*' \
+	--exclude='client/build/*' \
+	--exclude='client/.svelte-kit/*' 1>/dev/null
