a5aee48ccb8f707c1542d3242158990686152a3e
max funk <mxfactorial@users.noreply.github.com>
2025-12-21 19:02:49 -0800

    print invoke object

--- migrations/go-migrate/function.sh
+++ migrations/go-migrate/function.sh
@@ -3,23 +3,20 @@ function handler () {
 
   # avoid using identically named variables in sourced migrate script
 
-  # if EVENT_DATA has branch property, then assign variables from object root
-  if [[ $(echo $EVENT_DATA | jq -r '.branch') != "null" ]]; then
-    BRANCH=$(echo $EVENT_DATA | jq -r '.branch')
-    DATABASE_TYPE=$(echo $EVENT_DATA | jq -r '.db_type')
-    MIGRATE_CMD=$(echo $EVENT_DATA | jq -r '.cmd')
-    PASSPHRASE=$(echo $EVENT_DATA | jq -r '.passphrase')
-  else
-    # parse escaped variables from body string
+  # if EVENT_DATA has body property, parse from body string (function url)
+  # otherwise assign variables from object root (lambda invoke)
+  if [[ $(echo $EVENT_DATA | jq -r '.body') != "null" ]]; then
     BODY=$(echo $EVENT_DATA | jq -r '.body')
-    BRANCH=$(echo $BODY | jq -r '.branch')
     DATABASE_TYPE=$(echo $BODY | jq -r '.db_type')
     MIGRATE_CMD=$(echo $BODY | jq -r '.cmd')
     PASSPHRASE=$(echo $BODY | jq -r '.passphrase')
+  else
+    DATABASE_TYPE=$(echo $EVENT_DATA | jq -r '.db_type')
+    MIGRATE_CMD=$(echo $EVENT_DATA | jq -r '.cmd')
+    PASSPHRASE=$(echo $EVENT_DATA | jq -r '.passphrase')
   fi
 
-  CLONE_DIR="/tmp/mxfactorial"
-  MIGRATE_DIR="$CLONE_DIR/migrations"
+  MIGRATE_DIR="/var/task/migrations"
 
   if [[ "$PASSPHRASE" != "$GO_MIGRATE_PASSPHRASE" ]]; then
     # add cloudwatch logs from custom runtime lambda by redirecting stdout to stderr
@@ -27,16 +24,8 @@ function handler () {
     exit 1
   fi
 
-  # clean up previous invoke
-  rm -rf /tmp/mxfactorial
-
-  # clone repo
-  git clone --branch $BRANCH --depth 1 https://github.com/systemaccounting/mxfactorial "$CLONE_DIR"
-
-  # migrate using files from cloned repo
+  # migrate using baked-in migrations
   source ./migrate.sh --dir "$MIGRATE_DIR" --db_type "$DATABASE_TYPE" --cmd "$MIGRATE_CMD"
 
-  RESPONSE="branch: $BRANCH, db_type: $DATABASE_TYPE, cmd: $MIGRATE_CMD"
-
-  echo $RESPONSE
+  echo "{\"db_type\":\"$DATABASE_TYPE\",\"cmd\":\"$MIGRATE_CMD\"}"
 }
