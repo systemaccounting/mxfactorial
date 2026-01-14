04bf97c47f6a7571bf8a01379dcce5db19c0ad12
max funk <mxfactorial@users.noreply.github.com>
2026-01-13 21:21:11 -0800

    remove restart local stack script

--- scripts/stop-local.sh
+++ scripts/stop-local.sh
@@ -2,75 +2,32 @@
 
 PROJECT_CONF=project.yaml
 
-# custom IFS since process list entries have spaces
-CUSTOM_IFS=$(yq '.scripts.env_var.set.CUSTOM_IFS.default' $PROJECT_CONF)
-
-# services started by cargo-watch
-SERVICE_PROCESSES=($(yq ".scripts.env_var.set.SERVICE_PROCESSES.default | join(\"$CUSTOM_IFS\")" $PROJECT_CONF))
-SERVICE_PROCESSES+=($CUSTOM_IFS) # add IFS to terminate last index
-SVC_LIST_SIZE=($(yq '.scripts.env_var.set.SERVICE_PROCESSES.default | length' $PROJECT_CONF))
-
-# left over processes to term
-ADDED_PROCESSES_TO_TERM=($(yq ".scripts.env_var.set.ADDED_PROCESSES_TO_TERM.default | join(\"$CUSTOM_IFS\")" $PROJECT_CONF))
-ADDED_PROCESSES_TO_TERM+=($CUSTOM_IFS)
-ADDED_LIST_SIZE=($(yq '.scripts.env_var.set.ADDED_PROCESSES_TO_TERM.default | length' $PROJECT_CONF))
-
-# services started in workflows without cargo-watch
-CI_SERVICE_PROCESSES=($(yq ".scripts.env_var.set.CI_SERVICE_PROCESSES.default | join(\"$CUSTOM_IFS\")" $PROJECT_CONF))
-CI_SERVICE_PROCESSES+=($CUSTOM_IFS)
-CI_SVC_LIST_SIZE=($(yq '.scripts.env_var.set.CI_SERVICE_PROCESSES.default | length' $PROJECT_CONF))
-
-CLIENT_PROCESS=$(yq '.scripts.env_var.set.CLIENT_PROCESS.default' $PROJECT_CONF)
-
-declare -a PATTERNS
-
-if [[ $CI ]]; then
-	# add ci processes to PATTERNS
-	COUNT=1
-	while IFS="$CUSTOM_IFS" read -r -d "$CUSTOM_IFS" line; do
-		ADJUSTED="$line"
-		if [[ $COUNT -eq $CI_SVC_LIST_SIZE ]]; then
-			ADJUSTED="${line::-1}"
-		fi
-		PATTERNS+=("'$ADJUSTED'")
-		COUNT=$(($COUNT+1))
-	done < <(echo "${CI_SERVICE_PROCESSES[@]}")
-else
-	# add cargo-watch processes to PATTERNS when local/devcontainer
-	COUNT=1
-	while IFS="$CUSTOM_IFS" read -r -d "$CUSTOM_IFS" line; do
-		ADJUSTED="$line" # remove trailing whitespace from last index
-		if [[ $COUNT -eq $SVC_LIST_SIZE ]]; then
-			ADJUSTED="${line::-1}"
-		fi
-		PATTERNS+=("'$ADJUSTED'")
-		COUNT=$(($COUNT+1))
-	done < <(echo "${SERVICE_PROCESSES[@]}")
-
-	PATTERNS+=("'$CLIENT_PROCESS'") # add client process
-fi
-
-COUNT=1
-while IFS="$CUSTOM_IFS" read -r -d "$CUSTOM_IFS" line; do
-	ADJUSTED="$line"
-	if [[ $COUNT -eq $ADDED_LIST_SIZE ]]; then
-		ADJUSTED="${line::-1}"
-	fi
-	PATTERNS+=("'$ADJUSTED'")
-	COUNT=$(($COUNT+1))
-done < <(echo "${ADDED_PROCESSES_TO_TERM[@]}")
-
 source ./scripts/manage-cde-ports.sh
 disable_cde_ports
 
 function stop() {
-	for pid in $(ps aux | eval "grep -e $1" | grep -v 'grep' | awk '{print $2}'); do
-		kill "$pid" > /dev/null 2>&1 || true;
+	for pid in $(ps aux | grep -e "$1" | grep -v 'grep' | awk '{print $2}'); do
+		kill "$pid" > /dev/null 2>&1 || true
 	done
 }
 
-for p in "${PATTERNS[@]}"; do
-	stop "$p"
-done
+# kill service processes
+if [[ $CI ]]; then
+	while IFS= read -r pattern; do
+		stop "$pattern"
+	done < <(yq -r '.scripts.env_var.set.CI_SERVICE_PROCESSES.default[]' $PROJECT_CONF)
+else
+	while IFS= read -r pattern; do
+		stop "$pattern"
+	done < <(yq -r '.scripts.env_var.set.SERVICE_PROCESSES.default[]' $PROJECT_CONF)
 
-make --no-print-directory -C migrations clean # also stops redis in compose
+	# client process
+	stop "$(yq -r '.scripts.env_var.set.CLIENT_PROCESS.default' $PROJECT_CONF)"
+fi
+
+# additional processes
+while IFS= read -r pattern; do
+	stop "$pattern"
+done < <(yq -r '.scripts.env_var.set.ADDED_PROCESSES_TO_TERM.default[]' $PROJECT_CONF)
+
+docker compose -f docker/storage.yaml down
