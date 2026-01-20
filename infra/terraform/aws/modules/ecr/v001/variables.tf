8c623cf7159b282e0fb56c043af225e0a35d19c5
max funk <mxfactorial@users.noreply.github.com>
2026-01-19 18:28:11 -0800

    add codebuild project for each ecr repo

--- infra/terraform/aws/modules/ecr/v001/variables.tf
+++ infra/terraform/aws/modules/ecr/v001/variables.tf
@@ -1,8 +1,12 @@
 variable "env" {}
 variable "env_id" {}
 variable "force_delete" {
-	type = bool
+	type    = bool
 	default = false
 }
 variable "service_name" {}
 variable "max_image_storage_count" {}
+variable "artifacts_bucket_arn" {}
+variable "aws_region" {}
+variable "aws_account_id" {}
+variable "codebuild_compute_type" {}
