4faac9a27c6ffe5907e8c0be2bf87dfb6f5e747b
max funk <mxfactorial@users.noreply.github.com>
2026-01-24 21:42:56 -0800

    add split rule_instance tables in testseed

--- migrations/testseed/000004_transaction.up.sql
+++ migrations/testseed/000004_transaction.up.sql
@@ -3,7 +3,7 @@ with insert_transaction as (
 	insert into transaction (author, author_role, sum_value) values ('GroceryCo', 'creditor', 11.99) returning id
 ),
 item_tax_rule_instance as (
-	SELECT id FROM rule_instance WHERE rule_instance_name='NinePercentSalesTax' AND state_name = 'California'
+	SELECT id FROM transaction_item_rule_instance WHERE rule_instance_name='NinePercentSalesTax' AND state_name = 'California'
 ),
 butter as (
 	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_profile_id, creditor_profile_id, debitor_approval_time, creditor_approval_time, rule_exec_ids) values ((select id from insert_transaction), null, 'butter', 3.000, 1, 'JoeCarter', 'GroceryCo', (select id from account_profile where account_name = 'JoeCarter'), (select id from account_profile where account_name = 'GroceryCo'), NOW(), NOW(), '{"4pF89U71"}') returning id
@@ -24,19 +24,19 @@ tax_tea as (
 	insert into transaction_item (transaction_id, rule_instance_id, item_id, price, quantity, debitor, creditor, debitor_profile_id, creditor_profile_id, debitor_approval_time, creditor_approval_time, rule_exec_ids) values ((select id from insert_transaction), (select id from item_tax_rule_instance), '9% state sales tax', 0.360, 1, 'JoeCarter', 'StateOfCalifornia', (select id from account_profile where account_name = 'JoeCarter'), (select id from account_profile where account_name = 'StateOfCalifornia'), NOW(), NOW(), '{"4pF89U73"}') returning id
 ),
 appr_igor_debitor_rule_instance as (
-	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveDebitStateOfCalifornia' AND account_name = 'IgorPetrov'
+	SELECT * FROM approval_rule_instance WHERE rule_instance_name='ApproveDebitStateOfCalifornia' AND account_name = 'IgorPetrov'
 ),
 appr_miriam_debitor_rule_instance as (
-	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveDebitStateOfCalifornia' AND account_name = 'MiriamLevy'
+	SELECT * FROM approval_rule_instance WHERE rule_instance_name='ApproveDebitStateOfCalifornia' AND account_name = 'MiriamLevy'
 ),
 appr_ben_creditor_rule_instance as (
-	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'BenRoss'
+	SELECT * FROM approval_rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'BenRoss'
 ),
 appr_jacob_creditor_rule_instance as (
-	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'DanLee'
+	SELECT * FROM approval_rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'DanLee'
 ),
 appr_miriam_creditor_rule_instance as (
-	SELECT * FROM rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'MiriamLevy'
+	SELECT * FROM approval_rule_instance WHERE rule_instance_name='ApproveAllCaliforniaCredit' AND account_name = 'MiriamLevy'
 )
 insert into approval (rule_instance_id, transaction_id, transaction_item_id, account_name, account_role, approval_time) values(
 (select id from appr_igor_debitor_rule_instance), (select id from insert_transaction), (select id from butter), (select account_name from appr_igor_debitor_rule_instance), (select account_role from appr_igor_debitor_rule_instance), NOW()
