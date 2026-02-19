e5da04b77e78fb7f57d5c7cac8d23870c7da6a32
max funk <mxfactorial@users.noreply.github.com>
2026-02-19 08:07:09 -0800

    export ecr repo names and arns

--- infra/terraform/aws/modules/project-storage/v001/outputs.tf
+++ infra/terraform/aws/modules/project-storage/v001/outputs.tf
@@ -6,6 +6,10 @@ output "artifacts_bucket_arn" {
   value = aws_s3_bucket.artifacts.arn
 }
 
-output "codebuild_project_names" {
-  value = { for k, v in module.ecr_repos : k => v.codebuild_project_name }
+output "ecr_repo_names" {
+  value = local.ECR_REPOS
+}
+
+output "ecr_repo_arns" {
+  value = { for k, v in module.ecr_repos : k => v.ecr_repository_arn }
 }
