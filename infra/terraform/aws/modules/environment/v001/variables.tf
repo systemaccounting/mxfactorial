5d6ea9356ad74a5f9808e6868432020f9ac0ef1a
max funk <mxfactorial@users.noreply.github.com>
2026-02-19 08:06:51 -0800

    conditional event and measure fargate tasks

--- infra/terraform/aws/modules/environment/v001/variables.tf
+++ infra/terraform/aws/modules/environment/v001/variables.tf
@@ -2,7 +2,7 @@ variable "env" {}
 variable "db_snapshot_id" {}
 variable "rds_allow_major_version_upgrade" {}
 variable "rds_instance_class" {}
-variable "rds_parameter_group" {}
+variable "rds_parameter_group_family" {}
 variable "rds_instance_name" {}
 variable "rds_engine_version" {}
 variable "apigw_authorization_header_key" {}
@@ -19,8 +19,8 @@ variable "env_id" {}
 variable "build_db" { type = bool }
 variable "build_cache" { type = bool }
 variable "readiness_check_path" { default = "/healthz" } // todo: assign from root project.yaml
-variable "microk8s_instance_type" {}
-variable "enable_microk8s" { type = bool }
+variable "ecs_instance_size" { default = "" } // "256/512", "512/1024", etc. empty to disable
+variable "apigw_excluded_az_ids" { default = ["use1-az3"] } // AZs where API Gateway v2 is unavailable
 # variable "apigwv2_logging_level" {}
 variable "apigwv2_burst_limit" {}
 variable "apigwv2_rate_limit" {}
