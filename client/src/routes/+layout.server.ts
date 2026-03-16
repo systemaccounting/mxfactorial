92e820630382b1e61fdf9a94a31e099e47b77719
max funk <mxfactorial@users.noreply.github.com>
2026-03-16 07:27:24 -0700

    switch to LayoutServerLoad

--- client/src/routes/+layout.server.ts
+++ client/src/routes/+layout.server.ts
@@ -1,9 +1,9 @@
-import type { PageServerLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
 import { env } from '$env/dynamic/public';
 
-export const load: PageServerLoad = async () => {
+export const load: LayoutServerLoad = async () => {
 	return {
 		poolId: env.PUBLIC_POOL_ID,
-		clientId: env.PUBLIC_CLIENT_ID,
+		clientId: env.PUBLIC_CLIENT_ID
 	};
 };
